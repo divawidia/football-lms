4bf17daebc3fd262888ebde8a4e36538d4b2df6c
divawidia <wiartha2001@gmail.com>
2024-08-19 15:21:15 +0800

    create invoices subscriptions table migration

---
+++ database/migrations/2024_08_19_072015_create_invoice_subscriptions_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoiceId')->constrained('invoices')->nullOnDelete();
+            $table->foreignId('subscriptionId')->constrained('subscriptions')->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_subscriptions');
+    }
+};
