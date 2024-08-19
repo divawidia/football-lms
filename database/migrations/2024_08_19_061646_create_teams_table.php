f157bdb3e36b61516d5162ff606e85a829308498
divawidia <wiartha2001@gmail.com>
2024-08-19 14:18:56 +0800

    create teams table migration

---
+++ database/migrations/2024_08_19_061646_create_teams_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('coachId')->constrained('coaches')->cascadeOnDelete();
+            $table->foreignId('academyId')->constrained('academies')->cascadeOnDelete();
+            $table->string('teamName');
+            $table->string('ageGroup');
+            $table->string('division');
+            $table->string('logo')->nullable();
+            $table->enum('status', [1,0]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
