d559a3d3393e0e895c9a4479aeda8873d60eda0f
divawidia <wiartha2001@gmail.com>
2024-08-19 16:56:56 +0800

    create academy model

---
+++ app/Models/Academy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Academy extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'email',
+        'phoneNumber',
+        'academyName',
+        'address',
+        'state',
+        'city',
+        'country',
+        'zipCode',
+        'directorName',
+        'status',
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'academyId', 'id');
+    }
+}
