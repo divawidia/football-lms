7e373e4a1127d754cd1fc1c8d28d379c107d4eac
divawidia <wiartha2001@gmail.com>
2024-08-19 14:56:36 +0800

    create player_match_stats table migration

---
+++ database/migrations/2024_08_19_065200_create_player_match_stats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('player_match_stats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('playerId')->constrained('players')->nullOnDelete();
+            $table->foreignId('eventId')->constrained('event_schedules')->cascadeOnDelete();
+            $table->integer('minutesPlayed');
+            $table->integer('goals');
+            $table->integer('assists');
+            $table->integer('ownGoal');
+            $table->integer('shots');
+            $table->integer('passes');
+            $table->integer('fouls');
+            $table->integer('yellowCards');
+            $table->integer('redCards');
+            $table->integer('saves');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('player_match_stats');
+    }
+};
