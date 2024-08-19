1a6e4a75a021209595b3191943f360e87254f0b6
divawidia <wiartha2001@gmail.com>
2024-08-19 15:05:34 +0800

    create product_categories table migration

---
+++ database/migrations/2024_08_19_070420_create_product_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('adminId')->constrained('admins')->nullOnDelete();
+            $table->string('categoryName');
+            $table->text('description')->nullable();
+            $table->enum('status', [1,0]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
