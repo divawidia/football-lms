bb959a9339f63a02f3cfacb377e4cd80c8a7847d
divawidia <wiartha2001@gmail.com>
2024-08-20 06:52:59 +0800

    create coach model

---
+++ app/Models/Coach.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Coach extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'certificationLevel',
+        'specialization',
+        'height',
+        'weight',
+        'joinDate',
+        'status',
+        'hireDate',
+        'userId'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+}
