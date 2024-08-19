05bd272c46ed7ca9bc25cd7f01d2ac469709e504
divawidia <wiartha2001@gmail.com>
2024-08-19 14:10:13 +0800

    create admins table migration

---
+++ database/migrations/2024_08_19_060839_create_admins_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('userId')->constrained('users')->cascadeOnDelete();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('position');
+            $table->enum('status', [0, 1]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
