1c2a06621aa93119ae1df394d290518181ec1e59
divawidia <wiartha2001@gmail.com>
2024-08-19 16:29:52 +0800

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class Admin extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'position',
+        'status'
+    ];
+}
