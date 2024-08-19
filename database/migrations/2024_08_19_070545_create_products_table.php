4a3856b75f74adba746fa965606523c04ca45bd0
divawidia <wiartha2001@gmail.com>
2024-08-19 15:09:13 +0800

    create products table migration

---
+++ database/migrations/2024_08_19_070545_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('adminId')->constrained('admins')->nullOnDelete();
+            $table->foreignId('categoryId')->constrained('product_categories')->nullOnDelete();
+            $table->string('productName');
+            $table->integer('price');
+            $table->text('description')->nullable();
+            $table->enum('priceOption', ['subscription', 'one time payment']);
+            $table->enum('subscriptionCycle', ['monthly', 'quarterly', 'semianually', 'anually'])->nullable();
+            $table->enum('status', [1,0]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
