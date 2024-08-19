84f4f8c29bf6d5005fb2f1b10dd31a5bda309b61
divawidia <wiartha2001@gmail.com>
2024-08-20 06:45:27 +0800

    create player model

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -5,27 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Traits\HasRoles;
 
-class Admin extends Authenticatable
+class Player extends Model
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'firstName',
         'lastName',
         'position',
+        'skill',
+        'strongFoot',
+        'height',
+        'weight',
+        'joinDate',
         'status',
         'userId'
     ];
 
     public function user()
     {
-        return $this->belongsTo(User::class, 'userId', 'id');
+        return $this->belongsTo(User::class, 'userId');
     }
-
-
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -60,4 +60,6 @@ class User extends Authenticatable
     {
         return $this->hasOne(Admin::class, 'userId');
     }
+
+
 }
