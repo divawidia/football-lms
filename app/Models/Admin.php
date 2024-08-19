a881a93c6efd7a1be16b4e344b00622072e9fffd
divawidia <wiartha2001@gmail.com>
2024-08-20 06:39:17 +0800

    update admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -11,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class Admin extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     protected $guard = 'admin';
 
@@ -19,6 +20,14 @@ class Admin extends Authenticatable
         'firstName',
         'lastName',
         'position',
-        'status'
+        'status',
+        'userId'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId', 'id');
+    }
+
+
 }
