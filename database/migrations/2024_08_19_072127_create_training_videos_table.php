2b7e5c5948f599578291f9e63968c8cd076e9b55
divawidia <wiartha2001@gmail.com>
2024-08-19 15:24:22 +0800

    create training videos table migration

---
+++ database/migrations/2024_08_19_072127_create_training_videos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('training_videos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('userId')->constrained('users')->nullOnDelete();
+            $table->string('trainingTitle');
+            $table->text('description')->nullable();
+            $table->string('previewPhoto');
+            $table->integer('totalLesson');
+            $table->integer('totalMinute');
+            $table->enum('level', ['beginer', 'intermediate', 'expert']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('training_videos');
+    }
+};
