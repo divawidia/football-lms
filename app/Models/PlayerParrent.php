db4133fc9ae340bd4f2c3273c4e30c81c4a00e79
divawidia <wiartha2001@gmail.com>
2024-08-20 06:56:27 +0800

    create PlayerParrent model

---
+++ app/Models/PlayerParrent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class PlayerParrent extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = [
+        'player_parrents'
+    ];
+
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'email',
+        'phoneNumber',
+        'relations',
+        'playerId'
+    ];
+
+    public function player()
+    {
+        return $this->belongsTo(Player::class, 'playerId');
+    }
+}
