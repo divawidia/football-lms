78cc651da8ce7aa2e305453a6a46da62d2053343
divawidia <wiartha2001@gmail.com>
2024-08-20 09:07:03 +0800

    create productCategory model

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'adminId',
+        'categoryName',
+        'description',
+        'status',
+    ];
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class, 'adminId');
+    }
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'categoryId');
+    }
+}
