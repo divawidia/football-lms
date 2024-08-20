5797e2eaa7b1efa28062b782d6530bb47382e5e3
divawidia <wiartha2001@gmail.com>
2024-08-20 08:28:12 +0800

    create EventSchedule model

---
+++ app/Models/EventSchedule.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class EventSchedule extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'teamId',
+        'userId',
+        'coachId',
+        'opponentTeamsId',
+        'eventType',
+        'matchType',
+        'eventName',
+        'startDateTime',
+        'endDateTime',
+        'place',
+        'note',
+        'status'
+    ];
+
+    public function team()
+    {
+        return $this->belongsTo(Team::class, 'teamId');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userId');
+    }
+    public function coach()
+    {
+        return $this->belongsTo(Coach::class, 'coachId');
+    }
+    public function opponentTeam()
+    {
+        return $this->belongsTo(OpponentTeam::class, 'opponentTeamsId');
+    }
+}
