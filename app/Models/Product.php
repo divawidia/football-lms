9fd7e12f299dd779cd6bb5e49a8d98ec873eb2e5
divawidia <wiartha2001@gmail.com>
2024-08-20 09:10:21 +0800

    create product model

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'adminId',
+        'categoryId',
+        'productName',
+        'price',
+        'description',
+        'priceOption',
+        'subscriptionCycle',
+        'status',
+    ];
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class, 'adminId');
+    }
+    public function category()
+    {
+        return $this->belongsTo(ProductCategory::class, 'categoryId');
+    }
+    public function invoices()
+    {
+        return $this->belongsToMany(Invoice::class, 'productId', 'invoiceId')
+            ->withPivot('qty','ammount')->withTimestamps();
+    }
+}
