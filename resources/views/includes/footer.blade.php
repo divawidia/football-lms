2c8ad971cf52ca3604162f9ebe591641f8634d4d
divawidia <wiartha2001@gmail.com>
2024-08-22 13:58:47 +0800

    create footer template

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="bg-white border-top-2 mt-auto">
+    <div class="container page__container page-section d-flex flex-column">
+        <p class="text-70 brand mb-24pt">
+{{--            <img class="brand-icon"--}}
+{{--                 src="../../public/images/logo/black-70@2x.png"--}}
+{{--                 width="30"--}}
+{{--                 alt="Luma"> --}}
+            Logo
+        </p>
+        <p class="measure-lead-max text-50 small mr-8pt">Lorem ipsum dolor sit amet consectetur. Pellentesque libero purus ac diam eget eu in vulputate. A bibendum dictum amet mauris nunc erat fusce sapien. Convallis non nisl ut sed neque fermentum elementum. Tempor venenatis bibendum justo phasellus.</p>
+        <p class="mb-8pt d-flex">
+            <a href=""
+               class="text-70 text-underline mr-8pt small">Terms</a>
+            <a href=""
+               class="text-70 text-underline small">Privacy policy</a>
+        </p>
+        <p class="text-50 small mt-n1 mb-0">Copyright <script>document.write(new Date().getFullYear())</script>; All rights reserved.</p>
+    </div>
+</div>
