519b670892fba7c07e31ca7806c9433aabffa161
divawidia <wiartha2001@gmail.com>
2024-08-22 16:04:27 +0800

    create navigation drawer for coaches

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>@yield('title') | Football LMS</title>
-        <!-- Prevent the demo from appearing in search engines -->
-        <meta name="robots" content="noindex">
-        @include('includes.style')
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>@yield('title') | Football LMS</title>
+    <!-- Prevent the demo from appearing in search engines -->
+    <meta name="robots" content="noindex">
+    @include('includes.style')
+</head>
 
-    <body class="layout-app ">
-        <div class="preloader">
-            <div class="sk-chase">
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-                <div class="sk-chase-dot"></div>
-            </div>
-        </div>
+<body class="layout-app ">
+<div class="preloader">
+    <div class="sk-chase">
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+        <div class="sk-chase-dot"></div>
+    </div>
+</div>
 
-        <div class="mdk-drawer-layout js-mdk-drawer-layout" data-push data-responsive-width="992px">
-            <div class="mdk-drawer-layout__content page-content">
-                @include('includes.header')
+<div class="mdk-drawer-layout js-mdk-drawer-layout" data-push data-responsive-width="992px">
+    <div class="mdk-drawer-layout__content page-content">
+        @include('includes.header')
 
-                @yield('content')
+        @yield('content')
 
-                @include('includes.footer')
+        @include('includes.footer')
 
-            </div>
-            @include('includes.navigation')
-        </div>
+    </div>
+    @include('includes.admins.navigation')
+</div>
 
-        @include('includes.scripts')
-    </body>
+@include('includes.scripts')
+</body>
 </html>
