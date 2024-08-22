c6027ae2c3ba7dcac0e64fc0a144a95d5e2d1be2
divawidia <wiartha2001@gmail.com>
2024-08-22 16:41:35 +0800

    create auth function

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class LoginController extends Controller
 {
@@ -36,5 +41,24 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+    public function authenticated(Request $request, $user): RedirectResponse
+    {
+        $user->update([
+            'last_seen' => Carbon::now()
+        ]);
+
+        Cache::put('user-is-online-' . $user->id, true, Carbon::now()->addMinutes(10));
+
+        if (Auth::user()->hasRole('admin') {
+            Alert::toast('Selamat datang ' . Auth::user()->name, 'success');
+            return redirect()->route('admin.dashboard');
+        } else if (Auth::user()->hasRole('coach')) {
+            Alert::toast('Selamat datang ' . Auth::user()->name, 'success');
+            return redirect()->route('instruktur.dashboard');
+        } else  if (Auth::user()->hasRole('player')) {
+            Alert::toast('Selamat datang ' . Auth::user()->name, 'success');
+            return redirect()->route('mahasiswa.dashboard');
+        }
     }
 }
