6663528e7613caa98e77d86e386ddc27b1b5843d
divawidia <wiartha2001@gmail.com>
2024-08-23 12:29:48 +0800

    create player factory

---
+++ database/factories/PlayerFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Player>
+ */
+class PlayerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'userId' => User::factory(),
+            'position' => 'Center Back',
+            'skill' => 'Beginner',
+            'strongFoot' => 'left',
+            'height' => fake()->numberBetween(150,170),
+            'weight' => fake()->numberBetween(40,70),
+            'joinDate' => fake()->date,
+        ];
+    }
+}
