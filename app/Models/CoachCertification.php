94ff97111f9f6d4d157405a05dd79a083d46bddb
divawidia <wiartha2001@gmail.com>
2024-08-30 12:02:29 +0800

    create coach certification model

--- app/Models/CoachCertification.php
+++ app/Models/CoachCertification.php
@@ -5,16 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CoachSpecialization extends Model
+class CoachCertification extends Model
 {
     use HasFactory;
 
     protected $fillable = [
         'name',
+        'federation'
     ];
 
     public function coaches()
     {
-        return $this->hasMany(Coach::class, 'specialization');
+        return $this->hasMany(Coach::class, 'certificationLevel');
     }
 }
