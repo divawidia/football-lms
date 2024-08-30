5c7be587853f51895248bc9f04b130ca0ace342c
divawidia <wiartha2001@gmail.com>
2024-08-30 12:34:32 +0800

    create coach_team_table migration

---
+++ database/migrations/2024_08_30_042855_create_coach_team_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coach_team', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('caochId')->constrained('coaches')->cascadeOnDelete();
+            $table->foreignId('teamId')->constrained('teams')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coach_team');
+    }
+};
