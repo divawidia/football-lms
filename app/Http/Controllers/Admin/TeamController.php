d82c3692abde5d8ef5b58f53f3c7aea988b9f5a6
divawidia <wiartha2001@gmail.com>
2024-08-31 20:42:19 +0800

    create team player datatables ajax function

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -96,6 +96,61 @@ class TeamController extends Controller
         return view('pages.admins.managements.teams.index');
     }
 
+    public function teamPlayers(Team $team){
+        if (request()->ajax()) {
+            $query = $team->players()->get();
+            return Datatables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '
+                                <div class="dropdown">
+                                  <button class="btn btn-sm btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="material-icons">
+                                        more_vert
+                                    </span>
+                                  </button>
+                                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                    <a class="dropdown-item" href="' . route('team-managements.edit', $item->id) . '"><span class="material-icons">edit</span> Edit Player</a>
+                                    <a class="dropdown-item" href="' . route('team-managements.show', $item->id) . '"><span class="material-icons">visibility</span> View Player</a>
+                                    <button type="button" class="dropdown-item delete-user" id="' . $item->id . '">
+                                        <span class="material-icons">delete</span> Remove Player From Team
+                                    </button>
+                                  </div>
+                                </div>';
+                })
+                ->editColumn('age', function ($item){
+                    return $this->getAge($item->user->dob);
+                })
+                ->editColumn('name', function ($item) {
+                    return '
+                            <div class="media flex-nowrap align-items-center"
+                                 style="white-space: nowrap;">
+                                <div class="avatar avatar-sm mr-8pt">
+                                    <img class="rounded-circle header-profile-user img-object-fit-cover" width="40" height="40" src="' . Storage::url($item->logo) . '" alt="profile-pic"/>
+                                </div>
+                                <div class="media-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex d-flex flex-column">
+                                            <p class="mb-0"><strong class="js-lists-values-lead">' . $item->teamName . '</strong></p>
+                                            <small class="js-lists-values-email text-50">' . $item->division . ' - '.$item->ageGroup.'</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>';
+                })
+                ->editColumn('appearance', function ($item) {
+                    return 0;
+                })
+                ->editColumn('goals', function ($item) {
+                    return 0;
+                })
+                ->editColumn('cleanSheets', function ($item) {
+                    return 0;
+                })
+                ->rawColumns(['action', 'name', 'age', 'appearance', 'goals', 'cleanSheets'])
+                ->make();
+        }
+    }
+
     /**
      * Show the form for creating a new resource.
      */
