0ad2ed4916d047c38b222397599d3cc86562129c
divawidia <wiartha2001@gmail.com>
2024-09-03 12:34:10 +0800

    create opponent team request validation

---
+++ app/Http/Requests/OpponentTeamRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class OpponentTeamRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'logo' => ['image', 'nullable'],
+            'teamName' => ['required', 'string', Rule::unique('opponent_teams', 'teamName')->ignore($this->team)],
+            'ageGroup' => ['required', 'string'],
+        ];
+    }
+}
