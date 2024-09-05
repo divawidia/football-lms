4da168fa9fa9c4b06003e246c57ad368e1294d3d
divawidia <wiartha2001@gmail.com>
2024-09-05 12:41:51 +0800

    create playerTeams method in player service

--- app/Services/PlayerService.php
+++ app/Services/PlayerService.php
@@ -101,6 +101,54 @@ class PlayerService extends Service
             ->rawColumns(['action', 'name','status', 'age', 'teams.name'])
             ->make();
     }
+
+    public function playerTeams(Player $player): \Illuminate\Http\JsonResponse
+    {
+        $query = Player::with('teams')->findOrFail($player->id);
+        dd($query);
+        return Datatables::of($query->teams)
+            ->addColumn('action', function ($item) {
+                return '
+                        <div class="dropdown">
+                          <button class="btn btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="material-icons">
+                                more_vert
+                            </span>
+                          </button>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="' . route('player-managements.edit', $item->userId) . '"><span class="material-icons">edit</span> Edit Player</a>
+                            <a class="dropdown-item" href="' . route('player-managements.show', $item->userId) . '"><span class="material-icons">visibility</span> View Player</a>
+                            <a class="dropdown-item" href="' . route('player-managements.change-password-page', $item->userId) . '"><span class="material-icons">lock</span> Change Player Password</a>
+                            <button type="button" class="dropdown-item delete-user" id="' . $item->userId . '">
+                                <span class="material-icons">delete</span> Delete Player
+                            </button>
+                          </div>
+                        </div>';
+            })
+            ->editColumn('name', function ($item) {
+                return '
+                        <div class="media flex-nowrap align-items-center"
+                                 style="white-space: nowrap;">
+                                <div class="avatar avatar-sm mr-8pt">
+                                    <img class="rounded-circle header-profile-user img-object-fit-cover" width="40" height="40" src="' . Storage::url($item->logo) . '" alt="profile-pic"/>
+                                </div>
+                                <div class="media-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex d-flex flex-column">
+                                            <p class="mb-0"><strong class="js-lists-values-lead">' . $item->teamName . '</strong></p>
+                                            <small class="js-lists-values-email text-50">' . $item->division . ' - '.$item->ageGroup.'</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>';
+            })
+            ->editColumn('date', function ($item){
+                return date('M d, Y', strtotime($item->pivot->created_at));
+            })
+            ->rawColumns(['action', 'name','date'])
+            ->make();
+    }
+
     public  function store(array $playerData, $academyId){
 
         $playerData['password'] = bcrypt($playerData['password']);
