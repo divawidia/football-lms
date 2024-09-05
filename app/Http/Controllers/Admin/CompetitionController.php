ba87a6b51080a4054371fc94012c917094ae0ece
divawidia <wiartha2001@gmail.com>
2024-09-05 08:24:42 +0800

    update competition controller

--- app/Http/Controllers/Admin/CompetitionController.php
+++ app/Http/Controllers/Admin/CompetitionController.php
@@ -9,6 +9,7 @@ use App\Models\Competition;
 use App\Models\OpponentTeam;
 use App\Models\Player;
 use App\Models\Team;
+use App\Services\CompetitionService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -17,78 +18,18 @@ use Yajra\DataTables\Facades\DataTables;
 
 class CompetitionController extends Controller
 {
+    private CompetitionService $competitionService;
+    public function __construct(CompetitionService $competitionService)
+    {
+        $this->competitionService = $competitionService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         if (request()->ajax()) {
-            $query = Competition::with('teams', 'opponentTeams')->get();
-            return Datatables::of($query)
-                ->addColumn('action', function ($item) {
-                    return '
-                            <div class="dropdown">
-                              <button class="btn btn-sm btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="material-icons">
-                                    more_vert
-                                </span>
-                              </button>
-                              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="' . route('team-managements.edit', $item->id) . '"><span class="material-icons">edit</span> Edit Competition</a>
-                                <a class="dropdown-item" href="' . route('team-managements.show', $item->id) . '"><span class="material-icons">visibility</span> View Competition</a>
-                                <button type="button" class="dropdown-item delete-user" id="' . $item->id . '">
-                                    <span class="material-icons">delete</span> Delete Competition
-                                </button>
-                              </div>
-                            </div>';
-                })
-                ->editColumn('teams', function ($item) {
-                    $academyTeam = '';
-                    if (count($item->teams) == 0){
-                        $academyTeam = 'No teams in this competition at this moment';
-                    }else{
-                        foreach ($item->teams as $team) {
-                            $academyTeam .= '<span class="badge badge-pill badge-danger">'.$team->teamName.'</span>';
-                        }
-                    }
-                    return $academyTeam;
-                })
-                ->editColumn('opponentTeams', function ($item) {
-                    $opponentTeam = '';
-                    if (count($item->opponentTeams) == 0){
-                        $opponentTeam = 'No teams in this competition at this moment';
-                    }else{
-                        $opponentTeam = count($item->opponentTeams).' Opponent teams';
-                    }
-                    return $opponentTeam;
-                })
-                ->editColumn('name', function ($item) {
-                    return '
-                            <div class="media flex-nowrap align-items-center"
-                                 style="white-space: nowrap;">
-                                <div class="avatar avatar-sm mr-8pt">
-                                    <img class="rounded-circle header-profile-user img-object-fit-cover" width="40" height="40" src="' . Storage::url($item->logo) . '" alt="profile-pic"/>
-                                </div>
-                                <div class="media-body">
-                                    <div class="d-flex align-items-center">
-                                        <div class="flex d-flex flex-column">
-                                            <p class="mb-0"><strong class="js-lists-values-lead">' . $item->name . '</strong></p>
-                                            <small class="js-lists-values-email text-50">' . $item->type . '</small>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>';
-                })
-                ->editColumn('date', function ($item) {
-                    $startDate = date('M d, Y', strtotime($item->startDate));
-                    $endDate = date('M d, Y', strtotime($item->endDate));
-                    return $startDate.' '.$endDate;
-                })
-                ->editColumn('contact', function ($item) {
-                    return $item->contactName. ' ('.$item->contactPhone.')';
-                })
-                ->rawColumns(['action', 'name', 'teams', 'opponentTeams', 'date', 'contact'])
-                ->make();
+            return $this->competitionService->index();
         }
         return view('pages.admins.managements.competitions.index');
     }
@@ -118,20 +59,7 @@ class CompetitionController extends Controller
     {
         $data = $request->validated();
 
-        if ($request->hasFile('logo')){
-            $data['logo'] = $request->file('logo')->store('assets/competition-logo', 'public');
-        }else{
-            $data['logo'] = 'images/undefined-user.png';
-        }
-
-        $competition = Competition::create($data);
-
-        if ($request->has('teams')){
-            $competition->teams()->attach($request->teams);
-        }
-        if ($request->has('opponentTeams')){
-            $competition->opponentTeams()->attach($request->opponentTeams);
-        }
+        $this->competitionService->store($data);
 
         $text = 'Competition '.$data['name'].' successfully added!';
         Alert::success($text);
@@ -182,33 +110,36 @@ class CompetitionController extends Controller
     {
         $data = $request->validated();
 
-        if ($request->hasFile('logo')){
-            $data['logo'] = $request->file('logo')->store('assets/competition-logo', 'public');
-        }else{
-            $data['logo'] = $team->logo;
-        }
-
-        $competition->update($data);
-        $competition->teams()->sync($request->teams);
-        $competition->opponentTeams()->sync($request->opponentTeams);
+        $this->competitionService->update($data, $competition);
 
         $text = 'Competition '.$competition->name.' successfully updated!';
         Alert::success($text);
         return redirect()->route('competition-managements.show', $competition->id);
     }
 
+    public function activate(Competition $competition)
+    {
+        $this->competitionService->activate($competition);
+
+        $text = 'Competition '.$competition->name.' status successfully updated!';
+        Alert::success($text);
+        return redirect()->route('competition-managements.show', $competition->id);
+    }
+
+    public function deactivate(Competition $competition)
+    {
+        $this->competitionService->deactivate($competition);
+
+        $text = 'Competition '.$competition->name.' status successfully updated!';
+        Alert::success($text);
+        return redirect()->route('competition-managements.show', $competition->id);
+    }
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Competition $competition)
     {
-        if (File::exists($competition->logo) && $competition->logo != 'images/undefined-user.png'){
-            File::delete($competition->logo);
-        }
-
-        $competition->teams()->detach();
-        $competition->opponentTeams()->detach();
-        $competition->delete();
+        $this->competitionService->destroy($competition);
 
         return response()->json(['success' => true]);
     }
