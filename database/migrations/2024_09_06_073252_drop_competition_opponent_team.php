8a2c02af9a1a4861529fd123b3260eda66f625f7
divawidia <wiartha2001@gmail.com>
2024-09-06 15:38:10 +0800

    create drop competition opponent team migration

---
+++ database/migrations/2024_09_06_073252_drop_competition_opponent_team.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::drop('competition_opponent_team');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create('competition_opponent_team', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teamId')->constrained('teams')->cascadeOnDelete();
+            $table->foreignId('competitionId')->constrained('competitions')->cascadeOnDelete();
+            $table->integer('matchPlayed')->default(0);
+            $table->integer('won')->default(0);
+            $table->integer('drawn')->default(0);
+            $table->integer('lost')->default(0);
+            $table->integer('goalsFor')->default(0);
+            $table->integer('goalsAgaints')->default(0);
+            $table->integer('goalsDifference')->default(0);
+            $table->integer('points')->default(0);
+            $table->integer('redCards')->default(0);
+            $table->integer('yellowCards')->default(0);
+            $table->string('groupDivision')->nullable();
+            $table->string('competitionResult')->nullable();
+            $table->timestamps();
+        });
+    }
+};
