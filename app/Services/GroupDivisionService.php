452db17bfab470261cf1f1b0fbb3363e193abf10
divawidia <wiartha2001@gmail.com>
2024-09-08 14:04:43 +0800

    create removeTeam method in groupDivision service

--- app/Services/GroupDivisionService.php
+++ app/Services/GroupDivisionService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Competition;
 use App\Models\GroupDivision;
+use App\Models\Team;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Storage;
@@ -75,6 +76,11 @@ class GroupDivisionService extends Service
             $groupDivision->teams()->attach($data['teams']);
         }
         return $groupDivision;
+    }
+
+    public function removeTeam(GroupDivision $group, Team $team)
+    {
+        return $group->teams()->detach($team);
     }
 
     public function update(array $data, GroupDivision $groupDivision): GroupDivision
