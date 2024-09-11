172663e326555f17ac601f7795b5a0b16ffb34b9
divawidia <wiartha2001@gmail.com>
2024-09-11 20:13:34 +0800

    create update attendace status method

--- app/Services/EventScheduleService.php
+++ app/Services/EventScheduleService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Coach;
 use App\Models\EventSchedule;
 use App\Models\Player;
 use App\Models\Team;
@@ -65,6 +66,13 @@ class EventScheduleService extends Service
     public function deactivate(EventSchedule $schedule)
     {
         return $schedule->update(['status' => '0']);
+    }
+
+    public function updatePlayerAttendanceStatus($data, EventSchedule $schedule, Player $player){
+        return $schedule->players()->updateExistingPivot($player->id, ['attendanceStatus'=> $data['attendanceStatus'], 'note' => $data['note']]);
+    }
+    public function updateCoachAttendanceStatus($data, EventSchedule $schedule, Coach $coaches){
+        return $schedule->coaches()->updateExistingPivot($coaches->id, ['attendanceStatus'=> $data['attendanceStatus'], 'note' => $data['note']]);
     }
 
     public function destroy(EventSchedule $schedule)
