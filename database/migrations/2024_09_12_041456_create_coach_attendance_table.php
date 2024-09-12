3d4afe2a57bf1d6b2d9e6c957268310bf9a030e2
divawidia <wiartha2001@gmail.com>
2024-09-12 12:16:04 +0800

    create coach attendance table migration

---
+++ database/migrations/2024_09_12_041456_create_coach_attendance_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coach_attendance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('scheduleId')->constrained('event_schedules')->cascadeOnDelete();
+            $table->foreignId('coachId')->nullable()->constrained('coaches')->nullOnDelete();
+            $table->enum('attendanceStatus', ['Required Action', 'Attended', 'Illness', 'Injured', 'Other'])->default('Required Action');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coach_attendance');
+    }
+};
