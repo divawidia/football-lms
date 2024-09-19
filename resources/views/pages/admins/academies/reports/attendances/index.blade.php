f627e8d815432c660ac89ae449df25b9b646502d
divawidia <wiartha2001@gmail.com>
2024-09-19 14:52:05 +0800

    create attendance index page

---
+++ resources/views/pages/admins/academies/reports/attendances/index.blade.php
@@ -0,0 +1,217 @@
+@extends('layouts.master')
+@section('title')
+    attendance report
+@endsection
+@section('page-title')
+    @yield('title')
+@endsection
+
+    @section('content')
+        <div class="pt-32pt">
+            <div class="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
+                <div class="flex d-flex flex-column flex-sm-row align-items-center">
+                    <div class="mb-24pt mb-sm-0 mr-sm-24pt text-sm-left">
+                        <h2 class="mb-0">@yield('title')</h2>
+                        <ol class="breadcrumb p-0 m-0">
+                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                            <li class="breadcrumb-item active">
+                                @yield('title')
+                            </li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container page__container page-section">
+            {{--    Overview    --}}
+            <div class="page-separator">
+                <div class="page-separator__text">Overview</div>
+                <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                        <div class="card-header">
+                            <h4 class="card-title">Player Attendance</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id='calendar'></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                        <div class="card-header">
+                            <h4 class="card-title">Player Attendance</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id='calendar'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="page-separator">
+                        <div class="page-separator__text">most attended player</div>
+                        <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+                    </div>
+                    <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                        <div class="card-header">
+                            <h4 class="card-title">Player Attendance</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id='calendar'></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="page-separator">
+                        <div class="page-separator__text">most absent player</div>
+                        <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+                    </div>
+                    <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                        <div class="card-header">
+                            <h4 class="card-title">Player Attendance</h4>
+                        </div>
+                        <div class="card-body">
+                            <div id='calendar'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page-separator">
+                <div class="page-separator__text">most absent player</div>
+                <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+            </div>
+            <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-0" id="table">
+                            <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Teams</th>
+                                <th>Match</th>
+                                <th>Training</th>
+                                <th>Attended</th>
+                                <th>Absent</th>
+                                <th>Injured</th>
+                                <th>Illness</th>
+                                <th>Others</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+    @push('addon-script')
+        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
+        <script>
+            $(document).ready(function() {
+                const datatable = $('#table').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ordering: true,
+                    ajax: {
+                        url: '{!! url()->current() !!}',
+                    },
+                    columns: [
+                        { data: 'team', name: 'team' },
+                        { data: 'opponentTeam', name: 'opponentTeam' },
+                        { data: 'date', name: 'date' },
+                        { data: 'place', name: 'place'},
+                        { data: 'competition', name: 'competition'},
+                        { data: 'matchType', name: 'matchType'},
+                        { data: 'status', name: 'status' },
+                        {
+                            data: 'action',
+                            name: 'action',
+                            orderable: false,
+                            searchable: false,
+                            width: '15%'
+                        },
+                    ]
+                });
+
+                $('body').on('click', '.delete', function() {
+                    let id = $(this).attr('id');
+
+                    Swal.fire({
+                        title: "Are you sure to delete this match?",
+                        text: "You won't be able to revert this!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#1ac2a1",
+                        cancelButtonColor: "#E52534",
+                        confirmButtonText: "Yes, delete it!"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $.ajax({
+                                url: "{{ route('match-schedules.destroy', ['schedule' => ':id']) }}".replace(':id', id),
+                                type: 'DELETE',
+                                data: {
+                                    _token: "{{ csrf_token() }}"
+                                },
+                                success: function(response) {
+                                    Swal.fire({
+                                        icon: "success",
+                                        title: "Match successfully deleted!",
+                                    });
+                                    datatable.ajax.reload();
+                                },
+                                error: function(error) {
+                                    Swal.fire({
+                                        icon: "error",
+                                        title: "Oops...",
+                                        text: "Something went wrong when deleting data!",
+                                    });
+                                }
+                            });
+                        }
+                    });
+                });
+
+                const calendarEl = document.getElementById('calendar');
+
+                function getInitialView() {
+                    if (window.innerWidth >= 768 && window.innerWidth < 1200) {
+                        return 'timeGridWeek';
+                    } else if (window.innerWidth <= 768) {
+                        return 'listMonth';
+                    } else {
+                        return 'dayGridMonth';
+                    }
+                }
+
+                const calendar = new FullCalendar.Calendar(calendarEl, {
+                    timeZone: 'local',
+                    editable: false,
+                    droppable: false,
+                    selectable: true,
+                    initialView: getInitialView(),
+                    themeSystem: 'bootstrap',
+                    headerToolbar: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+                    },
+                    // responsive
+                    windowResize: function (view) {
+                        const newView = getInitialView();
+                        calendar.changeView(newView);
+                    },
+                    events: @json($events)
+                });
+                calendar.render();
+            });
+        </script>
+    @endpush
