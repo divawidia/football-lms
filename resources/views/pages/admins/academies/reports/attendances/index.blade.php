04ecf99c243e649c0c3381f8753921badd527d17
divawidia <wiartha2001@gmail.com>
2024-09-19 15:03:38 +0800

    update attendance report index page

--- resources/views/pages/admins/academies/reports/attendances/index.blade.php
+++ resources/views/pages/admins/academies/reports/attendances/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title')
-    attendance report
+    Attendance
 @endsection
 @section('page-title')
     @yield('title')
@@ -10,7 +10,7 @@
         <div class="pt-32pt">
             <div class="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
                 <div class="flex d-flex flex-column flex-sm-row align-items-center">
-                    <div class="mb-24pt mb-sm-0 mr-sm-24pt text-sm-left">
+                    <div class="mr-sm-24pt text-sm-left">
                         <h2 class="mb-0">@yield('title')</h2>
                         <ol class="breadcrumb p-0 m-0">
                             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
@@ -57,36 +57,57 @@
                 <div class="col-lg-6">
                     <div class="page-separator">
                         <div class="page-separator__text">most attended player</div>
-                        <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
                     </div>
                     <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
-                        <div class="card-header">
-                            <h4 class="card-title">Player Attendance</h4>
-                        </div>
-                        <div class="card-body">
-                            <div id='calendar'></div>
+                        <div class="card-body d-flex align-items-center flex-row text-left">
+                            <img src=""
+                                 width="50"
+                                 height="50"
+                                 class="rounded-circle img-object-fit-cover"
+                                 alt="player-photo">
+                            <div class="flex ml-3">
+                                <h5 class="mb-0">name</h5>
+                                <p class="text-50 lh-1 mb-0">Team</p>
+                            </div>
+                            <div class="h2 mb-0 mr-3">12</div>
+                            <div class="ml-auto text-right">
+                                <div class="card-title">Event Attended</div>
+                                <p class="card-subtitle text-50">
+                                    90& of total event
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="page-separator">
                         <div class="page-separator__text">most absent player</div>
-                        <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
                     </div>
                     <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
-                        <div class="card-header">
-                            <h4 class="card-title">Player Attendance</h4>
-                        </div>
-                        <div class="card-body">
-                            <div id='calendar'></div>
+                        <div class="card-body d-flex align-items-center flex-row text-left">
+                            <img src=""
+                                 width="50"
+                                 height="50"
+                                 class="rounded-circle img-object-fit-cover"
+                                 alt="player-photo">
+                            <div class="flex ml-3">
+                                <h5 class="mb-0">name</h5>
+                                <p class="text-50 lh-1 mb-0">Team</p>
+                            </div>
+                            <div class="h2 mb-0 mr-3">12</div>
+                            <div class="ml-auto text-right">
+                                <div class="card-title">Event Absent</div>
+                                <p class="card-subtitle text-50">
+                                    90& of total event
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
 
             <div class="page-separator">
-                <div class="page-separator__text">most absent player</div>
-                <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+                <div class="page-separator__text">player attendance</div>
             </div>
             <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
                 <div class="card-body">
@@ -113,105 +134,3 @@
             </div>
         </div>
     @endsection
-    @push('addon-script')
-        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js'></script>
-        <script>
-            $(document).ready(function() {
-                const datatable = $('#table').DataTable({
-                    processing: true,
-                    serverSide: true,
-                    ordering: true,
-                    ajax: {
-                        url: '{!! url()->current() !!}',
-                    },
-                    columns: [
-                        { data: 'team', name: 'team' },
-                        { data: 'opponentTeam', name: 'opponentTeam' },
-                        { data: 'date', name: 'date' },
-                        { data: 'place', name: 'place'},
-                        { data: 'competition', name: 'competition'},
-                        { data: 'matchType', name: 'matchType'},
-                        { data: 'status', name: 'status' },
-                        {
-                            data: 'action',
-                            name: 'action',
-                            orderable: false,
-                            searchable: false,
-                            width: '15%'
-                        },
-                    ]
-                });
-
-                $('body').on('click', '.delete', function() {
-                    let id = $(this).attr('id');
-
-                    Swal.fire({
-                        title: "Are you sure to delete this match?",
-                        text: "You won't be able to revert this!",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#1ac2a1",
-                        cancelButtonColor: "#E52534",
-                        confirmButtonText: "Yes, delete it!"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            $.ajax({
-                                url: "{{ route('match-schedules.destroy', ['schedule' => ':id']) }}".replace(':id', id),
-                                type: 'DELETE',
-                                data: {
-                                    _token: "{{ csrf_token() }}"
-                                },
-                                success: function(response) {
-                                    Swal.fire({
-                                        icon: "success",
-                                        title: "Match successfully deleted!",
-                                    });
-                                    datatable.ajax.reload();
-                                },
-                                error: function(error) {
-                                    Swal.fire({
-                                        icon: "error",
-                                        title: "Oops...",
-                                        text: "Something went wrong when deleting data!",
-                                    });
-                                }
-                            });
-                        }
-                    });
-                });
-
-                const calendarEl = document.getElementById('calendar');
-
-                function getInitialView() {
-                    if (window.innerWidth >= 768 && window.innerWidth < 1200) {
-                        return 'timeGridWeek';
-                    } else if (window.innerWidth <= 768) {
-                        return 'listMonth';
-                    } else {
-                        return 'dayGridMonth';
-                    }
-                }
-
-                const calendar = new FullCalendar.Calendar(calendarEl, {
-                    timeZone: 'local',
-                    editable: false,
-                    droppable: false,
-                    selectable: true,
-                    initialView: getInitialView(),
-                    themeSystem: 'bootstrap',
-                    headerToolbar: {
-                        left: 'prev,next today',
-                        center: 'title',
-                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-                    },
-                    // responsive
-                    windowResize: function (view) {
-                        const newView = getInitialView();
-                        calendar.changeView(newView);
-                    },
-                    events: @json($events)
-                });
-                calendar.render();
-            });
-        </script>
-    @endpush
