7f95e7184d185a35dc9223745ae4bf32f2d5991e
divawidia <wiartha2001@gmail.com>
2024-09-22 08:40:11 +0800

    update opponent team edit page

--- resources/views/pages/admins/managements/opponentTeams/edit.blade.php
+++ resources/views/pages/admins/managements/opponentTeams/edit.blade.php
@@ -8,22 +8,18 @@
 
     @section('content')
         <div class="pt-32pt">
-            <div class="container page__container d-flex flex-column flex-md-row align-items-center text-center text-sm-left">
-                <div class="flex d-flex flex-column flex-sm-row align-items-center mb-24pt mb-md-0">
-                    <div class="mb-24pt mb-sm-0 mr-sm-24pt">
-                        <h2 class="mb-0">
-                            @yield('title')
-                        </h2>
-                        <ol class="breadcrumb p-0 m-0">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('opponentTeam-managements.index') }}">Teams Management</a></li>
-                            <li class="breadcrumb-item"><a href="{{ route('opponentTeam-managements.show', $team->id) }}">{{ $team->teamName }}</a></li>
-                            <li class="breadcrumb-item active">
-                                @yield('title')
-                            </li>
-                        </ol>
-                    </div>
-                </div>
+            <div class="container page__container d-flex flex-column">
+                <h2 class="mb-2">
+                    @yield('title')
+                </h2>
+                <ol class="breadcrumb p-0 m-0">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('opponentTeam-managements.index') }}">Teams Management</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('opponentTeam-managements.show', $team->id) }}">{{ $team->teamName }}</a></li>
+                    <li class="breadcrumb-item active">
+                        Edit
+                    </li>
+                </ol>
             </div>
         </div>
 
