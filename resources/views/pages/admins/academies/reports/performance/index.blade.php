cdfe20a37da65f510439847fc77de91bdec3d111
divawidia <wiartha2001@gmail.com>
2024-09-26 07:07:37 +0800

    update index performance page

--- resources/views/pages/admins/academies/reports/performance/index.blade.php
+++ resources/views/pages/admins/academies/reports/performance/index.blade.php
@@ -26,14 +26,14 @@
                 <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
             </div>
 
-            <div class="row">
+            <div class="row mb-3">
                 <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
                     <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
                         <div class="card-body d-flex align-items-center">
                             <div class="flex d-flex align-items-center">
                                 <div class="h2 mb-0 mr-3">12</div>
                                 <div class="ml-auto text-right">
-                                    <div class="card-title">Match Played</div>
+                                    <div class="card-title">Wins</div>
                                     <p class="card-subtitle text-50">
                                         4
                                         <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
@@ -50,58 +50,7 @@
                             <div class="flex d-flex align-items-center">
                                 <div class="h2 mb-0 mr-3">12</div>
                                 <div class="ml-auto text-right">
-                                    <div class="card-title">Goals</div>
-                                    <p class="card-subtitle text-50">
-                                        4
-                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
-                                        From Last Match
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
-                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
-                        <div class="card-body d-flex align-items-center">
-                            <div class="flex d-flex align-items-center">
-                                <div class="h2 mb-0 mr-3">12</div>
-                                <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">goals conceded</div>
-                                    <p class="card-subtitle text-50">
-                                        4
-                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
-                                        From Last Match
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
-                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
-                        <div class="card-body d-flex align-items-center">
-                            <div class="flex d-flex align-items-center">
-                                <div class="h2 mb-0 mr-3">12</div>
-                                <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">goal difference</div>
-                                    <p class="card-subtitle text-50">
-                                        4
-                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
-                                        From Last Match
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
-                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
-                        <div class="card-body d-flex align-items-center">
-                            <div class="flex d-flex align-items-center">
-                                <div class="h2 mb-0 mr-3">12</div>
-                                <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">clean sheets</div>
+                                    <div class="card-title">Losses</div>
                                     <p class="card-subtitle text-50">
                                         4
                                         <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
@@ -118,24 +67,7 @@
                             <div class="flex d-flex align-items-center">
                                 <div class="h2 mb-0 mr-3">12</div>
                                 <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">own goals</div>
-                                    <p class="card-subtitle text-50">
-                                        4
-                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
-                                        From Last Match
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
-                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
-                        <div class="card-body d-flex align-items-center">
-                            <div class="flex d-flex align-items-center">
-                                <div class="h2 mb-0 mr-3">12</div>
-                                <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">wins</div>
+                                    <div class="card-title text-capitalize">Draws</div>
                                     <p class="card-subtitle text-50">
                                         4
                                         <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
@@ -152,7 +84,7 @@
                             <div class="flex d-flex align-items-center">
                                 <div class="h2 mb-0 mr-3">12</div>
                                 <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">losses</div>
+                                    <div class="card-title text-capitalize">match appearannces</div>
                                     <p class="card-subtitle text-50">
                                         4
                                         <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
@@ -169,7 +101,75 @@
                             <div class="flex d-flex align-items-center">
                                 <div class="h2 mb-0 mr-3">12</div>
                                 <div class="ml-auto text-right">
-                                    <div class="card-title text-capitalize">draws</div>
+                                    <div class="card-title text-capitalize">goals</div>
+                                    <p class="card-subtitle text-50">
+                                        4
+                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
+                                        From Last Month
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
+                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
+                        <div class="card-body d-flex align-items-center">
+                            <div class="flex d-flex align-items-center">
+                                <div class="h2 mb-0 mr-3">12</div>
+                                <div class="ml-auto text-right">
+                                    <div class="card-title text-capitalize">assists</div>
+                                    <p class="card-subtitle text-50">
+                                        4
+                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
+                                        From Last Month
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
+                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
+                        <div class="card-body d-flex align-items-center">
+                            <div class="flex d-flex align-items-center">
+                                <div class="h2 mb-0 mr-3">12</div>
+                                <div class="ml-auto text-right">
+                                    <div class="card-title text-capitalize">Fouls</div>
+                                    <p class="card-subtitle text-50">
+                                        4
+                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
+                                        From Last Month
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
+                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
+                        <div class="card-body d-flex align-items-center">
+                            <div class="flex d-flex align-items-center">
+                                <div class="h2 mb-0 mr-3">12</div>
+                                <div class="ml-auto text-right">
+                                    <div class="card-title text-capitalize">shots</div>
+                                    <p class="card-subtitle text-50">
+                                        4
+                                        <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
+                                        From Last Month
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-lg-4 card-group-row__col flex-column mb-2">
+                    <div class="card border-1 border-left-3 border-left-accent mb-lg-0">
+                        <div class="card-body d-flex align-items-center">
+                            <div class="flex d-flex align-items-center">
+                                <div class="h2 mb-0 mr-3">12</div>
+                                <div class="ml-auto text-right">
+                                    <div class="card-title text-capitalize">Minutes played</div>
                                     <p class="card-subtitle text-50">
                                         4
                                         <i class="material-icons text-success ml-4pt icon-16pt">keyboard_arrow_up</i>
@@ -183,24 +183,100 @@
             </div>
 
             <div class="page-separator">
-                <div class="page-separator__text">player attendance</div>
+                <div class="page-separator__text">Latest Match</div>
             </div>
+
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row d-flex">
+                                <div class="col-4 d-flex flex-column flex-md-row align-items-center">
+                                    <img src=""
+                                         width="50"
+                                         height="50"
+                                         class="rounded-circle img-object-fit-cover"
+                                         alt="team-logo">
+                                    <div class="ml-md-3 text-center text-md-left">
+                                        <h6 class="mb-0">Team Name</h6>
+                                        <p class="text-50 lh-1 mb-0">U-11</p>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-center">
+                                    <h2 class="mb-0">0 - 0</h2>
+                                </div>
+                                <div class="col-4 d-flex flex-column-reverse flex-md-row align-items-center justify-content-end">
+                                    <div class="mr-md-3 text-center text-md-right">
+                                        <h6 class="mb-0">tEAM nAME</h6>
+                                        <p class="text-50 lh-1 mb-0">U-11</p>
+                                    </div>
+                                    <img src=""
+                                         width="50"
+                                         height="50"
+                                         class="rounded-circle img-object-fit-cover"
+                                         alt="team-logo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row d-flex">
+                                <div class="col-4 d-flex flex-column flex-md-row align-items-center">
+                                    <img src=""
+                                         width="50"
+                                         height="50"
+                                         class="rounded-circle img-object-fit-cover"
+                                         alt="team-logo">
+                                    <div class="ml-md-3 text-center text-md-left">
+                                        <h6 class="mb-0">Team Name</h6>
+                                        <p class="text-50 lh-1 mb-0">U-11</p>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-center">
+                                    <h2 class="mb-0">0 - 0</h2>
+                                </div>
+                                <div class="col-4 d-flex flex-column-reverse flex-md-row align-items-center justify-content-end">
+                                    <div class="mr-md-3 text-center text-md-right">
+                                        <h6 class="mb-0">tEAM nAME</h6>
+                                        <p class="text-50 lh-1 mb-0">U-11</p>
+                                    </div>
+                                    <img src=""
+                                         width="50"
+                                         height="50"
+                                         class="rounded-circle img-object-fit-cover"
+                                         alt="team-logo">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="page-separator">
+                <div class="page-separator__text">Match History</div>
+            </div>
+
             <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table class="table table-hover mb-0" id="table">
+                        <table class="table table-hover mb-0" id="playerStatTable">
                             <thead>
                             <tr>
                                 <th>Name</th>
-                                <th>Teams</th>
-                                <th>Total Events</th>
-                                <th>Match</th>
-                                <th>Training</th>
-                                <th>Attended</th>
-                                <th>Absent</th>
-                                <th>Injured</th>
-                                <th>Illness</th>
-                                <th>Others</th>
+                                <th>Minutes Played</th>
+                                <th>Goals</th>
+                                <th>Assists</th>
+                                <th>Own Goals</th>
+                                <th>Shots</th>
+                                <th>Passes</th>
+                                <th>Fouls</th>
+                                <th>Yellow Cards</th>
+                                <th>Red Cards</th>
+                                <th>Saves</th>
+                                <th>Last Updated</th>
                                 <th>Action</th>
                             </tr>
                             </thead>
@@ -210,6 +286,38 @@
                     </div>
                 </div>
             </div>
+
+            <div class="page-separator">
+                <div class="page-separator__text">Leaderboard</div>
+                <a href="" id="addTeamScorer" class="btn btn-primary btn-sm ml-auto"><span class="material-icons mr-2">add</span> Filter</a>
+            </div>
+
+            <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover mb-0" id="classTable">
+                            <thead>
+                            <tr>
+                                <th>Team</th>
+                                <th>Match Played</th>
+                                <th>won</th>
+                                <th>drawn</th>
+                                <th>lost</th>
+                                <th>goals For</th>
+                                <th>goals Againts</th>
+                                <th>goals Difference</th>
+                                <th>red Cards</th>
+                                <th>yellow Cards</th>
+                                <th>points</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
         </div>
     @endsection
 
