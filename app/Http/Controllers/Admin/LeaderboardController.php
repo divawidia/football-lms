8998048f5030fd1818531f52af9c2ee23b3c5803
divawidia <wiartha2001@gmail.com>
2024-09-27 23:12:40 +0800

    create LeaderboardController

---
+++ app/Http/Controllers/Admin/LeaderboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+
+class LeaderboardController extends Controller
+{
+    public function index(){
+
+    }
+}
