3fa3af0b2bde0855f3267bee4b63199a439f9488
divawidia <wiartha2001@gmail.com>
2024-09-28 12:36:33 +0800

    create store method inTrainingVideoLessonService

--- app/Services/TrainingVideoLessonService.php
+++ app/Services/TrainingVideoLessonService.php
@@ -2,7 +2,13 @@
 
 namespace App\Services;
 
+use App\Models\TrainingVideo;
+use App\Models\TrainingVideoLesson;
+
 class TrainingVideoLessonService
 {
-
+    public function store(array $data, TrainingVideo $trainingVideo){
+        $data['trainingVideoId'] = $trainingVideo->id;
+        return TrainingVideoLesson::create($data);
+    }
 }
