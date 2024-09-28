443917380d0cdec1a00891be96ed7357a0c7089f
divawidia <wiartha2001@gmail.com>
2024-09-28 14:06:00 +0800

    create index method in TrainingVideoLessonController

--- app/Http/Controllers/Admin/TrainingVideoLessonController.php
+++ app/Http/Controllers/Admin/TrainingVideoLessonController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\TrainingVideo;
+use App\Services\TrainingVideoLessonService;
 
 class TrainingVideoLessonController extends Controller
 {
-    //
+    private TrainingVideoLessonService $trainingVideoLessonService;
+
+    public function __construct(TrainingVideoLessonService $trainingVideoLessonService){
+        $this->trainingVideoLessonService = $trainingVideoLessonService;
+    }
+
+    public function index(TrainingVideo $trainingVideo){
+        return $this->trainingVideoLessonService->index($trainingVideo);
+    }
 }
