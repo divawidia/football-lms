9407282e9f83d3500086b739b85412ed7805b031
divawidia <wiartha2001@gmail.com>
2024-09-28 12:25:33 +0800

    create store and index method in TrainingVideoService

--- app/Services/TrainingVideoService.php
+++ app/Services/TrainingVideoService.php
@@ -2,7 +2,24 @@
 
 namespace App\Services;
 
+use App\Models\TrainingVideo;
+
 class TrainingVideoService
 {
+    public function index(){
+        return TrainingVideo::all();
+    }
 
+    public function store(array $data, $userId){
+        if (array_key_exists('previewPhoto', $data)){
+            $data['previewPhoto'] =$data['previewPhoto']->store('assets/training-videos', 'public');
+        }else{
+            $data['previewPhoto'] = 'images/video-preview.png';
+        }
+        $data['totalLesson'] = 0;
+        $data['totalMinute'] = 0;
+        $data['userId'] = $userId;
+
+        return TrainingVideo::create($data);
+    }
 }
