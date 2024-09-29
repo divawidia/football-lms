ba16ba9029c277b17c17ce46efd9bd6b396afc1e
divawidia <wiartha2001@gmail.com>
2024-09-29 12:15:09 +0800

    update training videos index page

--- resources/views/pages/admins/academies/training-videos/index.blade.php
+++ resources/views/pages/admins/academies/training-videos/index.blade.php
@@ -28,155 +28,44 @@
             </a>
 
             <div class="row">
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card card-sm card--elevated p-relative o-hidden">
-                        <a href="">
-                            <img class="img-fluid" src="{{ Storage::url('images/video-preview.png') }}" alt="training-preview">
-                        </a>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class="flex">
-                                    <a class="card-title mb-4pt" href="">Learn Angular fundamentals</a>
+                @foreach($data as $training)
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="card card-sm card--elevated p-relative o-hidden">
+                            <a href="{{ route('training-videos.show', $training->id) }}">
+                                <img class="img-fluid" src="{{ Storage::url($training->previewPhoto) }}" alt="training-preview">
+                            </a>
+                            <div class="card-body">
+                                <div class="d-flex">
+                                    <div class="flex">
+                                        <a class="card-title mb-4pt" href="">{{$training->trainingTitle}}</a>
+                                    </div>
+                                    <a href="{{ route('training-videos.show', $training->id) }}" class="ml-4pt material-icons text-20 card-course__icon-favorite">edit</a>
                                 </div>
-                                <a href="" class="ml-4pt material-icons text-20 card-course__icon-favorite">edit</a>
-                            </div>
-                            <div class="d-flex">
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">access_time</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>6 hours</small></p>
-                                </div>
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">play_circle_outline</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>12 lessons</small></p>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">assessment</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>Beginner</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card card-sm card--elevated p-relative o-hidden">
-                        <a href="">
-                            <img class="img-fluid" src="{{ Storage::url('images/video-preview.png') }}" alt="training-preview">
-                        </a>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class="flex">
-                                    <a class="card-title mb-4pt" href="">Learn Angular fundamentals</a>
-                                </div>
-                                <a href="" class="ml-4pt material-icons text-20 card-course__icon-favorite">edit</a>
-                            </div>
-                            <div class="d-flex">
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">access_time</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>6 hours</small></p>
-                                </div>
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">play_circle_outline</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>12 lessons</small></p>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">assessment</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>Beginner</small></p>
+                                <div class="d-flex">
+                                    <div class="d-flex align-items-center mr-2">
+                                        <span class="material-icons icon-16pt text-50 mr-4pt">access_time</span>
+                                        <p class="flex text-50 lh-1 mb-0"><small>{{ $training->totalMinute }} hours</small></p>
+                                    </div>
+                                    <div class="d-flex align-items-center mr-2">
+                                        <span class="material-icons icon-16pt text-50 mr-4pt">play_circle_outline</span>
+                                        <p class="flex text-50 lh-1 mb-0"><small>{{ $training->totalLesson }} lessons</small></p>
+                                    </div>
+                                    <div class="d-flex align-items-center">
+                                        <span class="material-icons icon-16pt text-50 mr-4pt">assessment</span>
+                                        <p class="flex text-50 lh-1 mb-0"><small>{{ $training->level }}</small></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card card-sm card--elevated p-relative o-hidden">
-                        <a href="">
-                            <img class="img-fluid" src="{{ Storage::url('images/video-preview.png') }}" alt="training-preview">
-                        </a>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class="flex">
-                                    <a class="card-title mb-4pt" href="">Learn Angular fundamentals</a>
-                                </div>
-                                <a href="" class="ml-4pt material-icons text-20 card-course__icon-favorite">edit</a>
-                            </div>
-                            <div class="d-flex">
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">access_time</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>6 hours</small></p>
-                                </div>
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">play_circle_outline</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>12 lessons</small></p>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">assessment</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>Beginner</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4">
-                    <div class="card card-sm card--elevated p-relative o-hidden">
-                        <a href="">
-                            <img class="img-fluid" src="{{ Storage::url('images/video-preview.png') }}" alt="training-preview">
-                        </a>
-                        <div class="card-body">
-                            <div class="d-flex">
-                                <div class="flex">
-                                    <a class="card-title mb-4pt" href="">Learn Angular fundamentals</a>
-                                </div>
-                                <a href="" class="ml-4pt material-icons text-20 card-course__icon-favorite">edit</a>
-                            </div>
-                            <div class="d-flex">
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">access_time</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>6 hours</small></p>
-                                </div>
-                                <div class="d-flex align-items-center mr-2">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">play_circle_outline</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>12 lessons</small></p>
-                                </div>
-                                <div class="d-flex align-items-center">
-                                    <span class="material-icons icon-16pt text-50 mr-4pt">assessment</span>
-                                    <p class="flex text-50 lh-1 mb-0"><small>Beginner</small></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
             </div>
-
         </div>
     @endsection
     @push('addon-script')
         <script>
             $(document).ready(function() {
-                const datatable = $('#table').DataTable({
-                    processing: true,
-                    serverSide: true,
-                    ordering: true,
-                    ajax: {
-                        url: '{!! url()->current() !!}',
-                    },
-                    columns: [
-                        { data: 'name', name: 'name' },
-                        { data: 'divisions', name: 'divisions' },
-                        { data: 'teams', name: 'teams' },
-                        { data: 'date', name: 'date'},
-                        { data: 'location', name: 'location'},
-                        { data: 'contact', name: 'contact' },
-                        { data: 'status', name: 'status' },
-                        {
-                            data: 'action',
-                            name: 'action',
-                            orderable: false,
-                            searchable: false,
-                            width: '15%'
-                        },
-                    ]
-                });
+
                 $('body').on('click', '.delete', function() {
                     let id = $(this).attr('id');
 
