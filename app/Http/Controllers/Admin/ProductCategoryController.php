ba5400045b4fefa8cbfbd0f8b8def3a915bd53da
divawidia <wiartha2001@gmail.com>
2024-10-02 13:03:38 +0800

    update method in ProductCategoryController

--- app/Http/Controllers/Admin/ProductCategoryController.php
+++ app/Http/Controllers/Admin/ProductCategoryController.php
@@ -3,24 +3,26 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProductCategory;
+use App\Services\ProductCategoryService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductCategoryController extends Controller
 {
+    private ProductCategoryService $productCategoryService;
+
+    public function __construct(ProductCategoryService $productCategoryService)
+    {
+        $this->productCategoryService = $productCategoryService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return $this->productCategoryService->index();
     }
 
     /**
@@ -28,38 +30,47 @@ class ProductCategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        $data = $request->validated();
+        $admin = Auth::user()->roles()->first();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        return response()->json($this->productCategoryService->store($data, $admin->id));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(ProductCategory $productCategory)
     {
-        //
+        return response()->json([
+            'status' => '200',
+            'data' => $productCategory,
+            'message' => 'Success'
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, ProductCategory $productCategory)
     {
-        //
+        $data = $request->validated();
+
+        return response()->json($this->productCategoryService->update($data, $productCategory));
+    }
+
+    public function activate(ProductCategory $productCategory){
+        return response()->json($this->productCategoryService->activate($productCategory));
+    }
+
+    public function deactivate(ProductCategory $productCategory){
+        return response()->json($this->productCategoryService->deactivate($productCategory));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(ProductCategory $productCategory)
     {
-        //
+        return response()->json($this->productCategoryService->destroy($productCategory));
     }
 }
