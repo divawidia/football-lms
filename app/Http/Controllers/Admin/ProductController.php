def1dafeb3636279d0e9663638c019755b565064
divawidia <wiartha2001@gmail.com>
2024-10-02 14:29:04 +0800

    create product request validation

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductCategoryRequest;
+use App\Http\Requests\ProductRequest;
 use App\Models\Product;
 use App\Models\ProductCategory;
 use App\Services\ProductCategoryService;
