b7935d8fb6a8fe0e10f30a947fccca4ddd486225
divawidia <wiartha2001@gmail.com>
2024-10-07 10:57:33 +0800

    create migration for modify_column_in_academies_table

---
+++ database/migrations/2024_10_07_025429_modify_column_in_academies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('academies', function (Blueprint $table) {
+            $table->dropColumn('state');
+            $table->dropColumn('city');
+            $table->dropColumn('country');
+            $table->foreignId('state_id')->nullable()->constrained('states')->nullOnDelete();
+            $table->foreignId('city_id')->nullable()->constrained('cities')->nullOnDelete();
+            $table->foreignId('country_id')->nullable()->constrained('countries')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('academies', function (Blueprint $table) {
+            $table->string('state');
+            $table->string('city');
+            $table->string('country');
+            $table->dropConstrainedForeignId('state_id');
+            $table->dropConstrainedForeignId('city_id');
+            $table->dropConstrainedForeignId('country_id');
+        });
+    }
+};
