389211c81480ee706ea647a83f0ba7c595cfc4c6
divawidia <wiartha2001@gmail.com>
2024-10-09 13:54:40 +0800

    create invoices method in subscription service

--- app/Services/SubscriptionService.php
+++ app/Services/SubscriptionService.php
@@ -95,7 +95,7 @@ class SubscriptionService extends Service
                 return $this->priceFormat($item->ammountDue);
             })
             ->editColumn('startDate', function ($item) {
-                return $this->convertToDatetime($item->dueDate);
+                return $this->convertToDatetime($item->startDate);
             })
             ->editColumn('nextDueDate', function ($item) {
                 return $this->convertToDatetime($item->nextDueDate);
@@ -120,108 +120,65 @@ class SubscriptionService extends Service
             ->make();
     }
 
-    public function store(array $data, $creatorUserIdd, $academyId)
-    {
-        $data['creatorUserId'] = $creatorUserIdd;
-        $data['academyId'] = $academyId;
-        $data['invoiceNumber'] = $this->generateInvoiceNumber();
-        $data['subtotal'] = 0;
-
-        foreach ($data['products'] as $product) {
-            $data['subtotal'] = $data['subtotal'] + $product['ammount'];
-        }
-        $data['ammountDue'] = $data['subtotal'];
-
-        if ($data['taxId']){
-            $tax = $this->tax->getTaxDetail($data['taxId']);
-            $data['totalTax'] = $data['subtotal'] * $tax->percentage/100;
-            $data['ammountDue'] = $data['ammountDue'] + $data['totalTax'];
-        }else{
-            $data['totalTax'] = 0;
-        }
-
-        $invoice = Invoice::create($data);
-
-        foreach ($data['products'] as $product){
-            $invoice->products()->attach($product['productId'], [
-                'qty' => $product['qty'],
-                'ammount' => $product['ammount']
-            ]);
-
-            $productDetail = $this->product->findProductById($product['productId']);
-            if ($productDetail->priceOption == 'subscription'){
-                $subscription = $this->storeSubscription($data['receiverUserId'], $product['ammount'], $product['productId']);
-                $invoice->subscriptions()->attach($subscription->id);
-            }
-        }
-        $this->midtransPayment($data, $invoice);
-
-        return $invoice;
-    }
-
-    public function calculateProductAmount(int $qty, $productId){
-        $product = $this->product->findProductById($productId);
-        $productPrice = $product->price;
-        $amount = $qty * $productPrice;
-        $cycle = $product->subscriptionCycle;
-
-        if ($cycle == 'monthly'){
-            $subscription = '/Month';
-        } elseif ($cycle == 'quarterly'){
-            $subscription = '/3 Month';
-        } elseif ($cycle == 'semianually'){
-            $subscription = '/6 Month';
-        } elseif ($cycle == 'anually'){
-            $subscription = '/Year';
-        } else {
-            $subscription = '';
-        }
-
-        return compact('productPrice', 'amount', 'subscription');
-    }
-
-    public function calculateInvoiceTotal(array $data){
-        $data['subtotal'] = 0;
-
-        foreach ($data['products'] as $product) {
-            $data['subtotal'] = $data['subtotal'] + $product['ammount'];
-        }
-        $data['ammountDue'] = $data['subtotal'];
-
-        if ($data['taxId']){
-            $tax = $this->tax->getTaxDetail($data['taxId']);
-            $data['totalTax'] = $data['subtotal'] * $tax->percentage;
-            $data['ammountDue'] = $data['ammountDue'] + $data['totalTax'];
-        }
-
-        return $data;
-    }
-
-    public function storeSubscription($userId, $ammount, $productId){
-        $data = [];
-        $data['startDate'] = Carbon::now();
-        $data['ammountDue'] = $ammount;
-        $data['status'] = 'scheduled';
-        $data['userId'] = $userId;
-        $data['productId'] = $productId;
-
-        $product = $this->product->findProductById($productId);
-
-        if ($product->subscriptionCycle == 'monthly'){
-            $data['cycle'] =  'monthly';
-            $data['nextDueDate'] = Carbon::now()->addMonthsNoOverflow(1);
-        } elseif ($product->subscriptionCycle == 'quarterly'){
-            $data['cycle'] =  'quarterly';
-            $data['nextDueDate'] = Carbon::now()->addMonthsNoOverflow(3);
-        } elseif ($product->subscriptionCycle == 'semianually'){
-            $data['cycle'] =  'semianually';
-            $data['nextDueDate'] = Carbon::now()->addMonthsNoOverflow(6);
-        } elseif ($product->subscriptionCycle == 'anually'){
-            $data['cycle'] =  'anually';
-            $data['nextDueDate'] = Carbon::now()->addMonthsNoOverflow(12);
-        }
-
-        return Subscription::create($data);
+    public function invoices(Subscription $subscription){
+        return Datatables::of($subscription->invoices)
+            ->addColumn('action', function ($item) {
+                return
+                    '<a class="btn btn-sm btn-outline-secondary" href="' . route('invoices.show', $item->id) . '" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <span class="material-icons">
+                            visibility
+                        </span>
+                        Show Invoice
+                    </a>';
+            })
+            ->editColumn('name', function ($item) {
+                return '
+                            <div class="media flex-nowrap align-items-center"
+                                 style="white-space: nowrap;">
+                                <div class="avatar avatar-sm mr-8pt">
+                                    <img class="rounded-circle header-profile-user img-object-fit-cover" width="40" height="40" src="' . Storage::url($item->receiverUser->foto) . '" alt="profile-pic"/>
+                                </div>
+                                <div class="media-body">
+                                    <div class="d-flex align-items-center">
+                                        <div class="flex d-flex flex-column">
+                                            <p class="mb-0"><strong class="js-lists-values-lead">' . $item->receiverUser->firstName . ' ' . $item->receiverUser->lastName . '</strong></p>
+                                            <small class="js-lists-values-email text-50">' . $item->receiverUser->roles[0]['name'] . '</small>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>';
+            })
+            ->editColumn('email', function ($item) {
+                return $item->receiverUser->email;
+            })
+            ->editColumn('ammount', function ($item) {
+                return $this->priceFormat($item->ammountDue);
+            })
+            ->editColumn('dueDate', function ($item) {
+                return $this->convertToDatetime($item->dueDate);
+            })
+            ->editColumn('createdAt', function ($item) {
+                return $this->convertToDatetime($item->created_at);
+            })
+            ->editColumn('updatedAt', function ($item) {
+                return $this->convertToDatetime($item->updatedAt);
+            })
+            ->editColumn('status', function ($item) {
+                $badge = '';
+                if ($item->status == 'Open') {
+                    $badge = '<span class="badge badge-pill badge-info">'.$item->status.'</span>';
+                } elseif ($item->status == 'Paid') {
+                    $badge = '<span class="badge badge-pill badge-success">'.$item->status.'</span>';
+                } elseif ($item->status == 'Past Due') {
+                    $badge = '<span class="badge badge-pill badge-warning">'.$item->status.'</span>';
+                } elseif ($item->status == 'Uncollectible') {
+                    $badge = '<span class="badge badge-pill badge-danger">'.$item->status.'</span>';
+                }
+                return $badge;
+            })
+            ->rawColumns(['action', 'email', 'ammount','dueDate', 'name', 'status', 'createdAt','updatedAt'])
+            ->addIndexColumn()
+            ->make();
     }
 
     public function show(Invoice $invoice)
