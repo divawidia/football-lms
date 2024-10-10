c968cf1104cc567e35ace5acf5067bc5d74a587f
divawidia <wiartha2001@gmail.com>
2024-10-10 14:50:57 +0800

    update index method in dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\DashboardService;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
+    private DashboardService $dashboardService;
+
+    public function __construct(DashboardService $dashboardService){
+        $this->dashboardService = $dashboardService;
+    }
     public function index()
     {
-        return view('pages.admins.dashboard');
+        $dataOverview = $this->dashboardService->overviewStats();
+        return view('pages.admins.dashboard', [
+            'dataOverview' => $dataOverview,
+        ]);
     }
 }
