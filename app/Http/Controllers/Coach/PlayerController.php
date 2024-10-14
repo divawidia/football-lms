bfbf605ae1d935fb3c98257a93b55016791006ac
divawidia <wiartha2001@gmail.com>
2024-10-14 13:54:14 +0800

    delete show method in player controller

--- app/Http/Controllers/Coach/PlayerController.php
+++ app/Http/Controllers/Coach/PlayerController.php
@@ -3,21 +3,7 @@
 namespace App\Http\Controllers\Coach;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\PlayerRequest;
-use App\Models\Admin;
-use App\Models\Player;
-use App\Models\PlayerParrent;
-use App\Models\PlayerPosition;
-use App\Models\Team;
-use App\Models\User;
 use App\Services\PlayerService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\Password;
-use Nnjeim\World\World;
-use RealRashid\SweetAlert\Facades\Alert;
 
 class PlayerController extends Controller
 {
@@ -37,13 +23,5 @@ class PlayerController extends Controller
             return $this->playerService->coachPlayerIndex($this->getLoggedCoachUser());
         }
         return view('pages.coaches.managements.players.index');
-    }
-
-    public function show(Player $player){
-        $overview = $this->playerService->show($player);
-        return view('pages.coaches.managements.players.detail', [
-            'data' => $player,
-            'overview' => $overview
-        ]);
     }
 }
