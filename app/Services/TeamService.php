36d49c45e5d2a74e2114e4332e4401e8da786395
divawidia <wiartha2001@gmail.com>
2024-10-16 11:20:20 +0800

    refactor index method in team controller

--- app/Services/TeamService.php
+++ app/Services/TeamService.php
@@ -2,54 +2,65 @@
 
 namespace App\Services;
 
-use App\Http\Controllers\Controller;
 use App\Models\Coach;
-use App\Models\OpponentTeam;
 use App\Models\Player;
 use App\Models\Team;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use Yajra\DataTables\Facades\DataTables;
 
 class TeamService extends Service
 {
-    public function index(): \Illuminate\Http\JsonResponse
+    public function indexDatatables($teamsData)
     {
-        $query = Team::with('coaches', 'players')->where('teamSide', 'Academy Team')->get();
-        return Datatables::of($query)->addColumn('action', function ($item) {
-                if ($item->status == '1') {
-                    $statusButton = '<form action="' . route('deactivate-team', $item->id) . '" method="POST">
-                                                    ' . method_field("PATCH") . '
-                                                    ' . csrf_field() . '
-                                                    <button type="submit" class="dropdown-item">
-                                                        <span class="material-icons">block</span> Deactivate Team
-                                                    </button>
-                                                </form>';
-                } else {
-                    $statusButton = '<form action="' . route('activate-team', $item->id) . '" method="POST">
-                                                    ' . method_field("PATCH") . '
-                                                    ' . csrf_field() . '
-                                                    <button type="submit" class="dropdown-item">
-                                                        <span class="material-icons">check_circle</span> Activate Team
-                                                    </button>
-                                                </form>';
+        return Datatables::of($teamsData)
+            ->addColumn('action', function ($item) {
+                $actionButton = '';
+                if (Auth::user()->hasRole('coach')){
+                    $actionButton =  '
+                          <a class="btn btn-sm btn-outline-secondary" href="' . route('coach.player-managements.show', $item->id) . '" data-toggle="tooltips" data-placement="bottom" title="View Player">
+                            <span class="material-icons">
+                                visibility
+                            </span>
+                          </a>';
+                } elseif (Auth::user()->hasRole('admin')){
+                    if ($item->status == '1') {
+                        $statusButton = '<form action="' . route('deactivate-team', $item->id) . '" method="POST">
+                                            ' . method_field("PATCH") . '
+                                            ' . csrf_field() . '
+                                            <button type="submit" class="dropdown-item">
+                                                <span class="material-icons">block</span> Deactivate Team
+                                            </button>
+                                        </form>';
+                    } else {
+                        $statusButton = '<form action="' . route('activate-team', $item->id) . '" method="POST">
+                                            ' . method_field("PATCH") . '
+                                            ' . csrf_field() . '
+                                            <button type="submit" class="dropdown-item">
+                                                <span class="material-icons">check_circle</span> Activate Team
+                                            </button>
+                                        </form>';
+                    }
+                    $actionButton =  '
+                        <div class="dropdown">
+                          <button class="btn btn-sm btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="material-icons">
+                                more_vert
+                            </span>
+                          </button>
+                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a class="dropdown-item" href="' . route('team-managements.edit', $item->id) . '"><span class="material-icons">edit</span> Edit Team</a>
+                            <a class="dropdown-item" href="' . route('team-managements.show', $item->id) . '"><span class="material-icons">visibility</span> View Team</a>
+                            ' . $statusButton . '
+                            <button type="button" class="dropdown-item delete-team" id="' . $item->id . '">
+                                <span class="material-icons">delete</span> Delete Team
+                            </button>
+                          </div>
+                        </div>';
                 }
-                return '
-                            <div class="dropdown">
-                              <button class="btn btn-sm btn-outline-secondary" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="material-icons">
-                                    more_vert
-                                </span>
-                              </button>
-                              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="' . route('team-managements.edit', $item->id) . '"><span class="material-icons">edit</span> Edit Team</a>
-                                <a class="dropdown-item" href="' . route('team-managements.show', $item->id) . '"><span class="material-icons">visibility</span> View Team</a>
-                                ' . $statusButton . '
-                                <button type="button" class="dropdown-item delete-team" id="' . $item->id . '">
-                                    <span class="material-icons">delete</span> Delete Team
-                                </button>
-                              </div>
-                            </div>';
-             })
+                return $actionButton;
+            })
             ->editColumn('players', function ($item) {
                 return count($item->players).' Player(s)';
             })
@@ -74,14 +85,23 @@ class TeamService extends Service
                             </div>';
             })
             ->editColumn('status', function ($item) {
+                $badge = '';
                 if ($item->status == '1') {
-                    return '<span class="badge badge-pill badge-success">Aktif</span>';
+                    $badge = '<span class="badge badge-pill badge-success">Active</span>';
                 } elseif ($item->status == '0') {
-                    return '<span class="badge badge-pill badge-danger">Non Aktif</span>';
+                    $badge = '<span class="badge badge-pill badge-danger">Non-Active</span>';
                 }
+                return $badge;
             })
             ->rawColumns(['action', 'name', 'status', 'players', 'coaches'])
+            ->addIndexColumn()
             ->make();
+    }
+
+    public function index(): JsonResponse
+    {
+        $query = Team::with('coaches', 'players')->where('teamSide', 'Academy Team')->get();
+        return $this->indexDatatables($query);
     }
 
     public function teamPlayers(Team $team){
