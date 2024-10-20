499973ba2f7b3f7d29c577f1b289f1df8f240a78
divawidia <wiartha2001@gmail.com>
2024-10-20 14:50:04 +0800

    update admin detail page

--- resources/views/pages/admins/managements/admins/detail.blade.php
+++ resources/views/pages/admins/managements/admins/detail.blade.php
@@ -6,6 +6,10 @@
     @yield('title')
 @endsection
 
+@section('modal')
+    @include('pages.admins.managements.admins.form-modal.change-password')
+@endsection
+
 @section('content')
     <nav class="navbar navbar-light border-bottom border-top px-0">
         <div class="container page__container">
@@ -18,17 +22,55 @@
     </nav>
     <div class="page-section bg-primary">
         <div class="container page__container d-flex flex-column flex-md-row align-items-center text-center text-md-left">
-            <img src="{{ Storage::url($user->foto) }}"
+            <img src="{{ Storage::url($data->user->foto) }}"
                  width="104"
                  height="104"
                  class="mr-md-32pt mb-3 mb-md-0 rounded-circle img-object-fit-cover"
                  alt="player-photo">
             <div class="flex mb-3 mb-md-0 ml-md-4">
                 <h2 class="text-white mb-0">{{ $fullName  }}</h2>
-                <p class="lead text-white-50 d-flex align-items-center">{{ $user->admin->position }}</p>
+                <p class="lead text-white-50 d-flex align-items-center">{{ $data->position }}</p>
             </div>
-            <a href="{{ route('admin-managements.edit', $user->id) }}"
-               class="btn btn-outline-white">Edit Profile</a>
+            <div class="dropdown">
+                <button class="btn btn-outline-white" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                    Action
+                    <span class="material-icons ml-3">
+                        keyboard_arrow_down
+                    </span>
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="{{ route('admin-managements.edit', $data->id) }}">
+                        <span class="material-icons mr-2 text-primary">edit</span>
+                        Edit admin profile
+                    </a>
+                    @if($data->user->status == '1')
+                        <form action="{{ route('deactivate-admin', $data->id) }}" method="POST">
+                            @method("PATCH")
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <span class="material-icons mr-2 text-danger">block</span> Deactivate admin
+                            </button>
+                        </form>
+                    @else
+                        <form action="{{ route('activate-admin', $data->id) }}" method="POST">
+                            @method("PATCH")
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <span class="material-icons mr-2 text-success">check_circle</span> Activate admin
+                            </button>
+                        </form>
+                    @endif
+                    <a class="dropdown-item changePassword" id="{{ $data->id }}">
+                        <span class="material-icons mr-2 text-primary">lock</span>
+                        Change admins Account Password
+                    </a>
+                    <button type="button" class="dropdown-item deleteAdmin" id="{{$data->id}}">
+                        <span class="material-icons mr-2 text-danger">delete</span>
+                        Delete admin
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -44,31 +86,31 @@
                     <div class="card-body flex-column">
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Email :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->email }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->email }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Phone Number :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->phoneNumber }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->phoneNumber }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Last Seen :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($user->lastSeen)) }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($data->user->lastSeen)) }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Date of Birth :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ date('M d, Y', strtotime($user->dob)) }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ date('M d, Y', strtotime($data->user->dob)) }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Gender :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->gender }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->gender }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Hired Date :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ date('M d, Y', strtotime($user->admin->hireDate)) }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ date('M d, Y', strtotime($data->hireDate)) }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Created At :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($user->created_at)) }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($data->user->created_at)) }}</div>
                         </div>
                     </div>
                 </div>
@@ -78,35 +120,35 @@
                     <div class="card-body flex-column">
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Status :</p></div>
-                            @if($user->status == '1')
+                            @if($data->user->status == '1')
                                 <span class="ml-auto p-2 badge badge-pill badge-success">Aktif</span>
-                            @elseif($user->status == '0')
+                            @elseif($data->user->status == '0')
                                 <span class="ml-auto p-2 badge badge-pill badge-danger">Non Aktif</span>
                             @endif
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Address :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->address }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->address }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Country :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->country->name }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->country->name }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">State :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->state->name }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->state->name }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">City :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->city->name }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->city->name }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Zip Code :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ $user->zipCode }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ $data->user->zipCode }}</div>
                         </div>
                         <div class="d-flex align-items-center">
                             <div class="p-2"><p class="card-title mb-4pt">Last Updated :</p></div>
-                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($user->updated_at)) }}</div>
+                            <div class="ml-auto p-2 text-muted">{{ date('l, M d, Y. h:i A', strtotime($data->user->updated_at)) }}</div>
                         </div>
                     </div>
                 </div>
@@ -114,3 +156,100 @@
         </div>
     </div>
 @endsection
+
+@push('addon-script')
+    <script>
+        $(document).ready(function () {
+            const body = $('body');
+
+            body.on('click', '.changePassword',function (e) {
+                const id = $(this).attr('id');
+                e.preventDefault();
+                $('#changePasswordAdminModal').modal('show');
+                $('#userId').val(id);
+            })
+            // update admin password
+            $('#formChangePasswordAdminModal').on('submit', function (e) {
+                e.preventDefault();
+                const id = $('#userId').val();
+                $.ajax({
+                    url: "{{ route('admin-managements.change-password', ['admin' => ":id"]) }}".replace(':id', id),
+                    type: $(this).attr('method'),
+                    data: new FormData(this),
+                    contentType: false,
+                    processData: false,
+                    success: function () {
+                        $('#changePasswordAdminModal').modal('hide');
+                        Swal.fire({
+                            title: 'Accounts password successfully updated!',
+                            icon: 'success',
+                            showCancelButton: false,
+                            allowOutsideClick: false,
+                            confirmButtonColor: "#1ac2a1",
+                            confirmButtonText:
+                                'Ok!'
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                location.reload();
+                            }
+                        });
+                    },
+                    error: function (xhr) {
+                        const response = JSON.parse(xhr.responseText);
+                        console.log(response);
+                        $.each(response.errors, function (key, val) {
+                            $('span.' + key + '_error').text(val[0]);
+                            $("#add_" + key).addClass('is-invalid');
+                        });
+                    }
+                });
+            });
+
+            // delete data alert
+            body.on('click', '.deleteAdmin', function () {
+                const id = $(this).attr('id');
+                Swal.fire({
+                    title: "Are you sure to delete this admin account?",
+                    text: "You won't be able to revert this!",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#1ac2a1",
+                    cancelButtonColor: "#E52534",
+                    confirmButtonText: "Yes, delete it!"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: "{{ route('admin-managements.destroy', ['admin' => ':id']) }}".replace(':id', id),
+                            type: 'DELETE',
+                            data: {
+                                _token: "{{ csrf_token() }}"
+                            },
+                            success: function () {
+                                Swal.fire({
+                                    title: 'Admin successfully archived!',
+                                    icon: 'success',
+                                    showCancelButton: false,
+                                    allowOutsideClick: false,
+                                    confirmButtonColor: "#1ac2a1",
+                                    confirmButtonText:
+                                        'Ok!'
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        window.location.href = '{{ route('admin-managements.index') }}';
+                                    }
+                                });
+                            },
+                            error: function (jqXHR, textStatus, errorThrown) {
+                                Swal.fire({
+                                    icon: "error",
+                                    title: "Something went wrong when deleting data!",
+                                    text: errorThrown
+                                });
+                            }
+                        });
+                    }
+                });
+            });
+        });
+    </script>
+@endpush
