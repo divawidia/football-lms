2ea7bd965a696f702b4f3a4a0be7a0dcb3a85d95
divawidia <wiartha2001@gmail.com>
2024-10-21 21:24:10 +0800

    update helper file

--- app/helpers.php
+++ app/helpers.php
@@ -5,7 +5,15 @@ use Illuminate\Support\Facades\Auth;
 function getLoggedUser(){
     return Auth::user();
 }
+function isSuperAdmin()
+{
+    return getLoggedUser()->hasRole('Super-Admin');
+}
 function isAdmin()
+{
+    return getLoggedUser()->hasRole('admin');
+}
+function isAllAdmin()
 {
     return getLoggedUser()->hasRole('admin|Super-Admin');
 }
