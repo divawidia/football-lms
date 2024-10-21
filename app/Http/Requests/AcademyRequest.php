5a9d943ef197430e95cc8e9acecad639285fb199
divawidia <wiartha2001@gmail.com>
2024-10-21 21:54:09 +0800

    create academy request

---
+++ app/Http/Requests/AcademyRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class AcademyRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'academyName' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'address' => ['required', 'string'],
+            'phoneNumber' => ['required', 'string'],
+            'zipCode' => ['required', 'numeric'],
+            'directorName' => ['required', 'numeric'],
+            'logo' => ['image', 'max:1024', 'nullable'],
+            'country_id' => ['required', Rule::exists('countries', 'id')],
+            'state_id' => ['required', Rule::exists('states', 'id')],
+            'city_id' => ['required', Rule::exists('cities', 'id')],
+        ];
+    }
+}
