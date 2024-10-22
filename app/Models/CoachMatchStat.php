6a34ba587e0470ea50765c7c72d1ea06a79d0707
divawidia <wiartha2001@gmail.com>
2024-10-23 00:17:41 +0800

    create coach match stats model

---
+++ app/Models/CoachMatchStat.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CoachMatchStat extends Model
+{
+    use HasFactory;
+    protected $table ='coach_match_stats';
+
+    protected $fillable = [
+        'coachId',
+        'teamId',
+        'eventId',
+        'teamScore',
+        'opponentTeamScore',
+        'teamOwnGoal',
+        'teamPossesion',
+        'teamShotOnTarget',
+        'teamShots',
+        'teamTouches',
+        'teamTackles',
+        'teamClearances',
+        'teamCorners',
+        'teamOffsides',
+        'teamYellowCards',
+        'teamRedCards',
+        'teamFoulsConceded',
+        'resultStatus',
+        'teamPasses',
+    ];
+
+    public function coach()
+    {
+        $this->belongsTo(Coach::class, 'coachId', 'id');
+    }
+
+    public function team()
+    {
+        $this->belongsTo(Team::class, 'teamId', 'id');
+    }
+
+    public function match()
+    {
+        $this->belongsTo(EventSchedule::class, 'eventId', 'id');
+    }
+}
