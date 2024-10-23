13dc8ab8c53ad2ebf9743d698cad4dfe05b8976a
divawidia <wiartha2001@gmail.com>
2024-10-23 23:03:33 +0800

    rafactor overviewStats method in dashboard service

--- app/Services/Coach/DashboardService.php
+++ app/Services/Coach/DashboardService.php
@@ -25,103 +25,14 @@ class DashboardService extends CoachService
     public function overviewStats(){
         $teamsManaged = $this->managedTeams($this->coach);
 
-        $totalMatchPlayed = $this->coach->schedules()->where('status', '0')
-                ->where('eventType', 'Match')
-                ->count();
-
-        $matches = $this->coach->schedules()->where('eventType', 'Match')
-            ->whereHas('matches', function ($q) use ($teamsManaged){
-                $q->where('teamId', $teamsManaged[0]->id);
-                if (count($teamsManaged) > 1){
-                    for ($i = 1; $i < count($teamsManaged); $i++){
-                        $q->orWhere('teamId', $teamsManaged[$i]->id);
-                    }
-                }
-        })->get();
-
-        $totalGoals =  $this->coach->schedules()->teams()->whereHas('team', function($q){
-                    $q->where('teamSide', 'Academy Team');
-                })->sum('teamScore');
-        $totalGoalsConceded = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Opponent Team');
-        })->sum('teamScore');
-        $totalCleanSheets = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Academy Team');
-        })->sum('cleanSheets');;
-        $totalOwnGoals = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Academy Team');
-        })->sum('teamOwnGoal');
-        $totalWins = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Academy Team');
-        })->where('resultStatus', 'Win')->count();
-        $totalLosses = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Academy Team');
-        })->where('resultStatus', 'Lose')->count();
-        $totalDraws = $this->coach->schedules()->teams()->whereHas('team', function($q){
-            $q->where('teamSide', 'Academy Team');
-        })->where('resultStatus', 'Draw')->count();
-
-//        $totalMatchPlayed = 0;
-//        $totalGoals = 0;
-//        $totalGoalsConceded = 0;
-//        $totalCleanSheets = 0;
-//        $totalOwnGoals = 0;
-//        $totalWins = 0;
-//        $totalLosses = 0;
-//        $totalDraws = 0;
-
-//        foreach ($teamsManaged as $team){
-//            $matchPlayed = EventSchedule::whereHas('teams', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })
-//                ->where('status', '0')
-//                ->where('eventType', 'Match')
-//                ->count();
-//            $goals = TeamMatch::whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })->sum('teamScore');
-//            $goalsConceded = TeamMatch::whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Opponent Team');
-//                    $q->where('teamId', $team->id);
-//                })
-//                ->sum('teamScore');
-//            $cleanSheets = TeamMatch::whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })
-//                ->sum('cleanSheets');
-//            $ownGoals = TeamMatch::whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })
-//                ->sum('teamOwnGoal');
-//            $wins = TeamMatch::where('resultStatus', 'Win')
-//                ->whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })->count();
-//            $losses = TeamMatch::where('resultStatus', 'Lose')
-//                ->whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })->count();
-//            $draws = TeamMatch::where('resultStatus', 'Draw')
-//                ->whereHas('team', function($q) use ($team) {
-//                    $q->where('teamSide', 'Academy Team');
-//                    $q->where('teamId', $team->id);
-//                })->count();
-//
-//            $totalMatchPlayed += $matchPlayed;
-//            $totalGoals += $goals;
-//            $totalGoalsConceded += $goalsConceded;
-//            $totalCleanSheets += $cleanSheets;
-//            $totalOwnGoals += $ownGoals;
-//            $totalWins += $wins;
-//            $totalLosses += $losses;
-//            $totalDraws += $draws;
-//        }
+        $totalMatchPlayed = $this->coach->coachMatchStats()->count();
+        $totalGoals =  $this->coach->coachMatchStats()->sum('teamScore');
+        $totalGoalsConceded =  $this->coach->coachMatchStats()->sum('goalConceded');
+        $totalCleanSheets = $this->coach->coachMatchStats()->sum('cleanSheets');
+        $totalOwnGoals = $this->coach->coachMatchStats()->sum('teamOwnGoal');
+        $totalWins = $this->coach->coachMatchStats()->where('resultStatus', 'Win')->count();
+        $totalLosses = $this->coach->coachMatchStats()->where('resultStatus', 'Lose')->count();
+        $totalDraws = $this->coach->coachMatchStats()->where('resultStatus', 'Draw')->count();
 
         $goalsDifference = $totalGoals - $totalGoalsConceded;
 
