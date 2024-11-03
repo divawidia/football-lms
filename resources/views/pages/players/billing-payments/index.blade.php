a60740f24b7d51b4d888502ab98419b1839a4389
divawidia <wiartha2001@gmail.com>
2024-11-03 16:00:35 +0800

    update billing payments index

--- resources/views/pages/players/billing-payments/index.blade.php
+++ resources/views/pages/players/billing-payments/index.blade.php
@@ -11,7 +11,7 @@
         <div class="container">
             <h2 class="mb-0 text-left">@yield('title')</h2>
             <ol class="breadcrumb p-0 m-0">
-                <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('player.dashboard') }}">Home</a></li>
                 <li class="breadcrumb-item active">
                     @yield('title')
                 </li>
@@ -21,19 +21,29 @@
 
     <div class="container page-section">
         <div class="page-separator">
-            <div class="page-separator__text">Invoices</div>
-            <a href="{{ route('invoices.archived') }}" class="btn btn-sm btn-white border-danger ml-auto ">
-                <span class="material-icons mr-2 text-danger">
-                    delete
-                </span>
-                Archived Invoice
-            </a>
-            <a href="{{ route('invoices.create') }}" class="btn btn-sm btn-primary ml-3 ">
-                <span class="material-icons mr-2">
-                    add
-                </span>
-                Add New
-            </a>
+            <div class="page-separator__text">outstanding payments</div>
+        </div>
+        <div class="alert alert-soft-warning mb-lg-32pt">
+            <div class="d-flex flex-wrap align-items-center">
+                <div class="mr-8pt">
+                    <i class="material-icons">access_time</i>
+                </div>
+                <div class="flex"
+                     style="min-width: 180px">
+                    <small class="text-100">
+                        Please pay your amount due of
+                        <strong>&dollar;9.00</strong> for invoice <a href="billing-invoice.html"
+                                                                     class="text-underline">10002331</a>
+                    </small>
+                </div>
+                <a href="billing-payment.html"
+                   class="btn btn-sm btn-link">Pay Now</a>
+            </div>
+        </div>
+
+
+        <div class="page-separator">
+            <div class="page-separator__text">payments History</div>
         </div>
         <div class="card dashboard-area-tabs p-relative o-hidden mb-lg-32pt">
             <div class="card-body">
@@ -43,8 +53,6 @@
                         <tr>
                             <th>#</th>
                             <th>Inovice Number</th>
-                            <th>Name</th>
-                            <th>Email</th>
                             <th>Amount Due</th>
                             <th>Due Date</th>
                             <th>Status</th>
@@ -76,8 +84,6 @@
                 columns: [
                     {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
                     {data: 'invoiceNumber', name: 'invoiceNumber'},
-                    {data: 'name', name: 'name'},
-                    {data: 'email', name: 'email'},
                     {data: 'ammount', name: 'ammount'},
                     {data: 'dueDate', name: 'dueDate'},
                     {data: 'status', name: 'status'},
@@ -91,52 +97,6 @@
                         width: '15%'
                     },
                 ]
-            });
-
-            // archive invoice
-            body.on('click', '.deleteInvoice', function () {
-                const id = $(this).attr('id');
-                Swal.fire({
-                    title: "Are you sure to archive this invoice?",
-                    text: "You won't be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#1ac2a1",
-                    cancelButtonColor: "#E52534",
-                    confirmButtonText: "Yes, archive it!"
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        $.ajax({
-                            url: "{{ route('invoices.destroy', ['invoice' => ':id']) }}".replace(':id', id),
-                            type: 'DELETE',
-                            data: {
-                                _token: "{{ csrf_token() }}"
-                            },
-                            success: function () {
-                                Swal.fire({
-                                    title: 'Invoice successfully archived!',
-                                    icon: 'success',
-                                    showCancelButton: false,
-                                    allowOutsideClick: false,
-                                    confirmButtonColor: "#1ac2a1",
-                                    confirmButtonText:
-                                        'Ok!'
-                                }).then((result) => {
-                                    if (result.isConfirmed) {
-                                        location.reload();
-                                    }
-                                });
-                            },
-                            error: function (jqXHR, textStatus, errorThrown) {
-                                Swal.fire({
-                                    icon: "error",
-                                    title: "Something went wrong when archiving data!",
-                                    text: errorThrown
-                                });
-                            }
-                        });
-                    }
-                });
             });
         });
     </script>
