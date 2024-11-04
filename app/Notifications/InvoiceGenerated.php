eb22c085e9e5a96cdba942312ec79dae5737359e
divawidia <wiartha2001@gmail.com>
2024-11-04 11:47:20 +0800

    create invoice generated notification class

---
+++ app/Notifications/InvoiceGenerated.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoiceGenerated extends Notification
+{
+    use Queueable;
+    protected $amount;
+    protected $duedate;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($amount, $duedate)
+    {
+        $this->amount = $amount;
+        $this->duedate = $duedate;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' =>'An invoice of '. $this->amount.' has been generated. Please complete the payment before the due date at '.$this->duedate.'!'
+        ];
+    }
+}
