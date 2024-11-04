b2f59c91e5167fd1c39b6218dd3d956d0c1358fd
divawidia <wiartha2001@gmail.com>
2024-11-04 23:19:46 +0800

    create invoice due notification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,8 +6,12 @@ use App\Console\Commands\DeactivateTrainingStatus;
 use App\Console\Commands\EndCompetitionStatus;
 use App\Console\Commands\EndMatchStatus;
 use App\Console\Commands\SetPastDueInvoiceStatus;
+use App\Models\Invoice;
+use App\Notifications\InvoiceDueSoon;
+use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
 
 class Kernel extends ConsoleKernel
 {
@@ -22,10 +26,16 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-         $schedule->command('update:training-status-data')->everyFiveMinutes();
-         $schedule->command('update:end-match-status')->everyFiveMinutes();
-         $schedule->command('update:end-competition-status')->everyFiveMinutes();
-         $schedule->command('update:set-past-due-invoice-status')->everyFiveMinutes();
+         $schedule->command('update:training-status-data')->everyMinute();
+         $schedule->command('update:end-match-status')->everyMinute();
+         $schedule->command('update:end-competition-status')->everyMinute();
+         $schedule->command('update:set-past-due-invoice-status')->everyMinute();
+        $schedule->call(function () {
+            $invoices = Invoice::where('dueDate', '=', Carbon::now()->addHour())->where('status', 'Open')->get();
+            foreach ($invoices as $invoice) {
+                $invoice->receiverUser->notify(new InvoiceDueSoon($invoice->dueDate, $invoice->id));
+            }
+        })->everyMinute();
     }
 
     /**
---
+++ app/Notifications/InvoiceDueSoon.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class InvoiceDueSoon extends Notification
+{
+    use Queueable;
+    protected $duedate;
+    protected $invoiceId;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($duedate, $invoiceId)
+    {
+        $this->duedate = $duedate;
+        $this->invoiceId = $invoiceId;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('The introduction to the notification.')
+                    ->action('Notification Action', url('/'))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' =>'Your payment is due soon. Please pay by '.$this->duedate.' to avoid late fees.',
+            'redirectRoute' => route('billing-and-payments.show', ['invoice' => $this->invoiceId])
+        ];
+    }
+}
