73e4e811beb58307af5bee19724eb5910df010f1
divawidia <wiartha2001@gmail.com>
2024-11-08 16:22:09 +0800

    update script.sh

--- scripts/.script.sh
+++ scripts/.script.sh
@@ -6,28 +6,35 @@ echo "Deployment started ..."
 # Enter maintenance mode or return true
 # if already is in maintenance mode
 (php artisan down) || true
+echo "Maintenance mode successfully activated ..."
 
 # Pull the latest version of the app
 git reset --hard
 git pull origin main
+echo "Successfully pulling from remote repository  ..."
 
 # Install composer dependencies
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+echo "Successfully install composer dependencies ..."
 
 composer dump-autoload
+echo "Successfully dump autoload ..."
 
 # Clear the old cache
 php artisan clear-compiled
 
 # Recreate cache
 php artisan optimize
+echo "Successfully run php optimize ..."
 
 # Compile npm assets
 npm install
 npm run build
+echo "Successfully compile npm ..."
 
 # Run database migrations
 php artisan migrate --force
+echo "Successfully run migration ..."
 
 php artisan optimize:clear
 
