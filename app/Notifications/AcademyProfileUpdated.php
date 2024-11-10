6fa1b9fe4dfa1f858e064a64faf7fb95603f6d6b
divawidia <wiartha2001@gmail.com>
2024-11-11 00:26:00 +0800

    create academy profile updated notification

---
+++ app/Notifications/AcademyProfileUpdated.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AcademyProfileUpdated extends Notification
+{
+    use Queueable;
+    protected $name;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' =>"{$this->name} has updated the academy profile. Please review the changes if necessary.",
+            'redirectRoute' => route('edit-academy.edit')
+        ];
+    }
+}
