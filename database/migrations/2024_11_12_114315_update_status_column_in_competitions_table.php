48d54f6ad6f849304abf76b36d794b29fe61b00b
divawidia <wiartha2001@gmail.com>
2024-11-12 12:43:59 +0800

    add migration for update_status_column_in_competitions_table

---
+++ database/migrations/2024_11_12_114315_update_status_column_in_competitions_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->enum('status', ['Scheduled', 'Completed', 'Ongoing', 'Cancelled'])->default('Scheduled')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('competitions', function (Blueprint $table) {
+            $table->enum('status', ['0', '1'])->default('1')->change();
+        });
+    }
+};
