e3d0ff85a5bbad33cd87e1f1f4185d0e4c053564
divawidia <wiartha2001@gmail.com>
2024-11-13 23:57:14 +0800

    create TrainingScheduleUpdated notification

---
+++ app/Notifications/TrainingSchedules/TrainingScheduleUpdatedForPlayer.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications\TrainingSchedules;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class TrainingScheduleUpdatedForPlayerCoach extends Notification
+{
+    use Queueable;
+    protected $trainingSchedule;
+    protected $team;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($trainingSchedule, $team)
+    {
+        $this->trainingSchedule = $trainingSchedule;
+        $this->team = $team;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject("New Training Session Scheduled")
+            ->greeting("Hello!")
+            ->line("The training session {$this->trainingSchedule->eventName} for your team {$this->team->teamName} on ".convertToDatetime($this->trainingSchedule->startDatetime)." has been updated.")
+            ->action('View training session detail', route('training-schedules.show', $this->trainingSchedule->id))
+            ->line("Please log in to view the details!")
+            ->line("If you have any questions or require further information, please don't hesitate to reach out.!");
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'data' =>'The training session '.$this->trainingSchedule->eventName.' for your team '.$this->team->teamName.' on '.convertToDatetime($this->trainingSchedule->startDatetime).' has been updated. Please check the schedule for details!',
+            'redirectRoute' => route('training-schedules.show', $this->trainingSchedule->id)
+        ];
+    }
+}
