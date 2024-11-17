a43174a038e615cbeee240c93cd0f1f81c4e92b8
divawidia <wiartha2001@gmail.com>
2024-11-17 08:54:44 +0800

    update email template

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,12 @@
+<tr>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+            <tr>
+                <td class="content-cell" align="center">
+{{--                    {{ Illuminate\Mail\Markdown::parse($slot) }}--}}
+                    Copyright {{ date('Y') }} {{academyData()->academyName}}. All rights reserved.
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
