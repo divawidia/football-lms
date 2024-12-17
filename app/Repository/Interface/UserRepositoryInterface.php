ae9110189e1bb1eb7d7f6241e2e57821734b931c
divawidia <wiartha2001@gmail.com>
2024-12-17 08:49:08 +0800

    feat (UserRepositoryInterface) : create UserRepositoryInterface

---
+++ app/Repository/Interface/UserRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Repository\Interface;
+
+use App\Models\Team;
+use App\Models\User;
+
+interface UserRepositoryInterface
+{
+    public function getAll();
+
+    public function getInArray($relation,$ids);
+
+    public function getAllUserWithoutLoggedUserData($authUserId);
+
+    public function getAllByRole($role);
+
+    public function getAllAdminUsers();
+
+    public function allTeamsParticipant(Team $team, $admins = true, $coaches = true, $players = true);
+
+    public function find($id);
+
+    public function createUserWithRole(array $data, $role);
+
+    public function updateUserStatus($userModel, $status = ['1', '0']);
+
+    public function changePassword($data, $userModel);
+
+    public function update(array $data);
+
+    public function delete($userModel);
+}
