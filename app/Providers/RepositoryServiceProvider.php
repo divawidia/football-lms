333eeaa95c96986d1608cbac8c07931b0431d5df
divawidia <wiartha2001@gmail.com>
2024-12-18 16:54:28 +0800

    feat (repository service provider) : add Team repository interface binding

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 use App\Repository\AdminRepository;
 use App\Repository\Interface\AdminRepositoryInterface;
 use App\Repository\Interface\PlayerRepositoryInterface;
+use App\Repository\Interface\TeamRepositoryInterface;
 use App\Repository\Interface\UserRepositoryInterface;
 use App\Repository\PlayerRepository;
+use App\Repository\TeamRepository;
 use App\Repository\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +22,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(AdminRepositoryInterface::class, AdminRepository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
         $this->app->bind(PlayerRepositoryInterface::class, PlayerRepository::class);
+        $this->app->bind(TeamRepositoryInterface::class, TeamRepository::class);
     }
 
     /**
