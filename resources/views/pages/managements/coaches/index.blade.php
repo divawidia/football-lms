2ff77824c11bfa62362226e861e9feeec3ba2c02
divawidia <wiartha2001@gmail.com>
2024-12-19 18:52:35 +0800

    refactor (coach management index) : change datatable script

--- resources/views/pages/managements/coaches/index.blade.php
+++ resources/views/pages/managements/coaches/index.blade.php
@@ -34,30 +34,31 @@
                 Add New Coach
             </a>
         @endif
-        <div class="card">
+
             <div class="card card-form d-flex flex-column flex-sm-row">
                 <div class="card-form__body card-body-form-group flex">
                     <div class="row">
                         <div class="col-lg-3">
                             <div class="form-group mb-0 mb-lg-3">
-                                <label class="form-label mb-0" for="position">Filter by Specialization</label>
-                                <select class="form-control form-select" id="position" data-toggle="select">
-                                    <option selected disabled>Select player's position</option>
-                                    @foreach($positions as $position)
-                                        <option value="{{ $position->id }}">{{ $position->name }}</option>
+                                <label class="form-label mb-0" for="certification">Filter by certification</label>
+                                <select class="form-control form-select" id="certification" data-toggle="select">
+                                    <option selected disabled>Select certification</option>
+                                    @foreach($certifications as $certification)
+                                        <option value="{{ $certification->id }}">{{ $certification->name }}</option>
                                     @endforeach
-                                    <option value="{{ null }}">All teams</option>
+                                    <option value="{{ null }}">All certification</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-4">
                             <div class="form-group">
-                                <label class="form-label mb-0" for="skill">Filter by Certification</label>
-                                <select class="form-control form-select" id="skill" data-toggle="select">
-                                    <option selected disabled>Select player's skill level</option>
-                                    @foreach(['Beginner', 'Intermediate', 'Advance'] as $skills)
-                                        <option value="{{ $skills }}">{{ $skills }}</option>
+                                <label class="form-label mb-0" for="specializations">Filter by specializations</label>
+                                <select class="form-control form-select" id="specializations" data-toggle="select">
+                                    <option selected disabled>Select specializations</option>
+                                    @foreach($specializations as $specialization)
+                                        <option value="{{ $specialization->id }}">{{ $specialization->name }}</option>
                                     @endforeach
+                                    <option value="{{ null }}">All specializations</option>
                                 </select>
                             </div>
                         </div>
@@ -65,7 +66,7 @@
                             <div class="form-group">
                                 <label class="form-label mb-0" for="team">Filter by team</label>
                                 <select class="form-control form-select" id="team" data-toggle="select">
-                                    <option selected disabled>Select player's team</option>
+                                    <option selected disabled>Select team</option>
                                     @foreach($teams as $team)
                                         <option value="{{ $team->id }}" data-avatar-src="{{ Storage::url($team->logo) }}">{{ $team->teamName }}</option>
                                     @endforeach
@@ -73,11 +74,11 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="col-lg-3">
+                        <div class="col-lg-2">
                             <div class="form-group">
                                 <label class="form-label mb-0" for="status">Filter by status</label>
                                 <select class="form-control form-select" id="status" data-toggle="select">
-                                    <option selected disabled>Select player's status</option>
+                                    <option selected disabled>Select status</option>
                                     @foreach(['Active' => '1', 'Non-active' => '0', 'All Status' => null] as $statusLabel => $statusVal)
                                         <option value="{{ $statusVal }}">{{ $statusLabel }}</option>
                                     @endforeach
@@ -88,6 +89,8 @@
                 </div>
                 <button class="btn bg-alt border-left border-top border-top-sm-0 rounded-0" type="button" id="filterBtn"><i class="material-icons text-primary icon-20pt">refresh</i></button>
             </div>
+
+        <div class="card">
             <div class="card-body">
                 <div class="table-responsive">
                     <table class="table table-hover mb-0" id="table">
@@ -110,56 +113,85 @@
             </div>
         </div>
     </div>
-
-    @if(isAllAdmin())
-        <x-process-data-confirmation btnClass=".setDeactivate"
-                                     :processRoute="route('deactivate-coach', ':id')"
-                                     :routeAfterProcess="route('coach-managements.index')"
-                                     method="PATCH"
-                                     confirmationText="Are you sure to deactivate this coach account's status?"
-                                     errorText="Something went wrong when deactivating this coach account!"/>
-
-        <x-process-data-confirmation btnClass=".setActivate"
-                                     :processRoute="route('activate-coach', ':id')"
-                                     :routeAfterProcess="route('coach-managements.index')"
-                                     method="PATCH"
-                                     confirmationText="Are you sure to activate this coach account's status?"
-                                     errorText="Something went wrong when activating this coach account!"/>
-
-        <x-process-data-confirmation btnClass=".delete-user"
-                                     :processRoute="route('coach-managements.destroy', ['coach' => ':id'])"
-                                     :routeAfterProcess="route('coach-managements.index')"
-                                     method="DELETE"
-                                     confirmationText="Are you sure to delete this coach account?"
-                                     errorText="Something went wrong when deleting this coach account!"/>
-    @endif
 @endsection
 @push('addon-script')
-    <script>
+    <script type="module">
+        import { processWithConfirmation } from "{{ Vite::asset('resources/js/ajax-processing-data.js') }}" ;
         $(document).ready(function () {
-            $('#table').DataTable({
-                processing: true,
-                serverSide: true,
-                ordering: true,
-                ajax: {
-                    url: '{!! url()->current() !!}',
-                },
-                columns: [
-                    {data: 'name', name: 'name'},
-                    {data: 'teams', name: 'teams'},
-                    {data: 'user.email', name: 'user.email'},
-                    {data: 'user.phoneNumber', name: 'user.phoneNumber'},
-                    {data: 'age', name: 'age'},
-                    {data: 'user.gender', name: 'user.gender'},
-                    {data: 'status', name: 'status'},
-                    {
-                        data: 'action',
-                        name: 'action',
-                        orderable: false,
-                        searchable: false
+
+            function coachsTable(certification = null, specializations = null, team = null, status = null) {
+                $('#table').DataTable({
+                    processing: true,
+                    serverSide: true,
+                    ordering: true,
+                    ajax: {
+                        url: '{!! url()->current() !!}',
+                        type: "GET",
+                        data: {
+                            certification: certification,
+                            specializations: specializations,
+                            team: team,
+                            status: status
+                        }
                     },
-                ]
+                    columns: [
+                        {data: 'name', name: 'name'},
+                        {data: 'teams', name: 'teams'},
+                        {data: 'user.email', name: 'user.email'},
+                        {data: 'user.phoneNumber', name: 'user.phoneNumber'},
+                        {data: 'age', name: 'age'},
+                        {data: 'user.gender', name: 'user.gender'},
+                        {data: 'status', name: 'status'},
+                        {
+                            data: 'action',
+                            name: 'action',
+                            orderable: false,
+                            searchable: false
+                        },
+                    ],
+                    bDestroy: true
+                });
+            }
+
+            $('#filterBtn').on('click', function () {
+                const certification = $('#certification').val();
+                const specializations = $('#specializations').val();
+                const team = $('#team').val();
+                const status = $('#status').val();
+                coachsTable(certification, specializations, team, status);
             });
+
+            coachsTable();
+
+            processWithConfirmation(
+                '.setDeactivate',
+                "{{ route('deactivate-coach', ':id') }}",
+                "{{ route('coach-managements.index') }}",
+                'PATCH',
+                "Are you sure to deactivate this coach account's status?",
+                "Something went wrong when deactivating this coach account!",
+                "{{ csrf_token() }}"
+            );
+
+            processWithConfirmation(
+                '.setActivate',
+                "{{ route('deactivate-coach', ':id') }}",
+                "{{ route('coach-managements.index') }}",
+                'PATCH',
+                "Are you sure to setActivate this coach account's status?",
+                "Something went wrong when setActivating this coach account!",
+                "{{ csrf_token() }}"
+            );
+
+            processWithConfirmation(
+                '.delete-user',
+                "{{ route('coach-managements.destroy', ['coach' => ':id']) }}",
+                "{{ route('coach-managements.index') }}",
+                'PATCH',
+                "Are you sure to delete this coach account's status?",
+                "Something went wrong when deleting this coach account!",
+                "{{ csrf_token() }}"
+            );
         });
     </script>
 @endpush
