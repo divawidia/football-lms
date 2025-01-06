91c28de5bec3532d244e8c541897441a4ad4bf88
divawidia <wiartha2001@gmail.com>
2025-01-06 14:29:56 +0800

    feat (forms components) : create textarea component blade

---
+++ resources/views/components/forms/textarea.blade.php
@@ -0,0 +1,23 @@
+<div class="form-group">
+    <label class="form-label" for="{{ $name }}">{{ $label }}</label>
+    @if($required)
+        <small class="text-danger">*</small>
+    @else
+        <small class="text-muted">(Optional)</small>
+    @endif
+    <textarea class="form-control h-100 @if(!$modal) @error($name) is-invalid @enderror @endif"
+              id="{{ $name }}"
+              name="{{ $name }}"
+              rows="5"
+              placeholder="{{ $placeholder }}"
+              @if($required) required @endif>
+        @if($modal) {{ old($name, $value) }} @endif
+    </textarea>
+    @if($modal)
+        <span class="invalid-feedback {{ $name }}_error" role="alert"><strong></strong></span>
+    @else
+        @error($name)
+        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+        @enderror
+    @endif
+</div>
