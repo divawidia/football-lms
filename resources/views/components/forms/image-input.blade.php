5e9160156fb381e793a47306599d6ed06c7d967b
divawidia <wiartha2001@gmail.com>
2025-01-06 14:27:36 +0800

    feat (forms components) : create image input component blade

---
+++ resources/views/components/forms/image-input.blade.php
@@ -0,0 +1,17 @@
+<label class="form-label">{{ $label }}</label>
+@if($required)
+    <small class="text-danger">*</small>
+@else
+    <small class="text-muted">(Optional)</small>
+@endif
+<div class="media align-items-center mb-2">
+    <img src="{{ Storage::url('images/undefined-user.png') }}"
+         alt="img"
+         width="54"
+         height="54"
+         id="preview"
+         class="mr-16pt rounded-circle img-object-fit-cover"/>
+    <div class="media-body">
+        <x-forms.basic-input type="file" :name="$name" acceptFileType="image/jpg, image/jpeg, image/png" :required="$required" :modal="$modal" label="Choose Image"/>
+    </div>
+</div>
