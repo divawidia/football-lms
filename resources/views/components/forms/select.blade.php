76f834664dc5950a0e7d49fa7181c48f2aaca40a
divawidia <wiartha2001@gmail.com>
2025-01-06 14:28:24 +0800

    feat (forms components) : create select component blade

---
+++ resources/views/components/forms/select.blade.php
@@ -0,0 +1,24 @@
+<div class="form-group">
+    @if($label)
+        <label class="form-label" for="{{ $id }}">{{ $label }}</label>
+        @if($required)
+            <small class="text-danger">*</small>
+        @else
+            <small class="text-muted">(Optional)</small>
+        @endif
+    @endif
+    <select class="form-control form-select @if(!$modal) @error($name) is-invalid @enderror @endif"
+            id="{{ $id }}" name="{{ $name }}"
+            @if($required) required @endif
+            data-toggle="select"
+            @if($multiple) multiple @endif>
+        {{ $slot }}
+    </select>
+    @if($modal)
+        <span class="invalid-feedback {{ $name }}_error" role="alert"><strong></strong></span>
+    @else
+        @error($name)
+        <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
+        @enderror
+    @endif
+</div>
