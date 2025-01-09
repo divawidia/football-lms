83f6d880af663c9359e837b2a6bad9c710b3361c
divawidia <wiartha2001@gmail.com>
2025-01-09 19:50:44 +0800

    feat (LeagueStanding Repository) : create interface

--- app/Repository/Interface/LeagueStandingRepositoryInterface.php
+++ app/Repository/Interface/LeagueStandingRepositoryInterface.php
@@ -2,32 +2,16 @@
 
 namespace App\Repository\Interface;
 
+use App\Models\Competition;
+use App\Models\LeagueStanding;
 use App\Models\Team;
 use App\Models\User;
 
-interface UserRepositoryInterface
+interface LeagueStandingRepositoryInterface
 {
-    public function getAll();
+    public function getAll(Competition $competition = null);
+    public function create(array $data, Competition $competition);
+    public function update(LeagueStanding $standing, array $data);
 
-    public function getInArray($relation,$ids);
-
-    public function getAllUserWithoutLoggedUserData($authUserId);
-
-    public function getAllByRole($role);
-
-    public function getAllAdminUsers();
-
-    public function allTeamsParticipant(Team $team, $admins = true, $coaches = true, $players = true);
-
-    public function find($id);
-
-    public function createUserWithRole(array $data, $role);
-
-    public function updateUserStatus($userModel, $status = ['1', '0']);
-
-    public function changePassword($data, $userModel);
-
-    public function update($userModel, array $data);
-
-    public function delete($userModel);
+    public function delete(LeagueStanding $standing);
 }
