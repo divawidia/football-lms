45b1ded53628a054982b9a2780ea6736ab3f6f61
divawidia <wiartha2001@gmail.com>
2025-01-09 14:28:28 +0800

    feat (edit match modal component) : add class

---
+++ app/View/Components/modal/Matches/EditMatch.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\View\Components\modal\Matches;
+
+use App\Models\Competition;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class EditMatch extends Component
+{
+    public Competition $competition;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(Competition $competition)
+    {
+        $this->competition = $competition;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.modal.matches.edit-match');
+    }
+}
