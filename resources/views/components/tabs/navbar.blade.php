59434f5f3ef284b63b753813d2c4b87a1c47a2ed
divawidia <wiartha2001@gmail.com>
2025-01-10 00:00:00 +0000

    feat (tabs navbar component) : create blade view

---
+++ resources/views/components/tabs/navbar.blade.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-light border-bottom border-top py-3">
+    <div class="container">
+        <ul class="nav nav-pills text-capitalize">
+            {{ $slot }}
+        </ul>
+    </div>
+</nav>
