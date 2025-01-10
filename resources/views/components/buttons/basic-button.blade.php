338cfb40f5f54eb8829788359be477f2dde76350
divawidia <wiartha2001@gmail.com>
2025-01-10 00:00:00 +0000

    refactor (basic button component) : add dropdownItem parameter

--- resources/views/components/buttons/basic-button.blade.php
+++ resources/views/components/buttons/basic-button.blade.php
@@ -3,13 +3,13 @@
         @if($modalCloseIcon)
             class="close" aria-label="Close"
         @else
-            class="btn btn-{{ $color }} btn-{{ $size }} {{ $margin }}"
+            class="@if($dropdownItem) dropdown-item @else btn @endif btn-{{ $color }} btn-{{ $size }} {{ $margin }} {{ $additionalClass }}"
         @endif
         @if($modalDismiss) data-bs-dismiss="modal" @endif>
     @if($modalCloseIcon)
         <span aria-hidden="true">&times;</span>
     @else
-        <span class="material-icons mr-2">{{ $icon }}</span>
+        <span class="material-icons text-{{ $iconColor }} mr-2">{{ $icon }}</span>
         {{ $text }}
     @endif
 </button>
