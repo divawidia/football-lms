d594a259f37799db333859baed39e98b32071eb5
divawidia <wiartha2001@gmail.com>
2025-01-10 00:00:00 +0000

    refactor (add team score component) : update modal

--- resources/views/components/modal/matches/add-team-scorer.blade.php
+++ resources/views/components/modal/matches/add-team-scorer.blade.php
@@ -1,89 +1,49 @@
-<div class="modal fade" id="createTeamScorerModal" tabindex="-1" aria-labelledby="createTeamScorerModal"
-     aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <form action="" method="POST" id="formAddScorerModal">
-                @csrf
-                <div class="modal-header">
-                    <h5 class="modal-title"></h5>
-                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <input type="hidden" id="dataTeam" name="dataTeam">
-                    <input type="hidden" id="teamId" name="teamId">
-                    <div class="form-group">
-                        <label class="form-label" for="playerId">Player Name</label>
-                        <small class="text-danger">*</small>
-                        <select class="form-control form-select" id="playerId" name="playerId" required data-toggle="select" >
-                            <option disabled selected>Select player who assisted the goal</option>
-                        </select>
-                        <span class="invalid-feedback playerId_error" role="alert"><strong></strong></span>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="assistPlayerId">Assist Player Name</label>
-                        <small class="text-danger">*</small>
-                        <select class="form-control form-select" id="assistPlayerId" name="assistPlayerId" required data-toggle="select" ></select>
-                        <span class="invalid-feedback assistPlayerId_error" role="alert"><strong></strong></span>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="minuteScored">Minute Scored</label>
-                        <small class="text-danger">*</small>
-                        <input type="number"
-                               class="form-control"
-                               id="minuteScored"
-                               name="minuteScored"
-                               min="1"
-                               max="160"
-                               placeholder="Pick minutes the player scored the goal. Eg : 60"
-                               required>
-                        <span class="invalid-feedback minuteScored_error" role="alert"><strong></strong></span>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
+<x-modal.form id="createTeamScorerModal" formId="formAddScorerModal">
+    <x-forms.basic-input type="hidden" name="teamId" :modal="true"/>
+    <x-forms.basic-input type="hidden" name="dataTeam" :modal="true"/>
 
-<x-modal-form-update-processing formId="#formAddScorerModal"
-                                updateDataId=""
-                                :routeUpdate="route('match-schedules.store-match-scorer', $eventSchedule->id)"
-                                modalId="#createTeamScorerModal"/>
+    <x-forms.select name="playerId" label="Player Name" :modal="true" :select2="false"></x-forms.select>
+
+    <x-forms.select name="assistPlayerId" label="Assist Player Name" :modal="true" :select2="false"></x-forms.select>
+
+    <x-forms.basic-input type="number" min="1" max="160" name="minuteScored" label="Minutes Scored" placeholder="Input minutes the player scored the goal, E.g. : 60" :modal="true"/>
+</x-modal.form>
 
 @push('addon-script')
-    <script>
+    <script type="module">
+        import { processModalForm } from "{{ Vite::asset('resources/js/ajax-processing-data.js') }}" ;
+        import { clearModalFormValidation } from "{{ Vite::asset('resources/js/modal.js') }}";
+
         $(document).ready(function (){
             const formId = '#formAddScorerModal';
+            const modalId = '#createTeamScorerModal';
+            const btnClass = '.addTeamScorer';
+            let team;
 
-            // show create team scorer modal
-            $('.addTeamScorer').on('click', function (e) {
+            // show create team scorer
+            $(btnClass).on('click', function(e) {
                 e.preventDefault();
-                const team = $(this).attr('data-team')
-
-                $('#createTeamScorerModal').modal('show');
-                $(formId+' .invalid-feedback').empty();
-                $(formId+' select').removeClass('is-invalid');
-                $(formId+' input').removeClass('is-invalid');
-                $(formId+' #assistPlayerId').empty();
+                $(modalId).modal('show');
+                team = $(this).attr('id')
+                clearModalFormValidation(formId)
+                $(formId+' #assistPlayerId').empty()
 
                 $.ajax({
-                    url: "{{route('get-event-player', ['schedule' => $eventSchedule->id]) }}",
+                    url: "{{ route('match-schedules.players', ['schedule' => $eventSchedule->hash]) }}",
                     type: 'GET',
                     data: {
                         team: team,
                     },
-                    dataType: 'json',
+                    dataType: 'JSON',
                     success: function (result) {
                         $(formId+' .modal-title').text('Add '+result.data.team.teamName+' Match Scorer')
-                        $(formId+' #dataTeam').val(team)
                         $(formId+' #teamId').val(result.data.team.id)
+                        $(formId+' #dataTeam').val(team)
+
+                        $(formId+' #playerId').html('<option disabled selected>Select player who scored the goal</option>');
                         $.each(result.data.players, function (key, value) {
-                            $(formId+' #playerId').append('<option value="' + value.id + '" data-avatar-src={{ Storage::url('') }}' + value.user.foto + '>' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');
+                            {{--$(formId+' #playerId').append('<option value="' + value.id + '" data-avatar-src={{ Storage::url('') }}' + value.user.foto + '>' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');--}}
+                            $(formId+' #playerId').append('<option value="' + value.id + '">' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');
                         });
                     },
                     error: function(jqXHR, textStatus, errorThrown) {
@@ -100,20 +60,20 @@
             // fetch assist player data
             $(formId+' #playerId').on('change', function () {
                 const id = $(this).val()
-                const team = $('#dataTeam').val()
 
                 $.ajax({
-                    url: "{{route('get-event-player', ['schedule' => $eventSchedule->id]) }}",
+                    url: "{{ route('match-schedules.players', ['schedule' => $eventSchedule->hash]) }}",
                     type: 'GET',
                     data: {
                         team: team,
                         exceptPlayerId: id,
                     },
-                    dataType: 'json',
+                    dataType: 'JSON',
                     success: function (result) {
                         $(formId+' #assistPlayerId').html('<option disabled selected>Select player who assisted the goal</option>');
                         $.each(result.data.players, function (key, value) {
-                            $(formId+' #assistPlayerId').append('<option value="' + value.id + '" data-avatar-src={{ Storage::url('') }}' + value.user.foto + '>' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');
+                            {{--$(formId+' #assistPlayerId').append('<option value="' + value.id + '" data-avatar-src={{ Storage::url('') }}' + value.user.foto + '>' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');--}}
+                            $(formId+' #assistPlayerId').append('<option value="' + value.id + '">' + value.user.firstName + ' ' + value.user.lastName + ' ~ ' + value.position.name + '</option>');
                         });
                     },
                     error: function(jqXHR, textStatus, errorThrown) {
@@ -126,6 +86,13 @@
                     }
                 });
             });
+
+            processModalForm(
+                formId,
+                "{{ route('match-schedules.store-match-scorer', $eventSchedule->hash) }}",
+                null,
+                modalId
+            );
         });
     </script>
 @endpush
