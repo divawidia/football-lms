e6283ef85f9a1fd3bdd6385177b4a7d3f3c62a5b
divawidia <wiartha2001@gmail.com>
2025-01-10 00:00:00 +0000

    refactor (event note card component) : update delete confirmation script

--- resources/views/components/cards/event-note.blade.php
+++ resources/views/components/cards/event-note.blade.php
@@ -5,7 +5,7 @@
             <div class="card-subtitle text-50">Last updated at {{ date('D, M d Y h:i A', strtotime($note->updated_at)) }}</div>
         </div>
         @if(isAllAdmin() || isCoach())
-            <x-buttons.dropdown title="" icon="more_vert">
+            <x-buttons.dropdown :title="null" icon="more_vert" btnColor="outline-secondary" iconMargin="">
                 <x-buttons.basic-button icon="edit" text="Edit Note" additionalClass="edit-note" :dropdownItem="true" :id="$note->id" color=""/>
                 <x-buttons.basic-button icon="delete" text="Delete Note" additionalClass="delete-note" :dropdownItem="true" :id="$note->id" color="" iconColor="danger"/>
             </x-buttons.dropdown>
@@ -16,19 +16,24 @@
     </div>
 </div>
 
-{{--    delete own goal player confirmation   --}}
-@if($note->schedule->eventType == 'Training')
-    <x-process-data-confirmation btnClass=".delete-note"
-                              :processRoute="$deleteRoute"
-                              :routeAfterProcess="route('training-schedules.show', ['schedule' => $note->schedule->hash])"
-                              method="DELETE"
-                              confirmationText="Are you sure to delete this note?"
-                              errorText="Something went wrong when deleting the note!"/>
-@else
-    <x-process-data-confirmation btnClass=".delete-note"
-                              :processRoute="$deleteRoute"
-                              :routeAfterProcess="route('match-schedules.show', ['schedule' => $note->schedule->hash])"
-                              method="DELETE"
-                              confirmationText="Are you sure to delete this note?"
-                              errorText="Something went wrong when deleting the note!"/>
-@endif
+@push('addon-script')
+    <script type="module">
+        import { processWithConfirmation } from "{{ Vite::asset('resources/js/ajax-processing-data.js') }}" ;
+
+        $(document).ready(function () {
+            processWithConfirmation(
+                '.delete-note',
+                "{{ route('match-schedules.destroy-note', ['schedule' => $schedule->hash, 'note'=>':id']) }}",
+                @if($schedule->eventType == 'Training')
+                    "{{ route('training-schedules.show', ['schedule' => $schedule->hash]) }}",
+                @else
+                    "{{ route('match-schedules.show', ['schedule' => $schedule->hash]) }}",
+                @endif
+                'DELETE',
+                "Are you sure to delete this note?",
+                "Something went wrong when deleting this note!",
+                "{{ csrf_token() }}"
+            );
+        });
+    </script>
+@endpush
