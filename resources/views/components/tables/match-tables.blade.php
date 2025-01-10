d5828202fcc8686a62e2ecf200b8c00890a06e2e
divawidia <wiartha2001@gmail.com>
2025-01-10 00:00:00 +0000

    refactor (match table component) : update script and table

--- resources/views/components/tables/match-tables.blade.php
+++ resources/views/components/tables/match-tables.blade.php
@@ -1,28 +1,12 @@
 <div class="card">
     <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-hover mb-0" id="{{ $tableId }}">
-                <thead>
-                <tr>
-                    <th>Team</th>
-                    <th>Opponent</th>
-                    <th>Match Date</th>
-                    <th>Location</th>
-                    <th>Competition</th>
-                    <th>Match Type</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-                </thead>
-                <tbody>
-                </tbody>
-            </table>
-        </div>
+        <x-table :headers="['#','Team', 'Opposing Team', 'Score', 'Competition','Match Date','Venue', 'Match Type','Status', 'Action']" tableId="{{ $tableId }}"/>
     </div>
 </div>
 
 @push('addon-script')
-    <script>
+    <script type="module">
+        import { processWithConfirmation } from "{{ Vite::asset('resources/js/ajax-processing-data.js') }}";
         $(document).ready(function (){
             const datatable = $('#{{ $tableId }}').DataTable({
                 processing: true,
@@ -32,64 +16,35 @@
                     url: '{{ $route }}',
                 },
                 columns: [
-                    { data: 'team', name: 'team' },
-                    { data: 'opponentTeam', name: 'opponentTeam' },
+                    {data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false},
+                    { data: 'homeTeam', name: 'homeTeam' },
+                    { data: 'awayTeam', name: 'awayTeam' },
+                    { data: 'score', name: 'score' },
+                    { data: 'competition', name: 'competition'},
                     { data: 'date', name: 'date' },
                     { data: 'place', name: 'place'},
-                    { data: 'competition', name: 'competition'},
                     { data: 'matchType', name: 'matchType'},
                     { data: 'status', name: 'status' },
                     {
                         data: 'action',
                         name: 'action',
                         orderable: false,
-                        searchable: false,
-                        width: '15%'
+                        searchable: false
                     },
                 ],
-                order: [[2, 'asc']]
+                order: [[5, 'asc']]
             });
 
             @if(isAllAdmin())
-            $('body').on('click', '.delete', function() {
-                let id = $(this).attr('id');
-
-                Swal.fire({
-                    title: "Are you sure to delete this match?",
-                    text: "You won't be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#1ac2a1",
-                    cancelButtonColor: "#E52534",
-                    confirmButtonText: "Yes, delete it!"
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        $.ajax({
-                            @if(isAllAdmin())
-                            url: "{{ route('match-schedules.destroy', ['schedule' => ':id']) }}".replace(':id', id),
-                            @endif
-                            type: 'DELETE',
-                            data: {
-                                _token: "{{ csrf_token() }}"
-                            },
-                            success: function(response) {
-                                Swal.fire({
-                                    icon: "success",
-                                    title: "Match successfully deleted!",
-                                });
-                                datatable.ajax.reload();
-                            },
-                            error: function (jqXHR, textStatus, errorThrown) {
-                                Swal.fire({
-                                    icon: "error",
-                                    title: "Something went wrong when deleting data!",
-                                    text: errorThrown
-                                });
-                            }
-                        });
-                    }
-                });
-            });
+                processWithConfirmation(
+                    '.delete',
+                    "{{ route('match-schedules.destroy', ['schedule' => ':id']) }}",
+                    "{{ route('match-schedules.index') }}",
+                    'DELETE',
+                    "Are you sure to delete this match?",
+                    "Something went wrong when deleting this match!",
+                    "{{ csrf_token() }}"
+                 );
             @endif
         });
     </script>
