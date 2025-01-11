d1a27ae8abca057b6b0794d18115302b07a81c5c
divawidia <wiartha2001@gmail.com>
2025-01-11 00:00:00 +0000

    refactor (edit external team score component) : create blade view

---
+++ resources/views/components/modal/matches/edit-external-team-score.blade.php
@@ -0,0 +1,56 @@
+<x-modal.form id="editExternalTeamScoreModal" formId="formEditExternalTeamScore" :editForm="true">
+    <x-forms.basic-input type="hidden" name="teamSide" :modal="true"/>
+    <div class="row">
+        <div class="col-6">
+            <x-forms.basic-input type="number" min="0" name="goalScored" label="Goal Scored" placeholder="Input team's goal scored" :modal="true"/>
+        </div>
+        <div class="col-6">
+            <x-forms.basic-input type="number" min="0" name="teamOwnGoal" label="Own Goal" placeholder="Input team's own goal" :modal="true" :required="false"/>
+        </div>
+    </div>
+</x-modal.form>
+
+@push('addon-script')
+    <script type="module">
+        import { processModalForm } from "{{ Vite::asset('resources/js/ajax-processing-data.js') }}" ;
+        import { clearModalFormValidation } from "{{ Vite::asset('resources/js/modal.js') }}";
+
+        $(document).ready(function (){
+            const formId = '#formEditExternalTeamScore';
+            const modalId = '#editExternalTeamScoreModal';
+
+            $('.edit-team-score-btn').on('click', function(e) {
+                e.preventDefault();
+                $(modalId).modal('show');
+                clearModalFormValidation(formId)
+
+                $.ajax({
+                    url: "{{ route('match-schedules.match-stats', ['schedule' => $eventSchedule->hash]) }}",
+                    type: 'GET',
+                    dataType: 'JSON',
+                    success: function (result) {
+                        $(formId+' .modal-title').text('Update '+result.data.teamName+' Team Score')
+                        $.each(result.data, function (key, value) {
+                            $(formId + ' #' + key).val(value)
+                        });
+                    },
+                    error: function(jqXHR, textStatus, errorThrown) {
+                        const response = JSON.parse(jqXHR.responseText);
+                        Swal.fire({
+                            icon: "error",
+                            title: errorThrown,
+                            text: response.message,
+                        });
+                    }
+                });
+            });
+
+            processModalForm(
+                formId,
+                "{{ route('match-schedules.update-external-team-score', $eventSchedule->hash) }}",
+                null,
+                modalId
+            );
+        });
+    </script>
+@endpush
