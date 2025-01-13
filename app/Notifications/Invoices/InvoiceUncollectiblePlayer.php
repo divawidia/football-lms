afdd21c1f0167c42b14b3ba34c8ef4c79ff793fb
divawidia <wiartha2001@gmail.com>
2025-01-13 14:58:28 +0800

    refactor (Invoice uncollectible player notification) : update delivery channel

--- app/Notifications/Invoices/InvoiceUncollectiblePlayer.php
+++ app/Notifications/Invoices/InvoiceUncollectiblePlayer.php
@@ -28,8 +28,10 @@ class InvoiceUncollectiblePlayer extends Notification implements ShouldQueue
      */
     public function via(object $notifiable): array
     {
-//        return ['mail', 'database'];
-        return ['database'];
+        return [
+            'mail',
+            'database'
+        ];
     }
 
     /**
