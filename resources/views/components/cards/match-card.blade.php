4752f523503eb3e2d9f7a79ef95f80f5966179e4
divawidia <wiartha2001@gmail.com>
2025-01-13 15:06:36 +0800

    refactor (match card component) : update score team

--- resources/views/components/cards/match-card.blade.php
+++ resources/views/components/cards/match-card.blade.php
@@ -1,37 +1,56 @@
+@php
+    $homeTeam = $match->teams()->where('teamId', $match->homeTeamId)->first();
+    if ($match->matchType == 'Internal Match') {
+        $awayTeam = $match->teams()->where('teamId', $match->awayTeamId)->first();
+    } else {
+        $awayTeam = $match->externalTeam;
+    }
+@endphp
 <a class="card" href="{{ route('match-schedules.show', $match->hash) }}">
     <div class="card-body">
         <div class="row">
             <div class="col-4 d-flex flex-column flex-md-row align-items-center">
-                <img src="{{ Storage::url($match->teams[0]->logo) }}"
+                <img src="{{ Storage::url($match->homeTeam->logo) }}"
                      width="50"
                      height="50"
                      class="rounded-circle img-object-fit-cover"
                      alt="team-logo">
                 <div class="ml-md-3 text-center text-md-left">
-                    <h6 class="mb-0">{{ $match->teams[0]->teamName }}</h6>
-                    <p class="text-50 lh-1 mb-0">{{ $match->teams[0]->ageGroup }}</p>
+                    <h6 class="mb-0">{{ $match->homeTeam->teamName }}</h6>
+                    <p class="text-50 lh-1 mb-0">{{ $match->homeTeam->ageGroup }}</p>
                 </div>
             </div>
             <div class="col-4 text-center">
                 <h2 class="mb-0">
                     @if($latestMatch == true)
-                        {{ $match->teams[0]->pivot->teamScore }} - {{ $match->teams[1]->pivot->teamScore }}
+                        {{ $homeTeam->pivot->teamScore }}
+                        -
+                        @if($match->matchType == 'Internal Match')
+                            {{ $awayTeam->pivot->teamScore }}
+                        @else
+                            {{ $awayTeam->teamScore }}
+                        @endif
                     @else
                         Vs.
                     @endif
                 </h2>
             </div>
-            <div
-                class="col-4 d-flex flex-column-reverse flex-md-row align-items-center justify-content-end">
-                <div class="mr-md-3 text-center text-md-right">
-                    <h6 class="mb-0">{{ $match->teams[1]->teamName }}</h6>
-                    <p class="text-50 lh-1 mb-0">{{ $match->teams[1]->ageGroup }}</p>
-                </div>
-                <img src="{{ Storage::url($match->teams[1]->logo) }}"
-                     width="50"
-                     height="50"
-                     class="rounded-circle img-object-fit-cover"
-                     alt="team-logo">
+            <div class="col-4 d-flex flex-column-reverse flex-md-row align-items-center justify-content-end">
+                @if($match->matchType == 'Internal Match')
+                    <div class="mr-md-3 text-center text-md-right">
+                        <h5 class="mb-0">{{ $match->awayTeam->teamName }}</h5>
+                        <p class="text-50 lh-1 mb-0">{{$match->awayTeam->ageGroup}}</p>
+                    </div>
+                    <img src="{{ Storage::url($match->awayTeam->logo) }}"
+                         width="50"
+                         height="50"
+                         class="rounded-circle img-object-fit-cover"
+                         alt="team-logo">
+                @else
+                    <div class="mr-md-3 text-center text-md-right">
+                        <h5 class="mb-0">{{ $match->externalTeam->teamName }}</h5>
+                    </div>
+                @endif
             </div>
         </div>
 
