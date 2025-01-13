ad6dd3e1f00048db6ef28964aa5d961fa5463867
divawidia <wiartha2001@gmail.com>
2025-01-13 14:56:41 +0800

    refactor (InvoiceGeneratePLayer notification) : update delivery channel

--- app/Notifications/Invoices/InvoiceGeneratedPlayer.php
+++ app/Notifications/Invoices/InvoiceGeneratedPlayer.php
@@ -26,8 +26,10 @@ class InvoiceGeneratedPlayer extends Notification implements ShouldQueue
      */
     public function via(object $notifiable): array
     {
-//        return ['mail', 'database'];
-        return ['database'];
+        return [
+            'mail',
+            'database'
+        ];
     }
 
     /**
