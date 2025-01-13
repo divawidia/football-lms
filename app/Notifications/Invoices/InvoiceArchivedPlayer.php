c605e7bdbf32400cc65d943248e70dc85da0cb7b
divawidia <wiartha2001@gmail.com>
2025-01-13 14:55:52 +0800

    refactor (InvoiceArchivedPlayer notification) : update delivery channel

--- app/Notifications/Invoices/InvoiceArchivedPlayer.php
+++ app/Notifications/Invoices/InvoiceArchivedPlayer.php
@@ -28,7 +28,10 @@ class InvoiceArchivedPlayer extends Notification implements ShouldQueue
      */
     public function via(object $notifiable): array
     {
-        return ['mail','database'];
+        return [
+            'mail',
+            'database'
+        ];
     }
 
     /**
