4611e3aa1be139f150f6a477902c034bd8baf6b4
divawidia <wiartha2001@gmail.com>
2025-01-16 17:08:52 +0800

    refactor (select2) : delete unused file

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -17,7 +17,6 @@
 
 <!-- Select2 -->
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-<script src="{{ URL::asset('js/select2.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
