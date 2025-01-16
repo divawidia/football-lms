573713b94f47c21ca1c83ad1c005798e467131f6
divawidia <wiartha2001@gmail.com>
2025-01-16 17:02:16 +0800

    refactor (js) : delete unused code

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,56 +1,25 @@
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
-<!-- jQuery Mask Plugin -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
 <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.js"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/metismenu"></script>
-
-{{--<script src="https://unpkg.com/simplebar@latest/dist/simplebar.min.js"></script>--}}
-
-<!-- Perfect Scrollbar -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/perfect-scrollbar/1.5.5/perfect-scrollbar.min.js"></script>
 
-<!-- DOM Factory -->
-<script src="{{ URL::asset('vendor/dom-factory.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/dom-factory@1.1.0/dist/dom-factory.min.js"></script>
 
-<!-- MDK -->
-<script src="{{ URL::asset('vendor/material-design-kit.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/material-design-kit@2.2.1/dist/material-design-kit.min.js"></script>
 
-<!-- App JS -->
-{{--<script src="{{ URL::asset('js/app.js') }}"></script>--}}
-
-<!-- Preloader -->
 <script src="{{ URL::asset('js/preloader.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-<!-- Touchspin -->
-<script src="{{ URL::asset('vendor/jquery.bootstrap-touchspin.js') }}"></script>
-<script src="{{ URL::asset('js/touchspin.js') }}"></script>
-
-<!-- Flatpickr -->
-<script src="{{ URL::asset('vendor/flatpickr/flatpickr.min.js') }}"></script>
-<script src="{{ URL::asset('js/flatpickr.js') }}"></script>
-
-<!-- DateRangePicker -->
-<script src="{{ URL::asset('vendor/moment.min.js') }}"></script>
-<script src="{{ URL::asset('vendor/daterangepicker.js') }}"></script>
-<script src="{{ URL::asset('js/daterangepicker.js') }}"></script>
-
-<!-- Quill -->
-<script src="{{ URL::asset('vendor/quill.min.js') }}"></script>
-<script src="{{ URL::asset('js/quill.js') }}"></script>
-
 <!-- Select2 -->
-<script src="{{ URL::asset('vendor/select2/select2.min.js') }}"></script>
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script src="{{ URL::asset('js/select2.js') }}"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-{{--<script src="https://unpkg.com/filepond@^4/dist/filepond.js"></script>--}}
 
 <script type="importmap">
     {
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,24 +1,21 @@
 <link href="https://fonts.googleapis.com/css?family=Lato:400,700%7CRoboto:400,500%7CExo+2:600&display=swap" rel="stylesheet">
 
 <!-- Preloader -->
-<link type="text/css" href="{{ URL::asset('vendor/spinkit.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/spinkit@2.0.1/spinkit.min.css">
 
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
 <!-- Perfect Scrollbar -->
-<link type="text/css" href="{{ URL::asset('vendor/perfect-scrollbar.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.5.6/css/perfect-scrollbar.min.css" integrity="sha512-ygIxOy3hmN2fzGeNqys7ymuBgwSCet0LVfqQbWY10AszPMn2rB9JY0eoG0m1pySicu+nvORrBmhHVSt7+GI9VA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/metismenu/dist/metisMenu.min.css">
-
-{{--<link rel="stylesheet" href="https://unpkg.com/simplebar@latest/dist/simplebar.css"/>--}}
-{{--<link rel="stylesheet" href="{{  node_modules/material-design-kit/dist/material-design-kit.css}}">--}}
+<link href="https://cdn.jsdelivr.net/npm/material-design-kit@2.2.1/dist/material-design-kit.min.css" rel="stylesheet">
 
 <!-- Material Design Icons -->
-<link type="text/css" href="{{ URL::asset('css/material-icons.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/material-icons@1.13.12/iconfont/material-icons.min.css">
 
 <!-- Font Awesome Icons -->
-<link type="text/css" href="{{ URL::asset('css/fontawesome.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
 
 <!-- Preloader -->
 <link type="text/css" href="{{ URL::asset('css/preloader.css') }}" rel="stylesheet">
@@ -26,30 +23,14 @@
 <!-- App CSS -->
 <link type="text/css" href="{{ URL::asset('css/app.css') }}" rel="stylesheet">
 
-<!-- Flatpickr -->
-<link type="text/css" href="{{ URL::asset('css/flatpickr.css') }}" rel="stylesheet"/>
-<link type="text/css" href="{{ URL::asset('css/flatpickr-airbnb.css') }}" rel="stylesheet"/>
-
-<!-- DateRangePicker -->
-<link type="text/css" href="{{ URL::asset('vendor/daterangepicker.css') }}" rel="stylesheet"/>
-
-<!-- Quill Theme -->
-<link type="text/css" href="{{ URL::asset('css/quill.css') }}" rel="stylesheet" />
-
-<!-- Touchspin -->
-<link type="text/css" href="{{ URL::asset('css/bootstrap-touchspin.css') }}" rel="stylesheet"/>
-
 <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-1.13.8/datatables.min.css" rel="stylesheet">
 
-<link type="text/css" href="{{ URL::asset('vendor/select2/select2.min.css') }}" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <link type="text/css" href="{{ URL::asset('css/select2.css') }}" rel="stylesheet">
 
-<link type="text/css" href="{{ URL::asset('css/sweetalert.css') }}" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.15.10/dist/sweetalert2.min.css" rel="stylesheet">
 
 <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-
-{{--<link href="https://unpkg.com/filepond@^4/dist/filepond.css" rel="stylesheet" />--}}
-{{--<link href="https://unpkg.com/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css" rel="stylesheet" />--}}
 
 <link rel="stylesheet" href="https://cdn.ckeditor.com/ckeditor5/43.1.1/ckeditor5.css">
 
