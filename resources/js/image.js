fa3500bf5f78ded43af3ec0312d3e560181b7ac3
divawidia <wiartha2001@gmail.com>
2025-01-16 16:07:13 +0800

    style (image js) : reformatting code

--- resources/js/image.js
+++ resources/js/image.js
@@ -1,52 +1,52 @@
 export const imageComponent = () => ({
 
-  /**
-   * Public properties.
-   * @type {Object}
-   */
-  properties: {
-
     /**
-     * Background position.
+     * Public properties.
      * @type {Object}
      */
-    position: {
-      reflectToAttribute: true,
-      value: 'center'
+    properties: {
+
+        /**
+         * Background position.
+         * @type {Object}
+         */
+        position: {
+            reflectToAttribute: true,
+            value: 'center'
+        },
+
+        /**
+         * Image height.
+         * @type {Object}
+         */
+        height: {
+            reflectToAttribute: true,
+            value: 'auto'
+        },
     },
 
     /**
-     * Image height.
-     * @type {Object}
+     * Mask element.
+     * @return {HTMLElement}
      */
-    height: {
-      reflectToAttribute: true,
-      value: 'auto'
+    get image() {
+        return this.element.querySelector('img')
     },
-  },
 
-  /**
-   * Mask element.
-   * @return {HTMLElement}
-   */
-  get image () {
-    return this.element.querySelector('img')
-  },
-
-  _reset () {
-    if (!this.image) {
-      return
+    _reset() {
+        if (!this.image) {
+            return
+        }
+        this.element.style.display = 'block'
+        this.element.style.position = 'relative'
+        this.element.style.overflow = 'hidden'
+        this.element.style.backgroundImage = `url(${this.image.src})`
+        this.element.style.backgroundSize = 'cover'
+        this.element.style.backgroundPosition = this.position
+        this.element.style.height = `${this.height === 'auto' ? this.image.offsetHeight : this.height}px`
+        this.image.style.visibility = 'hidden'
+        // this.element.removeChild(this.image)
     }
-    this.element.style.display = 'block'
-    this.element.style.position = 'relative'
-    this.element.style.overflow = 'hidden'
-    this.element.style.backgroundImage = `url(${ this.image.src })`
-    this.element.style.backgroundSize = 'cover'
-    this.element.style.backgroundPosition = this.position
-    this.element.style.height = `${ this.height === 'auto' ? this.image.offsetHeight : this.height }px`
-    this.image.style.visibility = 'hidden'
-    // this.element.removeChild(this.image)
-  }
 })
 
 domFactory.handler.register('image', imageComponent)
