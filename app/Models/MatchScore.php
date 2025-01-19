0eef7ee908c58b0ae5781757efe343a2867f27f3
divawidia <wiartha2001@gmail.com>
2025-01-19 00:00:00 +0000

    feat (Match model) : add relation methods return type

--- app/Models/MatchScore.php
+++ app/Models/MatchScore.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MatchScore extends Model
@@ -13,25 +14,25 @@ class MatchScore extends Model
     protected $fillable = [
         'playerId',
         'assistPlayerId',
-        'eventId',
+        'matchId',
         'minuteScored',
         'isOwnGoal',
         'teamId'
     ];
 
-    public function player()
+    public function player(): BelongsTo
     {
         return $this->belongsTo(Player::class, 'playerId', 'id');
     }
-    public function assistPlayer()
+    public function assistPlayer(): BelongsTo
     {
         return $this->belongsTo(Player::class, 'assistPlayerId', 'id');
     }
-    public function event()
+    public function match(): BelongsTo
     {
-        return $this->belongsTo(MatchModel::class, 'eventId', 'id');
+        return $this->belongsTo(MatchModel::class, 'matchId', 'id');
     }
-    public function team()
+    public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class, 'teamId', 'id');
     }
