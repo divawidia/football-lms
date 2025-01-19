dc24e6c1802892be34d2d21af056b5ae168bbe9a
divawidia <wiartha2001@gmail.com>
2025-01-19 00:00:00 +0000

    feat (db migration) : create player_training_attendance table

---
+++ database/migrations/2025_01_25_154313_create_player_training_attendance_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('player_training_attendance', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('trainingId')->constrained('trainings')->cascadeOnDelete();
+            $table->foreignId('playerId')->nullable()->constrained('players')->nullOnDelete();
+            $table->enum('attendanceStatus', ['Required Action', 'Attended', 'Illness', 'Injured', 'Other'])->default('Required Action');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('player_training_attendance');
+    }
+};
