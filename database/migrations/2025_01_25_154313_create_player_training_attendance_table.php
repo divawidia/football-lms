3bbd2e61ca3d434b801cf5437b2b055043ebfa12
divawidia <wiartha2001@gmail.com>
2025-01-20 00:00:00 +0000

    feat (db migration) : add teamId foreign in player_training_attendance table

--- database/migrations/2025_01_25_154313_create_player_training_attendance_table.php
+++ database/migrations/2025_01_25_154313_create_player_training_attendance_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('trainingId')->constrained('trainings')->cascadeOnDelete();
             $table->foreignId('playerId')->nullable()->constrained('players')->nullOnDelete();
+            $table->foreignId('teamId')->nullable()->constrained('teams')->nullOnDelete();
             $table->enum('attendanceStatus', ['Required Action', 'Attended', 'Illness', 'Injured', 'Other'])->default('Required Action');
             $table->text('note')->nullable();
             $table->timestamps();
