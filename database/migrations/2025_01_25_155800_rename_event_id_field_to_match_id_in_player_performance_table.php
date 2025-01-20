d7e57a703f4dde01da2b0bacff8c503df035d7a5
divawidia <wiartha2001@gmail.com>
2025-01-20 00:00:00 +0000

    feat (db migration) : rename eventId field in player_performance_reviews table

---
+++ database/migrations/2025_01_25_155800_rename_event_id_field_to_match_id_in_player_performance_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('player_performance_reviews', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('eventId');
+            $table->foreignId('matchId')->nullable()->constrained('matches')->cascadeOnDelete();
+            $table->foreignId('trainingId')->nullable()->constrained('trainings')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('player_performance_reviews', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('matchId');
+            $table->foreignId('eventId')->nullable()->constrained('matches')->cascadeOnDelete();
+            $table->dropConstrainedForeignId('trainingId');
+        });
+    }
+};
