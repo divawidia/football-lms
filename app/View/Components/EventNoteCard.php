332fafa8fb4a80b9ba2f2a508a1c58dfbe2b7a98
divawidia <wiartha2001@gmail.com>
2025-01-20 00:00:00 +0000

    refactor (EventNote card component) : update MatchModel dependency

--- app/View/Components/EventNoteCard.php
+++ app/View/Components/EventNoteCard.php
@@ -2,24 +2,23 @@
 
 namespace App\View\Components;
 
-use App\Models\Match;
-use App\Models\ScheduleNote;
+use App\Models\MatchNote;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
 class EventNoteCard extends Component
 {
-    public ScheduleNote $note;
-    public Match $schedule;
+    public MatchNote $note;
+    public MatchModel $match;
 
     /**
      * Create a new component instance.
      */
-    public function __construct(ScheduleNote $note, Match $schedule)
+    public function __construct(MatchNote $note, MatchModel $match)
     {
         $this->note = $note;
-        $this->schedule = $schedule;
+        $this->match = $match;
     }
 
     /**
