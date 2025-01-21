cae411c3b0eb0757600fbcc141d7c35242ffa142
divawidia <wiartha2001@gmail.com>
2025-01-21 00:00:00 +0000

    refactor (dashboard controller) update index method

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\AdminService;
 use App\Services\DashboardService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -11,21 +12,27 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     private DashboardService $dashboardService;
+    private AdminService $adminService;
 
-    public function __construct(DashboardService $dashboardService){
+    public function __construct(
+        DashboardService $dashboardService,
+        AdminService $adminService,
+    )
+    {
         $this->dashboardService = $dashboardService;
+        $this->adminService = $adminService;
     }
     public function index()
     {
         $dataOverview = $this->dashboardService->overviewStats();
-//        $revenueChart = $this->dashboardService->revenueChart();
         $teamAgeChart = $this->dashboardService->playerAgeChart();
         $upcomingMatches = $this->dashboardService->upcomingMatches();
         $upcomingTrainings = $this->dashboardService->upcomingTrainings();
 
         return view('pages.dashboards.admin', [
+            'totalAdmins' => $this->adminService->countAllAdmin(),
+            'totalAdminsThisMonth' => $this->adminService->countNewAdminThisMonth(),
             'dataOverview' => $dataOverview,
-//            'revenueChart' => $revenueChart,
             'teamAgeChart' => $teamAgeChart,
             'upcomingMatches' => $upcomingMatches,
             'upcomingTrainings' => $upcomingTrainings
