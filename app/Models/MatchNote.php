d76918518a8dcb616db3c4b5558a131f32ada372
divawidia <wiartha2001@gmail.com>
2025-01-21 00:00:00 +0000

    refactor (MatchNote Model) : update methods return type adn fillable field

--- app/Models/MatchNote.php
+++ app/Models/MatchNote.php
@@ -7,21 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Veelasky\LaravelHashId\Eloquent\HashableId;
 
-class ScheduleNote extends Model
+class MatchNote extends Model
 {
     use HasFactory, HashableId;
 
     protected $fillable = [
         'note',
-        'scheduleId',
+        'matchId',
         'teamId',
     ];
 
     public function schedule(): BelongsTo
     {
-        return $this->belongsTo(MatchModel::class, 'scheduleId');
+        return $this->belongsTo(MatchModel::class, 'matchId');
     }
-    public function team()
+    public function team(): BelongsTo
     {
         return $this->belongsTo(Team::class, 'teamId');
     }
