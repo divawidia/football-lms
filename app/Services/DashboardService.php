e60674b5ab528a97ac47b121d76cdcd0dc41726f
divawidia <wiartha2001@gmail.com>
2025-01-21 00:00:00 +0000

    feat (dashboard service)

--- app/Services/DashboardService.php
+++ app/Services/DashboardService.php
@@ -5,12 +5,11 @@ namespace App\Services;
 use App\Models\Admin;
 use App\Models\Coach;
 use App\Models\Competition;
-use App\Models\Match;
-use App\Models\Invoice;
+use App\Models\MatchModel;
 use App\Models\Player;
 use App\Models\Team;
+use App\Models\Training;
 use App\Repository\InvoiceRepository;
-use Illuminate\Support\Number;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
@@ -31,17 +30,17 @@ class DashboardService extends Service
         $totalPlayers = Player::count();
         $totalCoaches = Coach::count();
         $totalTeams = Team::where('teamSide','Academy Team')->count();
-        $totalAdmins = Admin::count();
+
         $totalCompetitions = Competition::count();
-        $totalUpcomingMatches = Match::where('eventType', 'Match')->where('status', '1')->count();
-        $totalUpcomingTrainings = Match::where('eventType', 'Training')->where('status', '1')->count();
+        $totalUpcomingMatches = MatchModel::where('status', 'Scheduled')->count();
+        $totalUpcomingTrainings = Training::where('status', 'Scheduled')->count();
         $totalRevenues = $this->invoiceRepository->calculateInvoiceByStatus('Paid', sumAmount: true);
         $totalRevenues = $this->formatReadableNumber($totalRevenues);
 
         $thisMonthTotalPlayers = Player::whereBetween('created_at',[$startMonth,$now])->count();
         $thisMonthTotalCoaches = Coach::whereBetween('created_at',[$startMonth,$now])->count();
         $thisMonthTotalTeams = Team::where('teamSide','Academy Team')->whereBetween('created_at',[$startMonth,$now])->count();
-        $thisMonthTotalAdmins = Admin::whereBetween('created_at',[$startMonth,$now])->count();
+
         $thisMonthTotalCompetitions = Competition::whereBetween('created_at',[$startMonth,$now])->count();
 
         $thisMonthTotalRevenues = $this->invoiceRepository->calculateInvoiceByStatus('Paid', $startMonth, $now, sumAmount: true);
@@ -52,7 +51,6 @@ class DashboardService extends Service
             'totalPlayers',
             'totalCoaches',
             'totalTeams',
-            'totalAdmins',
             'totalCompetitions',
             'totalUpcomingMatches',
             'totalUpcomingTrainings',
@@ -60,7 +58,6 @@ class DashboardService extends Service
             'thisMonthTotalPlayers',
             'thisMonthTotalCoaches',
             'thisMonthTotalTeams',
-            'thisMonthTotalAdmins',
             'thisMonthTotalCompetitions',
             'thisMonthTotalRevenues',
             'revenueGrowth'
@@ -68,41 +65,31 @@ class DashboardService extends Service
     }
 
     public function playerAgeChart(){
-        $results = DB::table('teams')
-            ->join('player_teams as pt', 'teams.id', '=', 'pt.teamId')
+        $results = Team::join('player_teams as pt', 'teams.id', '=', 'pt.teamId')
             ->select('ageGroup', DB::raw('COUNT(playerId) AS total_player'))
             ->where('teamSide', '=', 'Academy Team')
             ->groupBy('ageGroup')
             ->get();
 
-        $label = [];
-        $data = [];
-        foreach ($results as $result){
-            $label[] = $result->ageGroup;
-            $data[] = $result->total_player;
-        }
-
+        $label = $results->pluck('ageGroup');
+        $data = $results->pluck('total_player');
         return compact('label', 'data');
     }
 
     public function upcomingMatches(){
-        return Match::with('teams')
-            ->where('status', '1')
-            ->where('eventType', 'Match')
+        return MatchModel::with('teams')
+            ->where('status', 'Scheduled')
             ->orderBy('date')
             ->take(5)
             ->get();
-
     }
 
     public function upcomingTrainings(){
-        return Match::with('teams')
-            ->where('status', '1')
-            ->where('eventType', 'Training')
+        return Training::with('teams')
+            ->where('status', 'Scheduled')
             ->orderBy('date')
             ->take(4)
             ->get();
-
     }
 
 
