774a2a0cc07370f28129aab4119f529920e8f564
divawidia <wiartha2001@gmail.com>
2025-01-22 00:00:00 +0000

    feat (perfect scrollbar) : add function for tabs scrollbar

--- resources/js/perfect-scrollbar.js
+++ resources/js/perfect-scrollbar.js
@@ -2,14 +2,27 @@ import PerfectScrollbar from 'perfect-scrollbar';
 
 const container = document.querySelector('#container');
 const sidebar = document.querySelector('.sidebar');
+const tabs = document.querySelector('.nav-tabs-container');
 
 function ps(selectorElement){
     new PerfectScrollbar(selectorElement, {
-        wheelSpeed: 2,
+        wheelSpeed: 1,
         wheelPropagation: true,
         minScrollbarLength: 20
     });
 }
 
+function tabsPerfectScrollbar(scrollableTabs) {
+    new PerfectScrollbar(scrollableTabs, {
+        wheelSpeed: 1,
+        wheelPropagation: true,
+        suppressScrollY: true, // Disable vertical scrolling
+    });
+}
+
 ps(container);
 ps(sidebar);
+if (tabs !== null) {
+    tabsPerfectScrollbar(tabs);
+}
+
