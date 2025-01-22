d995c6fe8d7663ca5499f6e6826712579de844be
divawidia <wiartha2001@gmail.com>
2025-01-22 00:00:00 +0000

    refactor (navbar component) : update tabs

--- resources/views/components/tabs/navbar.blade.php
+++ resources/views/components/tabs/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light border-bottom border-top py-3">
-    <div class="container">
+    <div class="nav-tabs-container">
         <ul class="nav nav-pills text-capitalize">
             {{ $slot }}
         </ul>
