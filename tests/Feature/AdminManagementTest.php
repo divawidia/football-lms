6a36496d026922eb6355c113ce86827eb252ff57
divawidia <wiartha2001@gmail.com>
2025-01-23 00:00:00 +0000

    refactor (admin management feature test) : update several methods

--- tests/Feature/AdminManagementTest.php
+++ tests/Feature/AdminManagementTest.php
@@ -4,8 +4,12 @@ namespace Tests\Feature;
 
 use App\Models\Admin;
 use App\Models\User;
+use App\Notifications\AdminManagement;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
@@ -13,30 +17,24 @@ use Tests\TestCase;
 
 class AdminManagementTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
     use RefreshDatabase;
 
-    protected $superAdmin;
+    protected $superAdminUser;
+    protected $adminUser;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-        // Ensure roles are created
-        Role::firstOrCreate(['name' => 'Super-Admin']);
-        Role::firstOrCreate(['name' => 'admin']);
-
-        // Create a super admin for authentication
-        $this->superAdminUser = User::factory()->create();
+        $this->superAdminUser = User::factory()->has(Admin::factory())->create();
         $this->superAdminUser->assignRole('Super-Admin');
-        $this->superAdmin = Admin::factory()->create(['userId' => $this->superAdminUser->id]);
-        $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        $this->adminUser = User::factory()->has(Admin::factory())->create();
+        $this->adminUser->assignRole('admin');
     }
 
     /** @test */
-    public function authenticated_super_admin_can_view_admin_index()
+    public function test_authenticated_super_admin_can_view_admin_index()
     {
         $this->actingAs($this->superAdminUser);
 
@@ -45,103 +43,142 @@ class AdminManagementTest extends TestCase
         $response->assertViewIs('pages.managements.admins.index');
     }
 
-    /** @test */
-    public function super_admin_can_create_new_admin()
+    public function test_authenticated_admin_can_view_admin_index()
     {
+        $this->actingAs($this->adminUser);
+
+        $response = $this->get(route('admin-managements.index'));
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.managements.admins.index');
+    }
+
+    public function test_authenticated_super_admin_can_view_create_admin_page()
+    {
+        $this->actingAs($this->superAdminUser);
+
+        $response = $this->get(route('admin-managements.create'));
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.managements.admins.create');
+    }
+
+    public function test_authenticated_admin_cannot_view_create_admin_page()
+    {
+        $this->actingAs($this->adminUser);
+
+        $response = $this->get(route('admin-managements.create'));
+        $response->assertStatus(403);
+        $response->assertForbidden();
+    }
+
+    /** @test */
+    public function test_super_admin_can_store_new_admin()
+    {
+        Notification::fake();
         Storage::fake('public/assets/user-profile/');
 
         $this->actingAs($this->superAdminUser);
 
-        $userData = User::factory()->make()->toArray();
-        $adminData = Admin::factory()->make()->toArray();
-        $data = array_merge($userData, $adminData);
+        $data = User::factory()->make()->toArray();
         $data['password'] = 'Password123!';
         $data['password_confirmation'] = 'Password123!';
+        $data['position'] = 'Super Admin';
+        $data['hireDate'] = fake()->date;
         $data['foto'] = UploadedFile::fake()->image('avatar.jpg');
 
         $response = $this->post(route('admin-managements.store'), $data);
 
         $response->assertRedirect(route('admin-managements.index'));
         $this->assertDatabaseHas('users', [
-            'firstName' => $userData['firstName'],
-            'lastName' => $userData['lastName'],
-            'email' => $userData['email'],
+            'firstName' => $data['firstName'],
+            'lastName' => $data['lastName'],
+            'email' => $data['email'],
         ]);
         $this->assertDatabaseHas('admins', [
-            'position' => $adminData['position'],
+            'position' => $data['position'],
         ]);
+        Notification::assertSentTo($this->superAdminUser, AdminManagement::class);
     }
 
     /** @test */
-    public function super_admin_can_update_admin()
+    public function test_super_admin_can_update_admin()
     {
         $this->actingAs($this->superAdminUser);
 
         $updateData = [
-            'email' => $this->superAdminUser->email,
-            'gender' => $this->superAdminUser->gender,
-            'dob' => $this->superAdminUser->dob,
-            'address' => $this->superAdminUser->address,
-            'zipCode' => $this->superAdminUser->zipCode,
-            'country_id' => $this->superAdminUser->country_id,
-            'state_id' => $this->superAdminUser->state_id,
-            'city_id' => $this->superAdminUser->city_id,
-            'hireDate' => $this->superAdmin->hireDate,
-            'firstName' => 'Updated',
-            'lastName' => 'Name',
+            'email' => 'test@example.com',
+            'gender' => 'male',
+            'dob' => fake()->date,
+            'address' => 'test address',
+            'zipCode' => '80361',
+            'country_id' => $this->adminUser->country_id,
+            'state_id' => $this->adminUser->state_id,
+            'city_id' => $this->adminUser->city_id,
+            'hireDate' => fake()->date,
+            'firstName' => 'updated name',
+            'lastName' => 'updated name',
             'position' => 'Updated Position',
             'phoneNumber' => '9876543210',
         ];
 
-        $response = $this->put(route('admin-managements.update', $this->superAdmin), $updateData);
+        $response = $this->put(route('admin-managements.update', $this->adminUser->admin), $updateData);
 
         $response->assertRedirect(route('admin-managements.index'));
-
-        $this->superAdmin->refresh();
-        $this->superAdminUser->refresh();
-
-        $this->assertEquals('Updated', $this->superAdminUser->firstName);
-        $this->assertEquals('Name', $this->superAdminUser->lastName);
-        $this->assertEquals('Updated Position', $this->superAdmin->position);
+        $this->adminUser->refresh();
+        $this->assertDatabaseHas('users', [
+            'firstName' => $updateData['firstName'],
+            'lastName' => $updateData['lastName'],
+            'email' => $updateData['email'],
+            'dob' => $updateData['dob'],
+            'gender' => $updateData['gender'],
+            'address' => $updateData['address'],
+            'state_id' => $updateData['state_id'],
+            'city_id' => $updateData['city_id'],
+            'country_id' => $updateData['country_id'],
+            'zipCode' => $updateData['zipCode'],
+            'phoneNumber' => $updateData['phoneNumber'],
+        ]);
+        $this->assertDatabaseHas('admins', [
+            'position' => $updateData['position'],
+            'hireDate' => $updateData['hireDate'],
+        ]);
     }
 
     /** @test */
-    public function super_admin_can_deactivate_admin()
+    public function test_super_admin_can_deactivate_admin()
     {
         $this->actingAs($this->superAdminUser);
 
-        // Test deactivation
-        $deactivateResponse = $this->patch(route('deactivate-admin', $this->superAdmin), ['status' => 0]);
+        $deactivateResponse = $this->patch(route('admin-managements.deactivate', $this->adminUser->admin));
+
         $deactivateResponse->assertStatus(200);
-
-        $this->superAdminUser->refresh();
-        $this->assertEquals('0', $this->superAdminUser->status);
+        $this->adminUser->refresh();
+        $this->assertDatabaseHas('users', [
+            'status' => '0',
+        ]);
     }
 
-    public function super_admin_can_activate_admin()
+    public function test_super_admin_can_activate_admin()
     {
         $this->actingAs($this->superAdminUser);
-        $adminUser = User::factory()->create();
-        $adminUser->assignRole('admin');
-        $admin = Admin::factory()->create(['userId' => $adminUser->id]);
 
-        // Test activation
-        $activateResponse = $this->patch(route('activate-admin', $admin), ['status' => 1]);
+        $activateResponse = $this->patch(route('activate-admin', $this->adminUser->admin));
+
         $activateResponse->assertStatus(200);
-
-        $adminUser->refresh();
-        $this->assertEquals('1', $adminUser->status);
+        $this->adminUser->refresh();
+        $this->assertDatabaseHas('users', [
+            'status' => '1',
+        ]);
     }
 
     /** @test */
-    public function super_admin_can_delete_admin()
+    public function test_super_admin_can_delete_admin()
     {
         $this->actingAs($this->superAdminUser);
 
-        $response = $this->delete(route('admin-managements.destroy', $this->superAdmin));
+        $response = $this->delete(route('admin-managements.destroy', $this->adminUser->admin));
 
         $response->assertStatus(200);
-        $this->assertDatabaseMissing('users', ['id' => $this->superAdminUser->id]);
-        $this->assertDatabaseMissing('admins', ['id' => $this->superAdmin->id]);
+        $this->assertDatabaseMissing('users', ['id' => $this->adminUser->id]);
+        $this->assertDatabaseMissing('admins', ['id' => $this->adminUser->admin->id]);
     }
 }
