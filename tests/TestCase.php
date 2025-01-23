4c0ef2f3add5becba6bb37ccbee95607dacdbe66
divawidia <wiartha2001@gmail.com>
2025-01-23 00:00:00 +0000

    feat (test case) : create setUp methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Spatie\Permission\Models\Role;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Ensure roles are created
+        Role::firstOrCreate(['name' => 'Super-Admin']);
+        Role::firstOrCreate(['name' => 'admin']);
+        Role::firstOrCreate(['name' => 'coach']);
+        Role::firstOrCreate(['name' => 'player']);
+    }
 }
