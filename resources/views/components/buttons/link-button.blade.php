64c3faa9ae8d57cee56928193eb452d1a06a8c08
divawidia <wiartha2001@gmail.com>
2025-01-23 00:00:00 +0000

    refactor (link button component) : add dropdownItem parameter

--- resources/views/components/buttons/link-button.blade.php
+++ resources/views/components/buttons/link-button.blade.php
@@ -1,3 +1,5 @@
-<a class="btn btn-{{ $color }} btn-{{ $size }} {{ $margin }}" href="{{ $href }}" id="{{ $id }}">
+<a class="@if($dropdownItem) dropdown-item @else btn @endif btn-{{ $color }} btn-{{ $size }} {{ $margin }}"
+   href="{{ $href }}"
+   id="{{ $id }}">
     <span class="material-icons mr-2">{{ $icon }}</span> {{ $text }}
 </a>
