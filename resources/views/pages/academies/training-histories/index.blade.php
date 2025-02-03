ea8e45e7b1801ae6125c51d93de14dd4c0ef7cfe
divawidia <wiartha2001@gmail.com>
2025-02-03 15:44:05 +0800

    feat (training histories) : create index page

---
+++ resources/views/pages/academies/training-histories/index.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.master')
+@section('title')
+    Training Schedules
+@endsection
+@section('page-title')
+    @yield('title')
+@endsection
+
+@section('modal')
+    <x-modal.trainings.add-training/>
+    <x-modal.trainings.edit-training/>
+@endsection
+
+@section('content')
+    <div class="pt-32pt">
+        <div class="container">
+            <h2 class="mb-0">@yield('title')</h2>
+            <ol class="breadcrumb p-0 m-0">
+                <li class="breadcrumb-item"><a href="{{ checkRoleDashboardRoute() }}">Home</a></li>
+                <li class="breadcrumb-item active">
+                    @yield('title')
+                </li>
+            </ol>
+        </div>
+    </div>
+
+    <div class="container page-section">
+        @if(isAllAdmin() || isCoach())
+            <x-buttons.basic-button icon="add" text="Add New" additionalClass="add-training-btn" margin="mb-3"/>
+        @endif
+        <x-tables.training-tables :route="$tableRoute" tableId="tables"/>
+
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">Calendar</h4>
+            </div>
+            <div class="card-body">
+                <x-schedule-calendar :events="$events" calendarId="calendar"/>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('addon-script')
+    <script>
+        $(document).ready(function () {
+            processWithConfirmation(
+                '.cancelBtn',
+                "{{ route('training-schedules.cancel', ['training' =>':id']) }}",
+                "{{ route('training-schedules.index') }}",
+                'PATCH',
+                "Are you sure to cancel this training?",
+                "Something went wrong when cancelling this training!",
+                "{{ csrf_token() }}"
+            );
+
+            processWithConfirmation(
+                '.scheduled-btn',
+                "{{ route('training-schedules.scheduled', ['training' =>':id']) }}",
+                "{{ route('training-schedules.index') }}",
+                'PATCH',
+                "Are you sure to set this training to scheduled?",
+                "Something went wrong when set this training to scheduled!",
+                "{{ csrf_token() }}"
+            );
+        });
+    </script>
+@endpush
