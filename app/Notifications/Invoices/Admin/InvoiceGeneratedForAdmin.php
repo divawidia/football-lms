a52111fb314b8ed1f0155293deb7e017a3b0b844
divawidia <wiartha2001@gmail.com>
2025-02-12 00:00:00 +0000

    refactor (InvoiceGeneratedForAdmin notification) : update notification message

--- app/Notifications/Invoices/Admin/InvoiceGeneratedForAdmin.php
+++ app/Notifications/Invoices/Admin/InvoiceGeneratedForAdmin.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace App\Notifications\Invoices;
+namespace App\Notifications\Invoices\Admin;
 
+use App\Models\Invoice;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class InvoiceGeneratedAdmin extends Notification implements ShouldQueue
+class InvoiceGeneratedForAdmin extends Notification implements ShouldQueue
 {
     use Queueable;
-    protected $invoice;
-    protected $playerName;
-
-    public function __construct($invoice, $playerName)
+    protected Invoice $invoice;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Invoice $invoice)
     {
         $this->invoice = $invoice;
-        $this->playerName = $playerName;
     }
 
     /**
@@ -38,13 +39,14 @@ class InvoiceGeneratedAdmin extends Notification implements ShouldQueue
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject("New Invoice #{$this->invoice->invoiceNumber} Created for Player {$this->playerName}")
-            ->line("A new invoice #{$this->invoice->invoiceNumber} has been created for player: {$this->playerName}.")
+            ->subject("New Invoice #{$this->invoice->invoiceNumber} Created")
+            ->greeting("Hello {$notifiable->firstName} {$notifiable->lastName}!")
+            ->line("A new invoice #{$this->invoice->invoiceNumber} has been created for player: ".getUserFullName($this->invoice->receiverUser))
             ->line("Invoice Number: {$this->invoice->invoiceNumber}")
             ->line("Amount Due: ".priceFormat($this->invoice->ammountDue))
-            ->line("Due Date: {$this->invoice->dueDate}")
-            ->action('View Invoice here', route('invoices.show', ['invoice' => $this->invoice->id]))
-            ->line("Please review the invoice details in the invoices page. Please follow up the player {$this->playerName} to pay the invoice as soon as possible.")
+            ->line("Due Date: ".convertToDatetime($this->invoice->dueDate))
+            ->action('View Invoice here', route('invoices.show', ['invoice' => $this->invoice->hash]))
+            ->line("Please review the invoice details in the invoices page. Please follow up the player ".getUserFullName($this->invoice->receiverUser)." to pay the invoice as soon as possible.")
             ->line("If payment is not received by the due date, a reminder will be sent automatically to the player.")
             ->line("You can manage this invoice directly from the invoices page to track its status and take any required action.");
     }
@@ -57,8 +59,9 @@ class InvoiceGeneratedAdmin extends Notification implements ShouldQueue
     public function toArray(object $notifiable): array
     {
         return [
-            'data' =>'💼 A new invoice '.$this->invoice->invoiceNumber.' has been created for '.$this->playerName.'. Please follow up the player to pay the invoice as soon as possible.!',
-            'redirectRoute' => route('invoices.show', ['invoice' => $this->invoice->id])
+            'title' => "New Invoice has been created",
+            'data' =>'A new invoice '.$this->invoice->invoiceNumber.' has been created for '.getUserFullName($this->invoice->receiverUser).'. Please follow up the player to pay the invoice as soon as possible.!',
+            'redirectRoute' => route('invoices.show', ['invoice' => $this->invoice->hash])
         ];
     }
 }
