db8ee08c59d3807150df458fb8f305bb2036196c
divawidia <wiartha2001@gmail.com>
2025-02-12 00:00:00 +0000

    refactor (detail invoice page) : update invoice variables

--- resources/views/pages/payments/billing-payments/detail.blade.php
+++ resources/views/pages/payments/billing-payments/detail.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title')
-    Invoices {{ $data['invoice']->invoiceNumber }}
+    Invoices {{ $data->invoiceNumber }}
 @endsection
 @section('page-title')
     @yield('title')
@@ -8,7 +8,7 @@
 
 @section('content')
     <nav class="navbar navbar-light border-bottom border-top px-0">
-        <div class="container page__container">
+        <div class="container">
             <ul class="nav navbar-nav">
                 <li class="nav-item">
                     <a href="{{ route('billing-and-payments.index') }}" class="nav-link text-70"><i class="material-icons icon--left">keyboard_backspace</i> Back to Billings and Payments</a>
@@ -20,47 +20,44 @@
     <!-- BEFORE Page Content -->
 
     <div class="page-section bg-primary border-bottom-2">
-        <div class="container page__container">
+        <div class="container">
             <div class="row">
                 <div class="col-lg-9">
                     <div class="row">
                         <div class="col-md-6 mb-24pt mb-lg-0">
                             <p class="text-white-70 mb-0"><strong>Prepared for</strong></p>
-                            <h2 class="text-white">{{ $data['invoice']->receiverUser->firstName }} {{ $data['invoice']->receiverUser->lastName }}</h2>
+                            <h2 class="text-white">{{ $data->receiverUser->firstName }} {{ $data->receiverUser->lastName }}</h2>
                             <p class="text-white-50">
-                                {{ $data['invoice']->receiverUser->address }}
-                                <br>{{ $data['invoice']->receiverUser->city->name }}
-                                <br>{{ $data['invoice']->receiverUser->state->name }}
-                                <br>{{ $data['invoice']->receiverUser->country->name }}
-                                <br>{{ $data['invoice']->receiverUser->zipCode }}
+                                {{ $data->receiverUser->address }}
+                                <br>{{ $data->receiverUser->city->name }}
+                                <br>{{ $data->receiverUser->state->name }}
+                                <br>{{ $data->receiverUser->country->name }}
+                                <br>{{ $data->receiverUser->zipCode }}
                             </p>
                         </div>
                         <div class="col-md-6">
                             <p class="text-white-70 mb-0"><strong>Prepared by</strong></p>
-                            <h2 class="text-white">{{ $data['invoice']->academy->academyName }}</h2>
+                            <h2 class="text-white">{{ $data->academy->academyName }}</h2>
                             <p class="text-white-50">
-                                {{ $data['invoice']->academy->address }}
-                                <br>{{ $data['invoice']->academy->city->name }}
-                                <br>{{ $data['invoice']->academy->state->name }}
-                                <br>{{ $data['invoice']->academy->country->name }}
-                                <br>{{ $data['invoice']->academy->zipCode }}
+                                {{ $data->academy->address }}
+                                <br>{{ $data->academy->city->name }}
+                                <br>{{ $data->academy->state->name }}
+                                <br>{{ $data->academy->country->name }}
+                                <br>{{ $data->academy->zipCode }}
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 text-lg-right d-flex flex-lg-column mb-24pt mb-lg-0 border-bottom border-lg-0 pb-16pt pb-lg-0">
-                    <div class="flex">
-                        <p class="text-white-70 mb-8pt"><strong>Invoice {{ $data['invoice']->invoiceNumber }}</strong></p>
-                        <p class="text-white-50">
-                            {{ $data['createdDate'] }}
-                        </p>
+                <div class="col-lg-3 text-lg-right mb-24pt mb-lg-0 border-bottom border-lg-0 pb-16pt pb-lg-0">
+                    <div>
+                        <p class="text-white-70 mb-8pt"><strong>Invoice {{ $data->invoiceNumber }}</strong></p>
+                        <p class="text-white-50">{{ $data->created_at }}</p>
                     </div>
-                    @if($data['invoice']->status == 'Open')
-                        <x-pay-invoice-button btnClass="btn btn-primary mb-3" btnText="Pay Now!" :invoiceId="$data['invoice']->id" :snapToken="$data['invoice']->snapToken"/>
-                    @endif
-                    <a href="javascript:window.print()" class="btn btn-outline-white" id="{{$data['invoice']->id}}">
-                        <span class="material-icons">file_download</span>
-                        Download Invoice
-                    </a>
+                    <div>
+                        @if($data->status == 'Open')
+                            <x-pay-invoice-button btnClass="btn btn-primary mb-lg-3 mr-2 mr-lg-0" btnText="Pay Now!" :invoiceId="$data->id" :snapToken="$data->snapToken"/>
+                        @endif
+                        <x-buttons.link-button  href="javascript:window.print()" :id="$data->hash" icon="file_download" color="outline-white" text="Download Invoice"/>
+                    </div>
                 </div>
             </div>
         </div>
@@ -79,34 +76,33 @@
             <div class="card-body flex-column">
                 <div class="d-flex align-items-center">
                     <div class="p-2"><p class="card-title mb-4pt">Invoice Status :</p></div>
-                    @if ($data['invoice']->status == 'Open')
-                        <span class="ml-auto p-2 badge badge-pill badge-info">{{ $data['invoice']->status }}</span>
-                    @elseif($data['invoice']->status == 'Paid')
-                        <span class="ml-auto p-2 badge badge-pill badge-success">{{ $data['invoice']->status }}</span>
-                    @elseif ($data['invoice']->status == 'Past Due')
-                        <span class="ml-auto p-2 badge badge-pill badge-warning">{{ $data['invoice']->status }}</span>
-                    @elseif ($data['invoice']->status == 'Uncollectible')
-                        <span class="ml-auto p-2 badge badge-pill badge-danger">{{ $data['invoice']->status }}</span>
+                    @if ($data->status == 'Open')
+                        <span class="ml-auto p-2 badge badge-pill badge-info">{{ $data->status }}</span>
+                    @elseif($data->status == 'Paid')
+                        <span class="ml-auto p-2 badge badge-pill badge-success">{{ $data->status }}</span>
+                    @elseif ($data->status == 'Past Due')
+                        <span class="ml-auto p-2 badge badge-pill badge-warning">{{ $data->status }}</span>
+                    @elseif ($data->status == 'Uncollectible')
+                        <span class="ml-auto p-2 badge badge-pill badge-danger">{{ $data->status }}</span>
                     @endif
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Created At :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['createdAt'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->created_at) }}</div>
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Due Date :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['dueDate'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->dueDate) }}</div>
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Last updated at :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['updatedAt'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->updated_at) }}</div>
                 </div>
                 <div class="d-flex align-items-center">
                     <div class="p-2"><p class="card-title mb-4pt">Created by :</p></div>
-                    @if($data['invoice']->creatorUser)
-                        <div class="ml-auto p-2 text-muted">{{ $data['invoice']->creatorUser->firstName }} {{ $data['invoice']->creatorUser->lastName }}</div>
-                        @endif
-
+                    @if($data->creatorUser)
+                        <div class="ml-auto p-2 text-muted">{{ $data->creatorUser->firstName }} {{ $data->creatorUser->lastName }}</div>
+                    @endif
                 </div>
             </div>
         </div>
@@ -126,7 +122,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($data['invoice']->products as $product)
+                    @foreach($data->products as $product)
                         <tr>
                             <td>
                                 <p class="mb-0"><strong>{{ $product->productName }}</strong></p>
@@ -143,19 +139,19 @@
                 <tr>
                     <td class="text-right text-70"><strong>Subtotal :</strong></td>
                     <td style="width: 130px;"
-                        class="text-right"><strong>Rp. {{ number_format($data['invoice']->subtotal) }}</strong></td>
+                        class="text-right"><strong>Rp. {{ number_format($data->subtotal) }}</strong></td>
                 </tr>
-                @if($data['invoice']->tax)
+                @if($data->tax)
                     <tr>
-                        <td class="text-right text-70"><strong>Tax {{ $data['invoice']->tax->taxName }} ~ {{ $data['invoice']->tax->percentage }}% :</strong></td>
+                        <td class="text-right text-70"><strong>Tax {{ $data->tax->taxName }} ~ {{ $data->tax->percentage }}% :</strong></td>
                         <td style="width: 130px;"
-                            class="text-right"><strong>Rp. {{ number_format($data['invoice']->totalTax) }}</strong></td>
+                            class="text-right"><strong>Rp. {{ number_format($data->totalTax) }}</strong></td>
                     </tr>
                 @endif
                 <tr>
                     <td class="text-right text-70"><strong>Total :</strong></td>
                     <td style="width: 130px;"
-                        class="text-right"><strong>Rp. {{ number_format($data['invoice']->ammountDue) }}</strong></td>
+                        class="text-right"><strong>Rp. {{ number_format($data->ammountDue) }}</strong></td>
                 </tr>
                 </tfoot>
             </table>
