77d997a090b4019e54ea78a04141c80997fa2263
divawidia <wiartha2001@gmail.com>
2025-02-12 00:00:00 +0000

    refactor (archieved invoice detail page) : update several component and variables

--- resources/views/pages/payments/invoices/detail-archived.blade.php
+++ resources/views/pages/payments/invoices/detail-archived.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 @section('title')
-    Invoices {{ $data['invoice']->invoiceNumber }}
+    Invoices {{ $data->invoiceNumber }}
 @endsection
 @section('page-title')
     @yield('title')
@@ -8,7 +8,7 @@
 
 @section('content')
     <nav class="navbar navbar-light border-bottom border-top px-0">
-        <div class="container page__container">
+        <div class="container">
             <ul class="nav navbar-nav">
                 <li class="nav-item">
                     <a href="{{ route('invoices.archived') }}" class="nav-link text-70"><i class="material-icons icon--left">keyboard_backspace</i> Back to Archived Invoices</a>
@@ -26,56 +26,37 @@
                     <div class="row">
                         <div class="col-md-6 mb-24pt mb-lg-0">
                             <p class="text-white-70 mb-0"><strong>Prepared for</strong></p>
-                            <h2 class="text-white">{{ $data['invoice']->receiverUser->firstName }} {{ $data['invoice']->receiverUser->lastName }}</h2>
+                            <h2 class="text-white">{{ $data->receiverUser->firstName }} {{ $data->receiverUser->lastName }}</h2>
                             <p class="text-white-50">
-                                {{ $data['invoice']->receiverUser->address }}
-                                <br>{{ $data['invoice']->receiverUser->city->name }}
-                                <br>{{ $data['invoice']->receiverUser->state->name }}
-                                <br>{{ $data['invoice']->receiverUser->country->name }}
-                                <br>{{ $data['invoice']->receiverUser->zipCode }}
+                                {{ $data->receiverUser->address }}
+                                <br>{{ $data->receiverUser->city->name }}
+                                <br>{{ $data->receiverUser->state->name }}
+                                <br>{{ $data->receiverUser->country->name }}
+                                <br>{{ $data->receiverUser->zipCode }}
                             </p>
                         </div>
                         <div class="col-md-6">
                             <p class="text-white-70 mb-0"><strong>Prepared by</strong></p>
-                            <h2 class="text-white">{{ $data['invoice']->academy->academyName }}</h2>
+                            <h2 class="text-white">{{ $data->academy->academyName }}</h2>
                             <p class="text-white-50">
-                                {{ $data['invoice']->academy->address }}
-                                <br>{{ $data['invoice']->academy->city->name }}
-                                <br>{{ $data['invoice']->academy->state->name }}
-                                <br>{{ $data['invoice']->academy->country->name }}
-                                <br>{{ $data['invoice']->academy->zipCode }}
+                                {{ $data->academy->address }}
+                                <br>{{ $data->academy->city->name }}
+                                <br>{{ $data->academy->state->name }}
+                                <br>{{ $data->academy->country->name }}
+                                <br>{{ $data->academy->zipCode }}
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-3 text-lg-right d-flex flex-lg-column mb-24pt mb-lg-0 border-bottom border-lg-0 pb-16pt pb-lg-0">
                     <div class="flex">
-                        <p class="text-white-70 mb-8pt"><strong>Invoice {{ $data['invoice']->invoiceNumber }}</strong></p>
-                        <p class="text-white-50">
-                            {{ $data['createdDate'] }}
-                        </p>
+                        <p class="text-white-70 mb-8pt"><strong>Invoice {{ $data->invoiceNumber }}</strong></p>
+                        <p class="text-white-50">{{ $data['createdDate'] }}</p>
                     </div>
-                    <div class="dropdown">
-                        <button class="btn btn-outline-white" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Action
-                            <span class="material-icons ml-3">
-                                keyboard_arrow_down
-                            </span>
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a href="javascript:window.print()" class="dropdown-item" id="{{$data['invoice']->id}}">
-                                <span class="material-icons">file_download</span>
-                                Download Invoice
-                            </a>
-                            <button type="button" class="dropdown-item restoreInvoice">
-                                <span class="material-icons text-success">restore</span>
-                                Restore Invoice
-                            </button>
-                            <button type="button" class="dropdown-item forceDeleteInvoice">
-                                <span class="material-icons text-danger">delete</span>
-                                Permanently Delete Invoice
-                            </button>
-                        </div>
-                    </div>
+                    <x-buttons.dropdown title="Action" icon="keyboard_arrow_down" btnColor="outline-white" iconMargin="ml-3">
+                        <x-buttons.link-button :dropdownItem="true" href="javascript:window.print()" :id="$data->hash" icon="file_download" color="white" text="Download Invoice"/>
+                        <x-buttons.basic-button icon="restore" iconColor="danger" color="white" text="Restore Invoice" additionalClass="restoreInvoice" :dropdownItem="true"/>
+                        <x-buttons.basic-button icon="delete" iconColor="danger" color="white" text="Permanently Delete Invoice" additionalClass="forceDeleteInvoice" :dropdownItem="true"/>
+                    </x-buttons.dropdown>
                 </div>
             </div>
         </div>
@@ -85,40 +66,40 @@
 
     <!-- Page Content -->
 
-    <div class="page-section container page__container">
+    <div class="page-section container">
         <div class="page-separator">
             <div class="page-separator__text">Invoice Details</div>
         </div>
 
-        <div class="card card-sm card-group-row__card">
+        <div class="card">
             <div class="card-body flex-column">
                 <div class="d-flex align-items-center">
                     <div class="p-2"><p class="card-title mb-4pt">Invoice Status :</p></div>
-                    @if ($data['invoice']->status == 'Open')
-                        <span class="ml-auto p-2 badge badge-pill badge-info">{{ $data['invoice']->status }}</span>
-                    @elseif($data['invoice']->status == 'Paid')
-                        <span class="ml-auto p-2 badge badge-pill badge-success">{{ $data['invoice']->status }}</span>
-                    @elseif ($data['invoice']->status == 'Past Due')
-                        <span class="ml-auto p-2 badge badge-pill badge-warning">{{ $data['invoice']->status }}</span>
-                    @elseif ($data['invoice']->status == 'Uncollectible')
-                        <span class="ml-auto p-2 badge badge-pill badge-danger">{{ $data['invoice']->status }}</span>
+                    @if ($data->status == 'Open')
+                        <span class="ml-auto p-2 badge badge-pill badge-info">{{ $data->status }}</span>
+                    @elseif($data->status == 'Paid')
+                        <span class="ml-auto p-2 badge badge-pill badge-success">{{ $data->status }}</span>
+                    @elseif ($data->status == 'Past Due')
+                        <span class="ml-auto p-2 badge badge-pill badge-warning">{{ $data->status }}</span>
+                    @elseif ($data->status == 'Uncollectible')
+                        <span class="ml-auto p-2 badge badge-pill badge-danger">{{ $data->status }}</span>
                     @endif
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Created At :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['createdAt'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->created_at) }}</div>
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Due Date :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['dueDate'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->dueDate) }}</div>
                 </div>
                 <div class="d-flex align-items-center border-bottom">
                     <div class="p-2"><p class="card-title mb-4pt">Last updated at :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['updatedAt'] }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ convertToDatetime($data->updated_at) }}</div>
                 </div>
                 <div class="d-flex align-items-center">
                     <div class="p-2"><p class="card-title mb-4pt">Created by :</p></div>
-                    <div class="ml-auto p-2 text-muted">{{ $data['invoice']->creatorUser->firstName }} {{ $data['invoice']->creatorUser->lastName }}</div>
+                    <div class="ml-auto p-2 text-muted">{{ $data->creatorUser->firstName }} {{ $data->creatorUser->lastName }}</div>
                 </div>
             </div>
         </div>
@@ -127,7 +108,7 @@
             <div class="page-separator__text">Invoice Summary</div>
         </div>
 
-        <div class="card table-responsive mb-24pt">
+        <div class="card">
             <table class="table table-flush table--elevated">
                 <thead>
                     <tr>
@@ -138,7 +119,7 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($data['invoice']->products as $product)
+                    @foreach($data->products as $product)
                         <tr>
                             <td>
                                 <p class="mb-0"><strong>{{ $product->productName }}</strong></p>
@@ -155,131 +136,46 @@
                 <tr>
                     <td class="text-right text-70"><strong>Subtotal :</strong></td>
                     <td style="width: 130px;"
-                        class="text-right"><strong>Rp. {{ number_format($data['invoice']->subtotal) }}</strong></td>
+                        class="text-right"><strong>Rp. {{ number_format($data->subtotal) }}</strong></td>
                 </tr>
-                @if($data['invoice']->tax)
+                @if($data->tax)
                     <tr>
-                        <td class="text-right text-70"><strong>Tax {{ $data['invoice']->tax->taxName }} ~ {{ $data['invoice']->tax->percentage }}% :</strong></td>
+                        <td class="text-right text-70"><strong>Tax {{ $data->tax->taxName }} ~ {{ $data->tax->percentage }}% :</strong></td>
                         <td style="width: 130px;"
-                            class="text-right"><strong>Rp. {{ number_format($data['invoice']->totalTax) }}</strong></td>
+                            class="text-right"><strong>Rp. {{ number_format($data->totalTax) }}</strong></td>
                     </tr>
                 @endif
                 <tr>
                     <td class="text-right text-70"><strong>Total :</strong></td>
                     <td style="width: 130px;"
-                        class="text-right"><strong>Rp. {{ number_format($data['invoice']->ammountDue) }}</strong></td>
+                        class="text-right"><strong>Rp. {{ number_format($data->ammountDue) }}</strong></td>
                 </tr>
                 </tfoot>
             </table>
         </div>
     </div>
-
-    <x-process-data-confirmation btnClass=".restoreInvoice"
-                                 :processRoute="route('invoices.restore', ['invoice' => $data['invoice']->id])"
-                                 :routeAfterProcess="route('invoices.index')"
-                                 method="POST"
-                                 confirmationText="Are you sure to restore this invoice?"
-                                 errorText="Something went wrong when restoring the invoice!"/>
-
-    <x-process-data-confirmation btnClass=".forceDeleteInvoice"
-                                 :processRoute="route('invoices.permanent-delete', ['invoice' => $data['invoice']->id])"
-                                 :routeAfterProcess="route('invoices.index')"
-                                 method="DELETE"
-                                 confirmationText="Are you sure to permanently delete this invoice?"
-                                 errorText="Something went wrong when deleting the invoice!"/>
 @endsection
 @push('addon-script')
     <script>
         $(document).ready(function () {
-            // restore invoice
-            {{--$('.restoreInvoice').on('click', function () {--}}
-            {{--    Swal.fire({--}}
-            {{--        title: "Are you sure to restore this invoice?",--}}
-            {{--        text: "You won't be able to revert this!",--}}
-            {{--        icon: "warning",--}}
-            {{--        showCancelButton: true,--}}
-            {{--        confirmButtonColor: "#1ac2a1",--}}
-            {{--        cancelButtonColor: "#E52534",--}}
-            {{--        confirmButtonText: "Yes, restore it!"--}}
-            {{--    }).then((result) => {--}}
-            {{--        if (result.isConfirmed) {--}}
-            {{--            $.ajax({--}}
-            {{--                url: "{{ route('invoices.restore', ['invoice' => $data['invoice']->id]) }}",--}}
-            {{--                type: 'POST',--}}
-            {{--                data: {--}}
-            {{--                    _token: "{{ csrf_token() }}"--}}
-            {{--                },--}}
-            {{--                success: function () {--}}
-            {{--                    Swal.fire({--}}
-            {{--                        title: 'Invoice successfully restored!',--}}
-            {{--                        icon: 'success',--}}
-            {{--                        showCancelButton: false,--}}
-            {{--                        confirmButtonColor: "#1ac2a1",--}}
-            {{--                        confirmButtonText:--}}
-            {{--                            'Ok!'--}}
-            {{--                    }).then((result) => {--}}
-            {{--                        if (result.isConfirmed) {--}}
-            {{--                            location.href = '{{ route('invoices.index') }}';--}}
-            {{--                        }--}}
-            {{--                    });--}}
-            {{--                },--}}
-            {{--                error: function (jqXHR, textStatus, errorThrown) {--}}
-            {{--                    Swal.fire({--}}
-            {{--                        icon: "error",--}}
-            {{--                        title: "Something went wrong when archiving data!",--}}
-            {{--                        text: errorThrown--}}
-            {{--                    });--}}
-            {{--                }--}}
-            {{--            });--}}
-            {{--        }--}}
-            {{--    });--}}
-            {{--});--}}
-
-            // force delete invoice
-            {{--$('.forceDeleteInvoice').on('click', function () {--}}
-            {{--    let id = $(this).attr('id');--}}
-
-            {{--    Swal.fire({--}}
-            {{--        title: "Are you sure to permanently delete this invoice?",--}}
-            {{--        text: "You won't be able to revert this!",--}}
-            {{--        icon: "warning",--}}
-            {{--        showCancelButton: true,--}}
-            {{--        confirmButtonColor: "#1ac2a1",--}}
-            {{--        cancelButtonColor: "#E52534",--}}
-            {{--        confirmButtonText: "Yes, delete it!"--}}
-            {{--    }).then((result) => {--}}
-            {{--        if (result.isConfirmed) {--}}
-            {{--            $.ajax({--}}
-            {{--                url: "{{ route('invoices.permanent-delete', ['invoice' => $data['invoice']->id]) }}",--}}
-            {{--                type: 'DELETE',--}}
-            {{--                data: {--}}
-            {{--                    _token: "{{ csrf_token() }}"--}}
-            {{--                },--}}
-            {{--                success: function () {--}}
-            {{--                    Swal.fire({--}}
-            {{--                        title: 'Invoice successfully permanently deleted!',--}}
-            {{--                        icon: 'success',--}}
-            {{--                        showCancelButton: false,--}}
-            {{--                        confirmButtonColor: "#1ac2a1",--}}
-            {{--                        confirmButtonText:--}}
-            {{--                            'Ok!'--}}
-            {{--                    }).then((result) => {--}}
-            {{--                        if (result.isConfirmed) {--}}
-            {{--                            location.href = '{{ route('invoices.index') }}';--}}
-            {{--                        }--}}
-            {{--                    });--}}
-            {{--                },--}}
-            {{--                error: function (jqXHR, textStatus, errorThrown) {--}}
-            {{--                    Swal.fire({--}}
-            {{--                        icon: "error",--}}
-            {{--                        title: "Something went wrong when archiving data!",--}}
-            {{--                        text: errorThrown--}}
-            {{--                    });--}}
-            {{--                }--}}
-            {{--            });--}}
-            {{--        }--}}
-            {{--    });--}}
-            {{--});--}}
+            processWithConfirmation(
+                ".restoreInvoice",
+                "{{ route('invoices.restore', ['invoice' => ':id']) }}",
+                "{{ route('invoices.index') }}",
+                "POST",
+                "Are you sure to restore this invoice?",
+                "Something went wrong when restoring the invoice!",
+                "{{ csrf_token() }}"
+            );
+            processWithConfirmation(
+                ".forceDeleteInvoice",
+                "{{ route('invoices.permanent-delete', ['invoice' => ':id']) }}",
+                "{{ route('invoices.index') }}",
+                "DELETE",
+                "Are you sure to permanently delete this invoice?",
+                "Something went wrong when deleting the invoice!",
+                "{{ csrf_token() }}"
+            );
         });
     </script>
 @endpush
