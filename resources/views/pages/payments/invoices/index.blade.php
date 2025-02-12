40d6c8a24e5e061f2531f9647c111d0488a3b4c1
divawidia <wiartha2001@gmail.com>
2025-02-12 00:00:00 +0000

    refactor (index invoice page) : update table with table components

--- resources/views/pages/payments/invoices/index.blade.php
+++ resources/views/pages/payments/invoices/index.blade.php
@@ -12,9 +12,7 @@
             <h2 class="mb-0 text-left">@yield('title')</h2>
             <ol class="breadcrumb p-0 m-0">
                 <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
-                <li class="breadcrumb-item active">
-                    @yield('title')
-                </li>
+                <li class="breadcrumb-item active">@yield('title')</li>
             </ol>
         </div>
     </div>
@@ -22,61 +20,15 @@
     <div class="container page-section">
         <div class="page-separator">
             <div class="page-separator__text">Invoices</div>
-            <a href="{{ route('invoices.archived') }}" class="btn btn-sm btn-white border-danger ml-auto ">
-                <span class="material-icons mr-2 text-danger">
-                    delete
-                </span>
-                Archived Invoice
-            </a>
-            <a href="{{ route('invoices.create') }}" class="btn btn-sm btn-primary ml-3 ">
-                <span class="material-icons mr-2">
-                    add
-                </span>
-                Add New
-            </a>
+            <x-buttons.link-button color="white border-danger" margin="ml-auto" :href="route('invoices.archived')" icon="delete" text="Archived Invoice"/>
+            <x-buttons.link-button color="primary" margin="ml-3" :href="route('invoices.create')" icon="add" text="Add New Invoice"/>
         </div>
         <div class="card">
             <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover mb-0" id="invoicesTable">
-                        <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Inovice Number</th>
-                            <th>Contact</th>
-                            <th>Email</th>
-                            <th>Due Date</th>
-                            <th>Created At</th>
-                            <th>Last Updated</th>
-                            <th>Subtotal</th>
-                            <th>Total Tax</th>
-                            <th>Total Amount</th>
-                            <th>Status</th>
-                            <th>Payment Method</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
+                <x-table tableId="invoicesTable" :headers="['#', 'Invoice Number', 'Contact', 'Email', 'Due Date', 'Created At', 'Last Updated', 'Subtotal', 'Total Tax', 'Total Amount', 'Status', 'Payment Method', 'Action']" />
             </div>
         </div>
     </div>
-
-    <x-process-data-confirmation btnClass=".setUcollectibleStatus"
-                                 :processRoute="route('invoices.set-uncollectible', ':id')"
-                                 :routeAfterProcess="route('invoices.index')"
-                                 method="PATCH"
-                                 confirmationText="Are you sure to mark this invoice to uncollectible?"
-                                 errorText="Something went wrong when marking the invoice to uncollectible!"/>
-
-    <x-process-data-confirmation btnClass=".deleteInvoice"
-                                 :processRoute="route('invoices.destroy', ['invoice' => ':id'])"
-                                 :routeAfterProcess="route('invoices.index')"
-                                 method="DELETE"
-                                 confirmationText="Are you sure to archive this invoice?"
-                                 errorText="Something went wrong when archiving data!"/>
 @endsection
 @push('addon-script')
     <script>
@@ -101,15 +53,29 @@
                     {data: 'ammount', name: 'ammount'},
                     {data: 'status', name: 'status'},
                     {data: 'paymentMethod', name: 'paymentMethod'},
-                    {
-                        data: 'action',
-                        name: 'action',
-                        orderable: false,
-                        searchable: false,
-                        width: '15%'
-                    },
+                    {data: 'action', name: 'action', orderable: false, searchable: false},
                 ]
             });
+
+            processWithConfirmation(
+                ".setUcollectibleStatus",
+                "{{ route('invoices.set-uncollectible', ':id') }}",
+                null,
+                "PATCH",
+                "Are you sure to mark this invoice as uncollectible?",
+                "Something went wrong when marking the invoice as uncollectible!",
+                "{{ csrf_token() }}"
+            );
+
+            processWithConfirmation(
+                ".deleteInvoice",
+                "{{ route('invoices.destroy', ['invoice' => ':id']) }}",
+                null,
+                "DELETE",
+                "Are you sure to archive this invoice?",
+                "Something went wrong when archiving data!",
+                "{{ csrf_token() }}"
+            );
         });
     </script>
 @endpush
