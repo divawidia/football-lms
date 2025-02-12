baaa7bdd0bc1480e9b9bbb4e0525f407bd2dced0
divawidia <wiartha2001@gmail.com>
2025-02-12 00:00:00 +0000

    refactor (InvoiceDueSoon notification) : update notification message

--- app/Notifications/Invoices/Player/InvoiceGeneratedForPlayer.php
+++ app/Notifications/Invoices/Player/InvoiceGeneratedForPlayer.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace App\Notifications\Invoices;
+namespace App\Notifications\Invoices\Player;
 
+use App\Models\Invoice;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class InvoiceGeneratedPlayer extends Notification implements ShouldQueue
+class InvoiceGeneratedForPlayer extends Notification implements ShouldQueue
 {
     use Queueable;
-    protected $invoice;
-    protected $playerName;
-
-    public function __construct($invoice, $playerName)
+    protected Invoice $invoice;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Invoice $invoice)
     {
         $this->invoice = $invoice;
-        $this->playerName = $playerName;
     }
 
     /**
@@ -39,12 +40,12 @@ class InvoiceGeneratedPlayer extends Notification implements ShouldQueue
     {
         return (new MailMessage)
             ->subject("Your New Invoice #{$this->invoice->invoiceNumber} is Ready")
-            ->greeting("Hello {$this->playerName},")
-            ->line("A new invoice #{$this->invoice->invoiceNumber} has been created for your product payments.")
+            ->greeting("Hello {$notifiable->firstName} {$notifiable->lastName}!")
+            ->line("A new invoice #{$this->invoice->invoiceNumber} has been created for your payments.")
             ->line("Invoice Number: {$this->invoice->invoiceNumber}")
             ->line("Amount Due: ".priceFormat($this->invoice->ammountDue))
             ->line("Due Date: ".convertToDatetime($this->invoice->dueDate))
-            ->action('View Invoice here', route('billing-and-payments.show', ['invoice' => $this->invoice->id]))
+            ->action('View Invoice here', route('billing-and-payments.show', ['invoice' => $this->invoice->hash]))
             ->line("Please ensure payment is completed as soon as possible by the due date.")
             ->line("If you have any questions, feel free to reach out to our support team.")
             ->line("Thank you!");
@@ -58,8 +59,9 @@ class InvoiceGeneratedPlayer extends Notification implements ShouldQueue
     public function toArray(object $notifiable): array
     {
         return [
-            'data' =>'💼 A new invoice '.$this->invoice->invoiceNumber.' has been created for you. Please pay your invoice before due date '.convertToDatetime($this->invoice->dueDate).' as soon as possible!',
-            'redirectRoute' => route('billing-and-payments.show', ['invoice' => $this->invoice->id])
+            'title' => "New Invoice has been sent to you",
+            'data' =>'A new invoice '.$this->invoice->invoiceNumber.' has been sent for you. Please pay your invoice before due date '.convertToDatetime($this->invoice->dueDate).' as soon as possible!',
+            'redirectRoute' => route('billing-and-payments.show', ['invoice' => $this->invoice->hash])
         ];
     }
 }
