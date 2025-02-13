be0e1563c2431016ff4782f2463fe67831735f02
divawidia <wiartha2001@gmail.com>
2025-02-13 00:00:00 +0000

    refactor (SubscriptionScheduledForPlayer notification) : update message and contructor

--- app/Notifications/Subscriptions/Player/SubscriptionScheduledForPlayer.php
+++ app/Notifications/Subscriptions/Player/SubscriptionScheduledForPlayer.php
@@ -2,23 +2,23 @@
 
 namespace App\Notifications\Subscriptions\Player;
 
+use App\Models\Subscription;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SubscriptionSchedulledForPlayer extends Notification implements ShouldQueue
+class SubscriptionScheduledForPlayer extends Notification implements ShouldQueue
 {
     use Queueable;
-    protected $subscription;
-    protected $playerName;
+    protected Subscription $subscription;
+
     /**
      * Create a new notification instance.
      */
-    public function __construct($subscription, $playerName)
+    public function __construct(Subscription $subscription)
     {
         $this->subscription = $subscription;
-        $this->playerName = $playerName;
     }
 
     /**
@@ -28,7 +28,10 @@ class SubscriptionSchedulledForPlayer extends Notification implements ShouldQueu
      */
     public function via(object $notifiable): array
     {
-        return ['mail','database'];
+        return [
+            'mail',
+            'database'
+        ];
     }
 
     /**
@@ -37,10 +40,10 @@ class SubscriptionSchedulledForPlayer extends Notification implements ShouldQueu
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject("Subscription for {$this->subscription->product->productName} Activated")
-            ->greeting('Hello, ' . $this->playerName)
+            ->subject("Academy Subscription Activated")
+            ->greeting("Hello, {$notifiable->firstName} {$notifiable->lastName}!")
             ->line("We are pleased to inform you that your subscription invoice for {$this->subscription->product->productName} has been successfully paid.")
-            ->line('Your subscription is now active and ready to use.')
+            ->line('Your subscription is now active.')
             ->action('View Subscription', route('billing-and-payments.index'))
             ->line('Thank you for choosing our Football Academy!');
     }
@@ -53,6 +56,7 @@ class SubscriptionSchedulledForPlayer extends Notification implements ShouldQueu
     public function toArray(object $notifiable): array
     {
         return [
+            'title' =>"Academy Subscription Activated/Scheduled",
             'data' => 'Your subscription for '.$this->subscription->product->productName.' has been scheduled.',
             'redirectRoute' => route('billing-and-payments.index')
         ];
