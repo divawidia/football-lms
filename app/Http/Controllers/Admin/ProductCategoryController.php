77a3796043d54aec01d2fe69b9fbd2e709067c7c
divawidia <wiartha2001@gmail.com>
2025-02-13 00:00:00 +0000

    refactor (ProductCategory controller) : update all methods

--- app/Http/Controllers/Admin/ProductCategoryController.php
+++ app/Http/Controllers/Admin/ProductCategoryController.php
@@ -2,13 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ProductCategoryRequest;
 use App\Models\ProductCategory;
 use App\Services\ProductCategoryService;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use RealRashid\SweetAlert\Facades\Alert;
+use Illuminate\Http\JsonResponse;
 
 class ProductCategoryController extends Controller
 {
@@ -22,7 +21,7 @@ class ProductCategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
         return $this->productCategoryService->index();
     }
@@ -30,54 +29,48 @@ class ProductCategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(ProductCategoryRequest $request)
+    public function store(ProductCategoryRequest $request): JsonResponse
     {
         $data = $request->validated();
-        $admin = Auth::user()->id;
-        return response()->json($this->productCategoryService->store($data, $admin));
+        return ApiResponse::success($this->productCategoryService->store($data, $this->getLoggedUser()), "Product category successfully added.");
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(ProductCategory $productCategory)
+    public function edit(ProductCategory $productCategory): JsonResponse
     {
-        return response()->json([
-            'status' => '200',
-            'data' => $productCategory,
-            'message' => 'Success'
-        ]);
+        return ApiResponse::success($productCategory);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(ProductCategoryRequest $request, ProductCategory $productCategory)
+    public function update(ProductCategoryRequest $request, ProductCategory $productCategory): JsonResponse
     {
         $data = $request->validated();
-
-        return response()->json($this->productCategoryService->update($data, $productCategory));
+        $this->productCategoryService->update($data, $productCategory);
+        return ApiResponse::success(message: "Product category : {$productCategory->categoryName} updated successfully.");
     }
 
-    public function activate(ProductCategory $productCategory){
+    public function activate(ProductCategory $productCategory): JsonResponse
+    {
         $this->productCategoryService->activate($productCategory);
-        $text = 'Product Category '. $productCategory->categoryName . ' status successfully activated!';
-        Alert::success($text);
-        return redirect()->route('products.index');
+        return ApiResponse::success(message: "Product category : {$productCategory->categoryName} successfully activated.");
     }
 
-    public function deactivate(ProductCategory $productCategory){
+    public function deactivate(ProductCategory $productCategory): JsonResponse
+    {
         $this->productCategoryService->deactivate($productCategory);
-        $text = 'Product Category '. $productCategory->categoryName . ' status successfully deactivated!';
-        Alert::success($text);
-        return redirect()->route('products.index');
+        return ApiResponse::success(message: "Product category : {$productCategory->categoryName} successfully deactivated.");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(ProductCategory $productCategory)
+    public function destroy(ProductCategory $productCategory): JsonResponse
     {
-        return response()->json($this->productCategoryService->destroy($productCategory));
+        $this->productCategoryService->destroy($productCategory);
+        return ApiResponse::success(message: "Product category : {$productCategory->categoryName} successfully deleted.");
     }
 }
