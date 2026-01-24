9dc30422acc63b15e6aa465444ffd09a149f9968
divawidia <wiartha2001@gmail.com>
2026-01-24 00:00:00 +0000

    refactor (complete Training Notification) : update training dependencies

--- app/Console/Commands/Trainings/CompletedTrainingStatus.php
+++ app/Console/Commands/Trainings/CompletedTrainingStatus.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands\Trainings;
 
+use App\Repository\Interface\TrainingRepositoryInterface;
 use App\Repository\MatchRepository;
 use App\Services\MatchService;
+use App\Services\TrainingService;
 use Illuminate\Console\Command;
 
 class CompletedTrainingStatus extends Command
@@ -21,24 +23,26 @@ class CompletedTrainingStatus extends Command
      * @var string
      */
     protected $description = 'Completed training status records where the end date has passed';
-    private MatchService $eventScheduleService;
-    private MatchRepository $eventScheduleRepository;
-
-    public function __construct(MatchService $eventScheduleService, MatchRepository $eventScheduleRepository)
+    private TrainingService $trainingService;
+    private TrainingRepositoryInterface $trainingRepository;
+    public function __construct(
+        TrainingService $trainingService,
+        TrainingRepositoryInterface $trainingRepository)
     {
         parent::__construct();
-        $this->eventScheduleService = $eventScheduleService;
-        $this->eventScheduleRepository = $eventScheduleRepository;
+        $this->trainingService = $trainingService;
+        $this->trainingRepository = $trainingRepository;
     }
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
         // Update records where end_date is less than the current date
-        $trainings = $this->eventScheduleRepository->getEndingEvent('Training');
+        $trainings = $this->trainingRepository->getAll(relations: [], status: ['Ongoing'], beforeEndDate: true);
         foreach ($trainings as $training){
-            $this->eventScheduleService->setStatus($training, 'Completed');
+            $this->trainingService->setStatus($training, 'Completed');
         }
 
         $this->info('Training schedule status data updated successfully set to completed.');
