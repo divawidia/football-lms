d075ed62e94bf9596598e7f0bea86de0c2dc7599
divawidia <wiartha2001@gmail.com>
2026-01-24 00:00:00 +0000

    refactor (Training Reminder Notification) : update dependencies

--- app/Console/Commands/Trainings/TrainingReminderNotification.php
+++ app/Console/Commands/Trainings/TrainingReminderNotification.php
@@ -2,9 +2,13 @@
 
 namespace App\Console\Commands\Trainings;
 
+use App\Notifications\TrainingSchedules\TrainingSchedule;
 use App\Notifications\TrainingSchedules\TrainingScheduleReminder;
+use App\Repository\Interface\TrainingRepositoryInterface;
 use App\Repository\MatchRepository;
 use App\Repository\UserRepository;
+use App\Services\TrainingService;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Notification;
 
@@ -25,13 +29,15 @@ class TrainingReminderNotification extends Command
     protected $description = 'Sent training schedule reminder notification to players, coaches';
 
     private UserRepository $userRepository;
-    private MatchRepository $eventScheduleRepository;
-    public function __construct(UserRepository  $userRepository,
-                                MatchRepository $eventScheduleRepository)
+    private TrainingRepositoryInterface $trainingRepository;
+    public function __construct(
+        UserRepository  $userRepository,
+        TrainingRepositoryInterface $trainingRepository
+    )
     {
         parent::__construct();
         $this->userRepository = $userRepository;
-        $this->eventScheduleRepository = $eventScheduleRepository;
+        $this->trainingRepository = $trainingRepository;
     }
 
     /**
@@ -39,12 +45,12 @@ class TrainingReminderNotification extends Command
      */
     public function handle()
     {
-        $trainings = $this->eventScheduleRepository->getUpcomingEvent('Training', 24);
+        $trainings = $this->trainingRepository->getAll(relations: [], status: ['Scheduled'], startDate: Carbon::now(), endDate: Carbon::now()->addHours(24), reminderNotified:  '0');
         foreach ($trainings as $data) {
             $data->update(['isReminderNotified' => '1']);
             $team = $data->teams()->first();
             $allTeamParticipant = $this->userRepository->allTeamsParticipant($team, admins: false);
-            Notification::send($allTeamParticipant, new TrainingScheduleReminder($data, $team));
+            Notification::send($allTeamParticipant, new TrainingSchedule($data, $team, 'reminder'));
         }
 
         $this->info('Upcoming training schedule successfully sent reminder notification.');
